79deb4be5bace1e8ea740611760ee7ff505850f5
orels1 <orel-s1@yandex.ru>
2016-04-26 00:34:58 +0300

    russia cog update

--- cogs/russia.py
+++ cogs/russia.py
@@ -1,19 +1,50 @@
 import discord
 from discord.ext import commands
 import asyncio
-import random
+import os
+import aiohttp
 
 class Russian:
-    """HE'S FROM RUSSIA!"""
+	"""HE'S FROM RUSSIA!"""
 
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
+		self.url = 'https://s-media-cache-ak0.pinimg.com/736x/64/84/bb/6484bbae1782cc50059be379254c7e77.jpg'
+		self.rusLoaded = os.path.exists('data/russian/russian.jpg')
+		self.image = "data/russian/russian.jpg"
 
-    @commands.command()
-    async def russian(self):
-        """WE NEED MORE RUSSIA IN CHAT!"""
+	@commands.command()
+	async def russian(self):
+		"""WE NEED MORE RUSSIA IN CHAT!"""
 
-        await self.bot.say("PUTIN VODKA BALALAIKA!\nhttps://s-media-cache-ak0.pinimg.com/736x/64/84/bb/6484bbae1782cc50059be379254c7e77.jpg")
+		await self.bot.say("PUTIN VODKA BALALAIKA!\n")
+
+	async def check_russian(self, message):
+		if "russia" in message.content.split():
+			if not self.rusLoaded:
+				try:
+					async with aiohttp.get(self.url) as r:
+						image = await r.content.read()
+					with open('data/russian/russian.jpg','wb') as f:
+						f.write(image)
+					self.rusLoaded = os.path.exists('data/russian/russian.jpg')
+					await self.bot.send_message(message.channel, "PUTIN VODKA BALALAIKA!")
+					await self.bot.send_file(message.channel,self.image)
+				except Exception as e:
+					print(e)
+					print("Rissan error D: Gonna drink VODKA and use url instead")
+					await self.bot.send_message(message.channel,"PUTIN VODKA BALALAIKA!\n" + self.url)
+			else:
+				await self.bot.send_message(message.channel, "PUTIN VODKA BALALAIKA!")
+				await self.bot.send_file(message.channel,self.image)
+
+def check_folders():
+	if not os.path.exists("data/russian"):
+		print("Creating data/russian folder...")
+		os.makedirs("data/russian")
 
 def setup(bot):
-    bot.add_cog(Russian(bot))
+	check_folders()
+	n = Russian(bot)
+	bot.add_listener(n.check_russian, "on_message")
+	bot.add_cog(n)
