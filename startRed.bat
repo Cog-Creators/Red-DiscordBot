d455def6a077f286372e70f669e2b6987edd096a
Canule <Canule@users.noreply.github.com>
2016-04-28 13:00:09 +0200

    startRed.bat - Title Red and save PID

--- startRed.bat
+++ startRed.bat
@@ -1,3 +1,11 @@
+@Echo off
 chcp 65001
+
+set get_pid_title=Red%DATE:/=-%@%TIME::=-%
+TITLE %get_pid_title%
+for /F "tokens=1-2" %%A in ('tasklist.exe /nh /fi "windowtitle eq %get_pid_title%"') do set red_pid=%%B
+TITLE Red - Discordbot @ %red_pid%
+echo %red_pid%> red.pid
+
 python red.py
 pause
