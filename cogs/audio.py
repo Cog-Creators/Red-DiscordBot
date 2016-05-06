d59125568c2981e5471cf5401c46f771050e94f7
Will Tekulve <tekulve.will@gmail.com>
2016-05-06 16:24:02 -0400

    add the correct defaults to settings

--- cogs/audio.py
+++ cogs/audio.py
@@ -1249,7 +1249,7 @@ def check_folders():
 def check_files():
     default = {"VOLUME": 0.5, "MAX_LENGTH": 3700, "QUEUE_MODE": True,
                "MAX_CACHE": 0, "SOUNDCLOUD_CLIENT_ID": None,
-               "TITLE_STATUS": True, "AVCONV": False,
+               "TITLE_STATUS": True, "AVCONV": False, "VOTE_THRESHOLD": 50,
                "SERVERS": {}}
     settings_path = "data/audio/settings.json"
 
