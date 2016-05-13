2f46605bf6d101e70d3d97117412ee9ad5c92969
Will <tekulvw@users.noreply.github.com>
2016-05-12 20:29:01 -0400

    Logging to stdout (#248)
    Non-infinite log files now, 50M before archive
    
    Lower max size

--- red.py
+++ red.py
@@ -7,6 +7,7 @@ import os
 import time
 import sys
 import logging
+import logging.handlers
 import shutil
 import traceback
 
@@ -235,14 +236,23 @@ def set_logger():
     logger.addHandler(handler)
 
     logger = logging.getLogger("red")
-    logger.setLevel(logging.WARNING)
-    handler = logging.FileHandler(
-        filename='data/red/red.log', encoding='utf-8', mode='a')
-    handler.setFormatter(logging.Formatter(
+    logger.setLevel(logging.INFO)
+
+    red_format = logging.Formatter(
         '%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: '
         '%(message)s',
-        datefmt="[%d/%m/%Y %H:%M]"))
-    logger.addHandler(handler)
+        datefmt="[%d/%m/%Y %H:%M]")
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(red_format)
+
+    fhandler = logging.handlers.RotatingFileHandler(
+        filename='data/red/red.log', encoding='utf-8', mode='a',
+        maxBytes=10**7, backupCount=5)
+    fhandler.setFormatter(red_format)
+
+    logger.addHandler(fhandler)
+    logger.addHandler(stdout_handler)
 
 
 def get_answer():
