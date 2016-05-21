901099a0672756135448b699ba0934fa7f56e94d
Francis Taylor <fishyfing@gmail.com>
2016-05-21 14:24:38 +0100

    test mod reasons

--- cogs/mod.py
+++ cogs/mod.py
@@ -51,14 +51,14 @@ class Mod:
 
     @commands.command(no_pm=True, pass_context=True)
     @checks.admin_or_permissions(kick_members=True)
-    async def kick(self, ctx, user: discord.Member):
+    async def kick(self, ctx, user: discord.Member, reason):
         """Kicks user."""
         author = ctx.message.author
         try:
             await self.bot.kick(user)
             logger.info("{}({}) kicked {}({})".format(
                 author.name, author.id, user.name, user.id))
-            await self.bot.say("Done. That felt good.")
+            await self.bot.say("{} was kicked by {}.\nReason: `{}`".format(user.name, author.name, reason))
         except discord.errors.Forbidden:
             await self.bot.say("I'm not allowed to do that.")
         except Exception as e:
@@ -66,7 +66,7 @@ class Mod:
 
     @commands.command(no_pm=True, pass_context=True)
     @checks.admin_or_permissions(ban_members=True)
-    async def ban(self, ctx, user: discord.Member, days: int=0):
+    async def ban(self, ctx, user: discord.Member, days: int=0, reason):
         """Bans user and deletes last X days worth of messages.
 
         Minimum 0 days, maximum 7. Defaults to 0."""
@@ -78,7 +78,7 @@ class Mod:
             await self.bot.ban(user, days)
             logger.info("{}({}) banned {}({}), deleting {} days worth of messages".format(
                 author.name, author.id, user.name, user.id, str(days)))
-            await self.bot.say("Done. It was about time.")
+            await self.bot.say("{} was banned by {}.\nReason: `{}`".format(user.name, author.name, reason))
         except discord.errors.Forbidden:
             await self.bot.say("I'm not allowed to do that.")
         except Exception as e:
