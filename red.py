2b8518426cc3f6acb6ce1ec3fe87371889f04e19
Kowlin <boxedpp@gmail.com>
2016-06-03 02:00:31 +0200

    Lets not nuke configs...

--- red.py
+++ red.py
@@ -385,8 +385,6 @@ if __name__ == '__main__':
               " properly.")
         shutil.copy('data/red/settings.json',
                     'data/red/settings-{}.bak'.format(int(time.time())))
-        # Hopefully this won't backfire in case of discord servers' problems
-        os.remove('data/red/settings.json')
     except:
         logger.error(traceback.format_exc())
         loop.run_until_complete(bot.logout())
