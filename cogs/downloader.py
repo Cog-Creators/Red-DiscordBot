8d414b444db1e5ceb177ec97c2c43c5dd28211f1
Will Tekulve <tekulve.will@gmail.com>
2016-06-04 17:26:54 -0400

    fix no info.json nonetype error

--- cogs/downloader.py
+++ cogs/downloader.py
@@ -203,9 +203,10 @@ class Downloader:
             return
         install_cog = await self.install(repo_name, cog)
         data = self.get_info_data(repo_name, cog)
-        install_msg = data.get("INSTALL_MSG", None)
-        if install_msg is not None:
-            await self.bot.say(install_msg[:2000])
+        if data is not None:
+            install_msg = data.get("INSTALL_MSG", None)
+            if install_msg is not None:
+                await self.bot.say(install_msg[:2000])
         if install_cog:
             await self.bot.say("Installation completed. Load it now? (yes/no)")
             answer = await self.bot.wait_for_message(timeout=15,
