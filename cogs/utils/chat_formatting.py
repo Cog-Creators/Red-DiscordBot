bd36df4003fd77816f6440e9a6715ab6f42a695f
Will Tekulve <tekulve.will@gmail.com>
2016-06-20 21:38:19 -0400

    Pagination, switch to format strings, give box a lang

--- cogs/utils/chat_formatting.py
+++ cogs/utils/chat_formatting.py
@@ -1,20 +1,44 @@
 def bold(text):
-    return "**"+str(text)+"**"
+    return "**{}**".format(text)
 
-def italics(text):
-    return "*"+str(text)+"*"
 
-def strikethrough(text):
-    return "~~"+str(text)+"~~"
+def box(text, lang=""):
+    ret = "```{}\n{}\n```".format(lang, text)
+    return ret
 
-def underline(text):
-    return "__"+str(text)+"__"
-
-def box(text):
-    return "```"+str(text)+"```"
 
 def inline(text):
-    return "`"+str(text)+"`"
+    return "`{}`".format(text)
+
+
+def italics(text):
+    return "*{}*".format(text)
+
+
+def pagify(text, delims=[], escape=True):
+    """DOES NOT RESPECT MARKDOWN BOXES OR INLINE CODE"""
+    in_text = text
+    while len(in_text) > 2000:
+        closest_delim = max([in_text.rfind(d, end=2000) for d in delims])
+        closest_delim = closest_delim if closest_delim != -1 else 2000
+        if escape:
+            yield escape_mass_mentions(in_text[:closest_delim])
+        else:
+            yield in_text[:closest_delim]
+        in_text = in_text[closest_delim:]
+    if escape:
+        yield escape_mass_mentions(in_text)
+    else:
+        yield in_text
+
+
+def strikethrough(text):
+    return "~~{}~~".format(text)
+
+
+def underline(text):
+    return "__{}__".format(text)
+
 
 def escape_mass_mentions(text):
     words = {
