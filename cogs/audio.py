e9e41e37dc090dc448a721e71ebd6f06a4283910
Phantium <phantium@users.noreply.github.com>
2016-07-02 21:32:14 +0200

    Change predefined prefix to use the bot command prefix and removed ! prefix from docstrings.

--- cogs/audio.py
+++ cogs/audio.py
@@ -1111,7 +1111,7 @@ class Audio:
 
     @commands.command(pass_context=True, no_pm=True)
     async def pause(self, ctx):
-        """Pauses the current song, `!resume` to continue."""
+        """Pauses the current song, `resume` to continue."""
         server = ctx.message.server
         if not self.voice_connected(server):
             await self.bot.say("Not voice connected in this server.")
@@ -1381,7 +1381,7 @@ class Audio:
 
     @commands.command(pass_context=True, no_pm=True, name="queue")
     async def _queue(self, ctx, *, url=None):
-        """Queues a song to play next. Extended functionality in `!help`
+        """Queues a song to play next. Extended functionality in `help`
 
         If you use `queue` when one song is playing, your new song will get
             added to the song loop (if running). If you use `queue` when a
@@ -1477,7 +1477,7 @@ class Audio:
                 else:
                     msg = "The queue is currently not looping."
                 await self.bot.say(msg)
-                await self.bot.say("Do `!repeat toggle` to change this.")
+                await self.bot.say("Do `{}repeat toggle` to change this.".format(self.bot.command_prefix[0]))
             else:
                 await self.bot.say("Play something to see this setting.")
 
