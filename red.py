56fd164438c953048f77b9d82518e2cc2c80f0ff
palmtree5 <remlap5.ap@gmail.com>
2016-08-21 01:29:57 -0500

    Add update check on bot startup

--- red.py
+++ red.py
@@ -3,6 +3,7 @@ import discord
 from cogs.utils.settings import Settings
 from cogs.utils.dataIO import dataIO
 from cogs.utils.chat_formatting import inline
+import aiohttp
 import asyncio
 import os
 import time
@@ -60,6 +61,7 @@ async def on_ready():
         bot.oauth_url = url
         print(url)
         print("------")
+    await update_check()
     await bot.get_cog('Owner').disable_commands()
 
 
@@ -99,6 +101,33 @@ async def on_command_error(error, ctx):
                                         "available in DMs.")
     else:
         logger.exception(type(error).__name__, exc_info=error)
+
+async def update_check():
+    if settings.owner == "id_here":
+        print("No owner set!")
+        return
+    owner = discord.utils.get(bot.get_all_members(), settings.owner)
+    try:
+        async with aiohttp.get("https://api.github.com/repos/Twentysix26/Red-DiscordBot/commits") as r:
+            gh_commits = await r.json()
+        latest_sha = gh_commits[0]["sha"][:7]
+        cur_version = os.popen(r'git show -s HEAD --format="%cr|%s|%h"')
+        cur_version = cur_version.read()
+        version = cur_version.split("|")[-1]
+        if latest_sha is not version:
+            message = "Your bot is out of date! Please update me using git pull!"
+            try:
+                await bot.send_message(owner, message)
+            except discord.errors.InvalidArgument:
+                print("I cannot send your message, I'm unable to find"
+                      "my owner... *sigh*")
+            except discord.errors.HTTPException:
+                print("Your message is too long.")
+            except:
+                print("I'm unable to deliver your message. Sorry.")
+    except:
+        return
+    print("------")
 
 async def send_cmd_help(ctx):
     if ctx.invoked_subcommand:
