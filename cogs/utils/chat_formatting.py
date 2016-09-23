9c2246b9b250f0d5cd92027a8d0c521b7e7bd914
Twentysix <Twentysix26@users.noreply.github.com>
2016-09-23 09:41:29 +0200

    [Utils] General escape function
    Allows to escape formatting and mass mentions

--- cogs/utils/chat_formatting.py
+++ cogs/utils/chat_formatting.py
@@ -58,12 +58,16 @@ def strikethrough(text):
 def underline(text):
     return "__{}__".format(text)
 
+def escape(text, *, mass_mentions=False, formatting=False):
+    if mass_mentions:
+        text = text.replace("@everyone", "@\u200beveryone")
+        text = text.replace("@here", "@\u200bhere")
+    if formatting:
+        text = (text.replace("`", "\\`")
+                    .replace("*", "\\*")
+                    .replace("_", "\\_")
+                    .replace("~", "\\~"))
+    return text
 
 def escape_mass_mentions(text):
-    words = {
-        "@everyone": "@\u200beveryone",
-        "@here": "@\u200bhere"
-    }
-    for k, v in words.items():
-        text = text.replace(k, v)
-    return text
+    return escape(text, mass_mentions=True)
