4e402a1148c49dacc58e651c7b329f16ff2ea05d
Redjumpman <chadwick.eh@gmail.com>
2016-10-04 23:54:16 -0400

    Update alias.py
    conversion of fileIO to dataIO

--- cogs/alias.py
+++ cogs/alias.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from .utils.chat_formatting import *
-from .utils.dataIO import fileIO
+from .utils.dataIO import dataIO
 from .utils import checks
 from __main__ import user_allowed, send_cmd_help
 import os
@@ -10,7 +10,8 @@ from copy import deepcopy
 class Alias:
     def __init__(self, bot):
         self.bot = bot
-        self.aliases = fileIO("data/alias/aliases.json", "load")
+        self.file_path = "data/alias/aliases.json"
+        self.aliases = dataIO.load_json(self.file_path)
 
     @commands.group(pass_context=True, no_pm=True)
     async def alias(self, ctx):
@@ -42,7 +43,7 @@ class Alias:
             self.aliases[server.id] = {}
         if command not in self.bot.commands:
             self.aliases[server.id][command] = to_execute
-            fileIO("data/alias/aliases.json", "save", self.aliases)
+            dataIO.save_json(self.file_path, self.aliases)
             await self.bot.say("Alias '{}' added.".format(command))
         else:
             await self.bot.say("Cannot add '{}' because it's a real bot "
@@ -84,7 +85,7 @@ class Alias:
         server = ctx.message.server
         if server.id in self.aliases:
             self.aliases[server.id].pop(command, None)
-            fileIO("data/alias/aliases.json", "save", self.aliases)
+            dataIO.save_json(self.file_path, self.aliases)
         await self.bot.say("Alias '{}' deleted.".format(command))
 
     @alias.command(name="list", pass_context=True, no_pm=True)
@@ -153,7 +154,7 @@ class Alias:
                 del self.aliases[sid][alias]
             for alias, command in to_add:  # For fixing caps
                 self.aliases[sid][alias] = command
-        fileIO("data/alias/aliases.json", "save", self.aliases)
+        dataIO.save_json(self.file_path, self.aliases)
 
     def first_word(self, msg):
         return msg.split(" ")[0]
@@ -175,9 +176,9 @@ def check_file():
     aliases = {}
 
     f = "data/alias/aliases.json"
-    if not fileIO(f, "check"):
+    if not dataIO.is_valid_json(f):
         print("Creating default alias's aliases.json...")
-        fileIO(f, "save", aliases)
+        dataIO.save_json(f, aliases)
 
 
 def setup(bot):
