dbb3cb45704451a99dcd4a29c043a9af4bbf416e
Jan200101 <sentrycraft123@gmail.com>
2016-11-05 17:09:10 +0100

    reimported a module, added channelinfo, made code smaller

--- cogs/general.py
+++ cogs/general.py
@@ -4,6 +4,7 @@ from random import randint
 from random import choice as randchoice
 from cogs.utils.chat_formatting import pagify
 from cogs.utils.chat_formatting import box
+from cogs.utils.chat_formatting import escape_mass_mentions
 import datetime
 import time
 import aiohttp
@@ -157,6 +158,36 @@ class General:
         await self.bot.say(msg)
 
     @commands.command(pass_context=True)
+    async def channelinfo(self, ctx, channel : discord.Channel = None):
+        """Shows channel informations"""
+        author = ctx.message.channel
+        server = ctx.message.server
+        if not channel:
+            channel = author
+        data = ""
+        data += "Name: {}\n".format(escape_mass_mentions(str(channel)))
+        if "{}".format(channel.type)=="voice":
+            data += "Mention: {}\n".format(escape_mass_mentions("#" + str(channel)))
+        data += "ID: {}\n".format(channel.id)
+        if "{}".format(channel.is_default)=="True":
+            data += "Default Channel: Yes\n"
+        else:
+            data += "Default Channel: No\n"
+        if channel.is_private == True:
+            data += "Private: Yes\n"
+        if "{}".format(channel.type)=="text":
+            data += """Topic:\n"{}"\n""".format(channel.topic)
+        data += "Position: {}\n".format(channel.position)
+        passed = (ctx.message.timestamp - channel.created_at).days
+        data += "Created: {} ({} days ago)\n".format(channel.created_at, passed)
+        data += "Type: {}\n".format(channel.type)
+        if "{}".format(channel.type)=="voice":
+            data += "Users: {}\n".format(len(channel.voice_members))
+            data += "User limit: {}\n".format(channel.user_limit)
+        for page in pagify(data, ["\n"], shorten_by=13, page_length=2000):
+            await self.bot.say(box(page, 'Prolog'))
+
+    @commands.command(pass_context=True)
     async def userinfo(self, ctx, user : discord.Member = None):
         """Shows users's informations"""
         author = ctx.message.author
@@ -242,10 +273,8 @@ class General:
         data += "Text channels: {}\n".format(text_channels)
         data += "Voice channels: {}\n".format(voice_channels)
         data += "Channel names:\n{}\n".format([c.name for c in server.channels])
-        data += "Emojis: {}\n".format(len(server.emojis))
-        data += "{}\n".format(", ".join([e.name for e in server.emojis]))
-        data += "Roles: {} \n".format(len(server.roles))
-        data += "{}\n".format([r.name for r in server.role_hierarchy])
+        data += "Emojis: {}\n{}\n".format(len(server.emojis), [e.name for e in server.emojis])
+        data += "Roles: {} \n{}\n".format(len(server.roles), [r.name for r in server.role_hierarchy])
         passed = (ctx.message.timestamp - server.created_at).days
         data += "Created: {} ({} days ago)\n".format(server.created_at, passed)
         data += "Owner: {}\n".format(server.owner)
