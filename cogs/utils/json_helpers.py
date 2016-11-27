9fd208f4e83c9c965cf9f3f28a2bd8c4681ceb47
Caleb Johnson <me@calebj.io>
2016-11-27 17:48:20 -0600

    [utils] some changes to json_helpers

--- cogs/utils/json_helpers.py
+++ cogs/utils/json_helpers.py
@@ -1,21 +1,38 @@
 import os
+import discord
+import collections.defaultdict
 from cogs.utils.dataIO import dataIO
+
+GLOBAL_KEY = '__global__'
+
 
 class DataDB:
     """
     A helper class to streamline the saving of json files
     """
-    def __init__(self, file_path, *, create_dirs=False):
+    def __init__(self, file_path, *, create_dirs=False, default_value=None):
         self.path = file_path
 
-        if create_dirs:
-            self._create_dirs(file_path)
+        path_exists = os.path.isfile(file_path)
 
-        if os.path.isfile(file_path):
+        if create_dirs and not path_exists:
+            path, _ = os.path.split(file_path)
+            if path:
+                try:
+                    os.makedirs(path)
+                except FileExistsError:
+                    pass
+
+        if path_exists:
             self._data = dataIO.load_json(file_path)
         else:
             self._data = {}
             self._save()
+
+        if default_value is not None:
+            def _get_default():
+                return default_value
+            self._data = collections.defaultdict(_get_default, self._data)
 
     def set(self, key, value):
         """Sets a DB's entry"""
@@ -40,70 +57,73 @@ class DataDB:
         """Returns all DB's data"""
         return self._data
 
-    def _create_dirs(self, file_path):
-        path, _ = os.path.split(file_path)
-        if path:
-            try:
-                os.makedirs(path)
-            except FileExistsError:
-                pass
-
     def _save(self):
         dataIO.save_json(self.path, self._data)
 
-    def __contains__(self, item):
-        return item in self._data
+    def __contains__(self, key):
+        return key in self._data
 
-    def __getitem__(self, item):
-        return self._data[item]
+    def __getitem__(self, key):
+        return self._data[key]
 
     def __len__(self):
         return len(self._data)
+
 
 class ServerDB(DataDB):
     """
     A helper class to streamline the saving of server based json data
     """
-    def __init__(self, *args, default_dict={}, **kwargs):
-        self._default_dict = default_dict
+
+    def __init__(self, *args, global_defaults={}, **kwargs):
         super().__init__(*args, **kwargs)
+        self._global_defaults = global_defaults
 
     def set(self, server, key, value):
         """Sets a server's entry"""
-        self._set_default(server)
+        if not isinstance(server, discord.Server):
+            raise TypeError('Can only set server data')
         self._data[server.id][key] = value
         self._save()
 
-    def get(self, server, key, default=None):
+    def get(self, server, key):
         """Returns a server's entry"""
-        self._set_default(server)
-        return self._data[server.id].get(key, default)
+        if not isinstance(server, discord.Server):
+            raise TypeError('Can only get server data')
+        return self._data[server.id][key]
 
     def remove(self, server, key):
         """Removes a server's entry"""
-        self._set_default(server)
+        if not isinstance(server, discord.Server):
+            raise TypeError('Can only remove server data')
         del self._data[server.id][key]
         self._save()
 
     def get_all(self, server):
         """Returns all entries of a server"""
-        self._set_default(server)
+        if not isinstance(server, discord.Server):
+            raise TypeError('Can only remove servers')
         return self._data[server.id]
 
     def remove_all(self, server):
         """Removes all entries of a server"""
-        del self._data[server.id]
+        if not isinstance(server, discord.Server):
+            raise TypeError('Can only remove servers')
+        super().remove(server.id)
         self._save()
 
-    def wipe(self):
-        """Wipes all servers' data"""
-        self._data = {}
+    def set_global(self, key, value):
+        """Sets a global value"""
+        if GLOBAL_KEY not in self._data:
+            self._data[GLOBAL_KEY] = {}
+        self._data[GLOBAL_KEY][key] = value
         self._save()
 
-    def all(self):
-        """Returns all servers' data"""
-        return self._data
+    def get_global(self, key):
+        """Gets a global value"""
+        if GLOBAL_KEY not in self._data:
+            return self._global_defaults[key]
 
-    def _set_default(self, server):
-        if server.id not in self._data:
-            self._data[server.id] = self._default_dict.copy()
+        if key in self._data[GLOBAL_KEY]:
+            return self._data[GLOBAL_KEY][key]
+        return self._global_defaults[key]
