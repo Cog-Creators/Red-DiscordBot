5de7dfbb4b80bc87866b6160a548d78226aa29a9
palmtree5 <palm@palmtree5.com>
2016-11-29 21:14:59 -0600

    [Image] rewrite imgur command to use subcommands

--- cogs/image.py
+++ cogs/image.py
@@ -1,69 +1,107 @@
-import discord
 from discord.ext import commands
 from random import randint
 import aiohttp
 import random
+try:
+    from imgurpython import ImgurClient
+    have_imgurpython = True
+except:
+    have_imgurpython = False
+
 
 class Image:
     """Image related commands."""
 
     def __init__(self, bot):
         self.bot = bot
-        #Reserved for further ... stuff
+        self.imgurclient\
+            = ImgurClient("1fd3ef04daf8cab",
+                          "f963e574e8e3c17993c933af4f0522e1dc01e230")
+        # Reserved for further ... stuff
 
     """Commands section"""
 
-    @commands.command(no_pm=True)
-    async def imgur(self, *text):
+    @commands.group(no_pm=True, pass_context=True)
+    async def imgur(self, ctx):
         """Retrieves a picture from imgur
 
         imgur search [keyword] - Retrieves first hit of search query.
-        imgur [subreddit section] [top or new] - Retrieves top 3 hottest or latest pictures of today for given a subreddit section, e.g. 'funny'."""
-        imgurclient = ImgurClient("1fd3ef04daf8cab", "f963e574e8e3c17993c933af4f0522e1dc01e230")
-        if text == ():
-            rand = randint(0, 59) #60 results per generated page
-            items = imgurclient.gallery_random(page=0)
-            await self.bot.say(items[rand].link)
-        elif text[0] == "search":
-            items = imgurclient.gallery_search(" ".join(text[1:len(text)]), advanced=None, sort='time', window='all', page=0)
-            if len(items) < 1:
-                await self.bot.say("Your search terms gave no results.")
-            else:
-                await self.bot.say(items[0].link)
-        elif text[0] != ():
-            try:
-                if text[1] == "top":
-                    imgSort = "top"
-                elif text[1] == "new":
-                    imgSort = "time"
-                else:
-                    await self.bot.say("Only top or new is a valid subcommand.")
-                    return
-                items = imgurclient.subreddit_gallery(text[0], sort=imgSort, window='day', page=0)
+        imgur [subreddit section] [top or new] - Retrieves top 3 hottest or
+                        latest pictures of today for given a subreddit
+                        section, e.g. 'funny'."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @imgur.command(pass_context=True, name="random")
+    async def imgur_random(self, ctx):
+        """Retrieves a random image from Imgur"""
+        rand = randint(0, 59)  # 60 results per generated page
+        items = self.imgurclient.gallery_random(page=0)
+        await self.bot.say(items[rand].link)
+
+    @imgur.command(pass_context=True, name="search")
+    async def imgur_search(self, ctx, *, term: str):
+        """Searches Imgur for the specified term"""
+        items =\
+            self.imgurclient.gallery_search(term, advanced=None, sort='time',
+                                            window='all', page=0)
+        if len(items) < 1:
+            await self.bot.say("Your search terms gave no results.")
+        else:
+            await self.bot.say(items[0].link)
+
+    @imgur.command(pass_context=True, name="subreddit")
+    async def imgur_subreddit(self, ctx, subreddit: str, sort_type: str):
+        """Gets images from the specified subreddit section"""
+        if sort_type.lower() == "top" or sort_type.lower() == "new":
+            if sort_type.lower() == "new":
+                imgSort = "time"
+            elif sort_type.lower() == "top":
+                imgSort = "top"
+            # Find the time window with the most images (at least 3)
+            windowList = ['day', 'week', 'month', 'year', 'all']
+            for x in windowList:
+                items =\
+                    self.imgurclient.subreddit_gallery(subreddit, sort=imgSort,
+                                                       window=x, page=0)
                 if (len(items) < 3):
-                    await self.bot.say("This subreddit section does not exist, try 'funny'")
+                    continue
                 else:
-                    await self.bot.say("{} {} {}".format(items[0].link, items[1].link, items[2].link))
-            except:
-                await self.bot.say("Type help imgur for details.")
+                    for i in range(0, 3):
+                        itemTitle = items[i].title
+                        if items[i].animated:
+                            itemLink = items[i].gifv
+                        else:
+                            itemLink = items[i].link
+                        await self.bot.say("{}\n{}\n".format(itemTitle,
+                                                             itemLink))
+                    break
+            else:
+                await self.bot.say("\"{}\" either does not exist or does not" +
+                                   " have enough content!".format(subreddit))
+        else:
+            await self.bot.say("Only top or new is a valid subcommand.")
+            return
 
     @commands.command(no_pm=True)
     async def gif(self, *text):
         """Retrieves first search result from giphy
-        
+
         gif [keyword]"""
         if len(text) > 0:
             if len(text[0]) > 1 and len(text[0]) < 20:
                 try:
                     msg = "+".join(text)
-                    search = "http://api.giphy.com/v1/gifs/search?q=" + msg + "&api_key=dc6zaTOxFJmzC"
+                    search = "http://api.giphy.com/v1/gifs/search?q=" + msg +\
+                             "&api_key=dc6zaTOxFJmzC"
                     async with aiohttp.get(search) as r:
                         result = await r.json()
                     if result["data"] != []:
                         url = result["data"][0]["url"]
                         await self.bot.say(url)
                     else:
-                        await self.bot.say("Your search terms gave no results.")
+                        await\
+                            self.bot.say("Your search terms gave no results.")
                 except:
                     await self.bot.say("Error.")
             else:
@@ -71,7 +109,7 @@ class Image:
         else:
             await self.bot.say("gif [text]")
 
-    @commands.command(no_pm=True)
+    @commands.command(no_pm=True, pass_context=True)
     async def gifr(self, *text):
         """Retrieves a random gif from a giphy search
 
@@ -81,14 +119,17 @@ class Image:
             if len(text[0]) > 1 and len(text[0]) < 20:
                 try:
                     msg = "+".join(text)
-                    search = "http://api.giphy.com/v1/gifs/random?&api_key=dc6zaTOxFJmzC&tag=" + msg
+                    search = "http://api.giphy.com/v1/gifs/random" +\
+                             "?&api_key=dc6zaTOxFJmzC&tag=" + msg
                     async with aiohttp.get(search) as r:
                         result = await r.json()
                         if result["data"] != []:
                             url = result["data"]["url"]
                             await self.bot.say(url)
                         else:
-                            await self.bot.say("Your search terms gave no results.")
+                            await\
+                                self.bot.say("Your search terms " +
+                                             "gave no results.")
                 except:
                     await self.bot.say("Error.")
             else:
@@ -96,16 +137,10 @@ class Image:
         else:
             await self.bot.say("gifr [text]")
 
-class ModuleNotFound(Exception):
-    def __init__(self, m):
-        self.message = m
-    def __str__(self):
-        return self.message
 
 def setup(bot):
-    global ImgurClient
-    try:
-        from imgurpython import ImgurClient
-    except:
-        raise ModuleNotFound("imgurpython is not installed. Do 'pip3 install imgurpython' to use this cog.")
-    bot.add_cog(Image(bot))
+    if not have_imgurpython:
+        raise RuntimeError("You need to do pip3 install" +
+                           " imgurpython to use this!")
+    else:
+        bot.add_cog(Image(bot))
