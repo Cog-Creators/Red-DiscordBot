81b73cd575f68d18ae27f487b8bd1029ec2c2587
Max Tweddell <maxt.nz@gmail.com>
2016-12-10 16:02:27 +1300

    added mos moduel and fuckquix

---
+++ cogs/mos.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+from .utils.chat_formatting import *
+from random import randint
+from random import choice as randchoice
+import datetime
+import time
+import aiohttp
+import asyncio
+import csv
+import shelve
+
+
+
+
+class mos:
+    """ Made of Styrofoam Specific Commands."""
+    d = shelve.open('myfile.db')
+    FUCKS = d['fucks']
+    d.close() 
+
+
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(hidden=True)
+    async def noot(self):
+        "NOOT"
+        await self.bot.say("NOOT")
+
+    @commands.command(hidden=True)
+    async def fuckquix(self):
+        "fuckquix lol"
+        d = shelve.open('myfile.db')
+
+        mos.FUCKS['quix'] += 1
+        f = mos.FUCKS['quix']
+        await self.bot.say(f)
+
+        fucks = {'quix': f}
+        d = shelve.open('myfile.db')
+        d['fucks'] = fucks
+        d.close()    
+
+        
+def setup(bot):
+    n = mos(bot)
+    bot.add_cog(n)
