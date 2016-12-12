02825b51418d9aed8236f4fe80c44167a179f75a
Max Tweddell <maxt.nz@gmail.com>
2016-12-12 18:48:09 +1300

    changed div by zero exception

--- cogs/mos.py
+++ cogs/mos.py
@@ -15,12 +15,17 @@ import datetime
 from datetime import date
 from datetime import timedelta 
 import time
+import math
+import pint
+from currency_converter import CurrencyConverter
 
+c = CurrencyConverter()
+ureg = pint.UnitRegistry()
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 class mos:
     """ Made of Styrofoam Specific Commands."""
-    FUCKS = int(r.get('Quixoticelixer'))
+  
 
     def __init__(self,bot):
         self.bot = bot
@@ -33,10 +38,9 @@ class mos:
     @commands.command(hidden=True)
     async def fuckquix(self):
         "fuckquix lol"
-        d = shelve.open('myfile.db')
-
-        mos.FUCKS += 1
-        f = mos.FUCKS
+        FUCKS = int(r.get('Quixoticelixer'))   
+        FUCKS += 1
+        f = FUCKS
         await self.bot.say(f)
         r.set('Quixoticelixer',f)
 
@@ -100,16 +104,50 @@ class mos:
         if num == 1:
             await self.bot.say("https://github.com/Quixoticelixer/Red-DiscordBot/blob/develop/data/images/nigel.JPG?raw=true")
         elif num == 2:
-            await self.bot.say("https://github.com/Quixoticelixer/Red-DiscordBot/blob/develop/data/images/feshtank.JPG?raw=true")
+            await self.bot.say("https://github.com/Quixoticelixer/Red-DiscordBot/blob/develop/data/images/feshtank.jpg?raw=true")
         elif num == 3:
             await self.bot.say("https://github.com/Quixoticelixer/Red-DiscordBot/blob/develop/data/images/feshtank2.JPG?raw=true")
         elif num == 4:
             await self.bot.say("https://github.com/Quixoticelixer/Red-DiscordBot/blob/develop/data/images/subaru1.JPG?raw=true")
     @commands.command()
     async def miata(self):
-        await self.bot.say(":rainbow:")
-        
+        await self.bot.say("https://goo.gl/s9MKcU :rainbow::gay_pride_flag::rainbow::rainbow::gay_pride_flag::rainbow::rainbow::rainbow::rainbow::rainbow::rainbow::rainbow::rainbow:::gay_pride_flag::rainbow:::gay_pride_flag::rainbow:::gay_pride_flag::rainbow:::gay_pride_flag::rainbow::gay_pride_flag:")
+    
+    @commands.command()
+    async def ginger(self):
+        await self.bot.say("http://www.ranga.co.nz/wp-content/uploads/2015/12/Ranga-GB-6Pack.jpg")
 
+    @commands.command()
+    async def calc(self, input):
+       try:
+           await self.bot.say(eval(input))
+       except ZeroDivisionError:
+           weighted_choices = [('noot', 1),('0', 28), ('1', 28), ('git', 1), ('max', 1),('f', 2),('x', 2),('iqx',1),('-2', 1)]
+           stringer = [val for val, cnt in weighted_choices for i in range(cnt)]
+           output = 'fml'
+           for x in range (0,90):
+                 output = output + str(random.choice(stringer)) 
+           await self.bot.say(output)
+
+    @commands.command()
+    async def convert(self, unit1, unit2):
+        unit1 = int(unit1)
+        if unit2 == 'kg' or unit2 == 'kgs':
+            unit3 = unit1 * ureg.kilogram
+            unit3 = unit3.to(ureg.lbs)
+            await self.bot.say("{}kgs is {:3.1f}s".format(unit1,unit3))
+        elif unit2 == 'lb' or unit2 == 'lbs':
+            unit3 = unit1 * ureg.lbs
+            unit3 = unit3.to(ureg.kg)
+            await self.bot.say("{}lbs is {:3.1f}s".format(unit1,unit3))
+   # @commands.command()
+  #  async def moni_convert(self, unit):
+   #     eur = c.convert(unit, 'USD','EUR')
+   #     cad = c.convert(unit, 'USD','CAD')
+   #     nzd = c.convert(unit, 'USD','NZD')
+   #     aud = c.convert(unit, 'USD','AUD')
+
+        # await self.bot.say('{0} USD is {1:7.2f} EUR, {2:7.2f} CAD, {3:7.2f} NZD, {4:7.2f} AUD'.format(unit,eur,cad,nzd,aud))
 
 def setup(bot):
     n = mos(bot)
