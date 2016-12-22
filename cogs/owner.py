131a3f9a18ada4aafd059b5dc8bacf1119b00e5d
Jan200101 <sentrycraft123@gmail.com>
2016-12-22 21:13:34 +0100

    added travis-ci support for forks

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,6 +14,7 @@ import datetime
 import glob
 import os
 import aiohttp
+import requests
 
 log = logging.getLogger("red.owner")
 
@@ -741,11 +742,8 @@ class Owner:
         """Shows Red's current version"""
         response = self.bot.loop.run_in_executor(None, self._get_version)
         result = await asyncio.wait_for(response, timeout=10)
-        try:
-            await self.bot.say(embed=result)
-        except discord.HTTPException:
-            await self.bot.say("I need the `Embed links` permission "
-                               "to send this")
+        await self.bot.say(embed=result)
+
 
     def _load_cog(self, cogname):
         if not self._does_cogfile_exist(cogname):
@@ -803,6 +801,9 @@ class Owner:
     def _get_version(self):
         url = os.popen(r'git config --get remote.origin.url')
         url = url.read().strip()[:-4]
+        branch = os.popen(r'git rev-parse --abbrev-ref HEAD')
+        branch = branch.read().strip()
+        travisbuildstatus = "{}.png?branch={}".format(url.replace("github.com", "api.travis-ci.org"), branch)
         repo_name = url.split("/")[-1]
         commits = os.popen(r'git show -s -n 3 HEAD --format="%cr|%s|%H"')
         ncommits = os.popen(r'git rev-list --count HEAD').read()
@@ -819,7 +820,11 @@ class Owner:
             commit_url = url + "/commit/" + chash
             content = "[{}]({}) - {} ".format(chash[:6], commit_url, commit)
             embed.add_field(name=when, value=content, inline=False)
-        embed.set_thumbnail(url="https://travis-ci.org/Twentysix26/Red-DiscordBot.png?branch=develop")
+
+        request = requests.get(travisbuildstatus)
+        if request.status_code == 200:
+            embed.set_thumbnail(url=travisbuildstatus)
+
         embed.set_footer(text="Total commits: " + ncommits)
 
         return embed
