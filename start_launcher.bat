3e582203fd7050ed6b845effc11068d786a1a1d3
Jan200101 <sentrycraft123@gmail.com>
2017-02-02 16:55:55 +0100

    changed shell size a bit (still up to removing it) removed pauses as the launcher does them

--- start_launcher.bat
+++ start_launcher.bat
@@ -2,19 +2,21 @@
 title Red Discord Bot - Windows Launcher
 color 0F ::Replace with 0C for the Red experience
 
-echo Please wait
-chcp 65001 > NUL
-
-echo.
 mode con: cols=80 lines=12
 pushd %~dp0
+
+echo(
+echo( Please wait
+echo(
+
+chcp 65001 > NUL
+cls
 
 ::Attempts to start py launcher without relying on PATH
 %SYSTEMROOT%\py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO attempt
 %SYSTEMROOT%\py.exe -3.5 launcher.py
 IF %ERRORLEVEL% NEQ 0 goto attempt
-PAUSE
 GOTO end
 
 ::Attempts to start py launcher by relying on PATH
@@ -22,7 +24,6 @@ GOTO end
 py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO lastattempt
 py.exe -3 launcher.py
-PAUSE
 GOTO end
 
 ::As a last resort, attempts to start whatever Python there is
@@ -30,7 +31,6 @@ GOTO end
 python.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO message
 python.exe launcher.py
-PAUSE
 GOTO end
 
 :message
@@ -39,8 +39,6 @@ echo( Couldn't find a valid Python ^>3.5 installation. Python needs to be instal
 echo( variable.
 echo( Please visit https://twentysix26.github.io/Red-Docs/red_win_requirements/#software
 echo(
-echo( Press any key to exit
-pause > NUL
 goto message
 
 :end
