086c1f0df8eaad56cce4a9723ca420cb1e7bb7af
Jan200101 <sentrycraft123@gmail.com>
2017-02-04 19:51:57 +0100

    updated it AGAIN O:

--- start_launcher.bat
+++ start_launcher.bat
@@ -1,11 +1,14 @@
 @echo off
+
 title Red Discord Bot - Windows Launcher
+
 ::Replace with 0C for the Red experience
 color 0F
 
 mode con: cols=85 lines=14
 pushd %~dp0
 
+::Filling the voice on first launch
 echo(
 echo( Please wait
 echo(
@@ -16,14 +19,28 @@ cls
 
 ::Attempts to start py launcher without relying on PATH
 %SYSTEMROOT%\py.exe --version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO attempt1
+%SYSTEMROOT%\py.exe -3.5 --version
 IF %ERRORLEVEL% NEQ 0 GOTO attempt
 %SYSTEMROOT%\py.exe -3.5 launcher.py
-IF %ERRORLEVEL% NEQ 0 goto attempt
+GOTO end
+
+:attempt
+%SYSTEMROOT%\py.exe -3 launcher.py
+IF %ERRORLEVEL% NEQ 0 goto attempt1
 GOTO end
 
 ::Attempts to start py launcher by relying on PATH
-:attempt
+:attempt1
 py.exe --version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO lastattempt
+py.exe -3.5 --version
+IF %ERRORLEVEL% NEQ 0 GOTO attempt2
+py.exe -3.5 launcher.py
+GOTO end
+
+:attempt2
+py.exe -3 --version
 IF %ERRORLEVEL% NEQ 0 GOTO lastattempt
 py.exe -3 launcher.py
 GOTO end
