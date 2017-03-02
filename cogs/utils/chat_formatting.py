59f9928f3b5f8893ae1c86e552c8fbe55fc93c9f
zephyrkul <zephyrkul@outlook.com>
2017-03-01 17:06:50 -0700

    Fix pagify bug
    Right now it seems pagify() will yield an empty string if it can't find a deliminator, and on looking at the code it looks like it's because it finds the deliminator at the start of the string if it's the second page or higher (since it was just cut off there for the previous string). By replacing the second rfind argument with len(d), I think that'll fix that, and limited testing shows that it does.

--- cogs/utils/chat_formatting.py
+++ cogs/utils/chat_formatting.py
@@ -40,7 +40,7 @@ def pagify(text, delims=["\n"], *, escape=True, shorten_by=8,
         shorten_by += num_mentions
     page_length -= shorten_by
     while len(in_text) > page_length:
-        closest_delim = max([in_text.rfind(d, 0, page_length)
+        closest_delim = max([in_text.rfind(d, len(d), page_length)
                              for d in delims])
         closest_delim = closest_delim if closest_delim != -1 else page_length
         if escape:
