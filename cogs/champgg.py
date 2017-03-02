e577394c5390f17d444eb9501de32156e56d4a52
devonpmack <devonpmack@gmail.com>
2017-03-02 14:36:28 -0500

    added new module changed help menu

---
+++ cogs/champgg.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
