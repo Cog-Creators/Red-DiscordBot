42811c4ef77a27ea8460e87d0b48621c05a6f3c3
Will Tekulve <tekulve.will@gmail.com>
2017-03-13 21:50:05 -0400

    Wow we can play now...except song switching/repeat is fucked

--- cogs/audio.py
+++ cogs/audio.py
@@ -95,10 +95,15 @@ class Song:
         # Only the filename, needs to get joined with download_folder to create
         #   the actual accessible path
 
+        self.local = False
+
         self.extra_data = kwargs
 
     def __eq__(self, other):
-        return self.id == other.id
+        try:
+            return self.id == other.id
+        except AttributeError:
+            return False
 
     @classmethod
     def from_ytdl(cls, **extracted_info):
@@ -465,8 +470,9 @@ music_cache = None  # Is set in setup
 
 
 class MusicQueue:
-    def __init__(self, bot, songs=[], temp_songs=[], start_index=0):
+    def __init__(self, bot, player, songs=[], temp_songs=[], start_index=0):
         self.bot = bot
+        self.player = player
         self._songs = songs
         self._temp_songs = temp_songs
 
@@ -486,8 +492,11 @@ class MusicQueue:
         # TODO: Add functions for adding to queue
 
     async def on_song_end(self, last_song):
+        # self.next()
         self.current_song = self.next_song
-        self.bot.dispatch("on_red_audio_song_change", self.current_song)
+        log.debug("Some song just ended: {} now starting {}".format(
+            last_song, self.current_song))
+        self.bot.dispatch("red_audio_song_change", self.current_song)
 
     @property
     def next_song(self):
@@ -528,6 +537,12 @@ class MusicQueue:
         except IndexError:
             return False
 
+    def check_start_song(self):
+        if self.player.is_playing is False:
+            log.debug("Not currently playing but something was just added to"
+                      " the queue, starting play sequence.")
+            self.bot.dispatch("red_audio_song_end", None)
+
     def add_to_queue(self, obj, position=None):
         """
         If position is `None` the song(s) will be put at the end of the queue.
@@ -540,6 +555,8 @@ class MusicQueue:
                 self._songs.insert(position, s)
         except AttributeError:
             self._songs.insert(position, obj)
+
+        self.check_start_song()
 
     def add_to_tempqueue(self, obj, position=None):
         """
@@ -554,6 +571,8 @@ class MusicQueue:
                 self._temp_songs.insert(position, s)
         except AttributeError:
             self._temp_songs.insert(position, obj)
+
+        self.check_start_song()
 
     def clear(self, songs=True, temp_songs=True):
         if songs is True:
@@ -571,7 +590,7 @@ class MusicQueue:
         else:
             self._temp_songs = self._temp_songs[num:]
 
-        self.bot.dispatch("on_red_audio_song_end", self.current_song)
+        self.bot.dispatch("red_audio_song_end", self.current_song)
 
     def next(self):
         return self.skip()
@@ -590,12 +609,12 @@ class MusicQueue:
         else:
             log.debug("Updated position {} in queue".format(position))
 
+        if self.current_song == new_object:
+            self.current_song = new_object
+
     async def update_downloaders(self):
-        try:
-            songs = [s for s in [self.current_song, ] +
-                     self.queue(self.advance_downloads) if s is not None]
-        except AttributeError:
-            return
+        songs = [s for s in [self.current_song, ] +
+                 self.queue(self.advance_downloads) if s is not None]
         for i, s in enumerate(songs):
             if s.id not in self._downloads:
                 d = self.bot.loop.create_task(
@@ -648,19 +667,22 @@ class MusicPlayer(MusicPlayerCommandsMixin):
         self._voice_client = None
         self._server = voice_member.server
 
-        self._queue = MusicQueue(bot)
-
         self._dpy_player = None
 
-        # self._play_loop = discord.compat.create_task(self.play_loop(),
-        #                                              loop=bot.loop)
+        self._song_changer = None
 
         self.stream_mode = False
+
+        self.ffmpeg_args = {}
+
+        self.current_song = None
 
         self.bot.add_listener(self.on_red_audio_unload,
                               "on_red_audio_unload")
 
         self.bot.add_listener(self.on_song_change, "on_red_audio_song_change")
+
+        self._queue = MusicQueue(bot, self)
 
     def __eq__(self, other):
         return self.server == other.server
@@ -721,19 +743,59 @@ class MusicPlayer(MusicPlayerCommandsMixin):
 
     # End mixin
 
-    async def on_song_change(self, song):
+    async def create_dpy_player(self, song):
+        log.debug("Starting dpy player creation")
+        try:
+            if self._voice_client is None:
+                await self.connect()
+        except asyncio.TimeoutError:
+            log.debug("Timed out, not creating dpy player.")
+        except:
+            log.exception("Something happened (not a timeout error)"
+                          ", not creating dpy player")
+
+        ffmpeg_args = ["-{} {}".format(k, v) for k, v in self.ffmpeg_args]
+        ffmpeg_args = " ".join(ffmpeg_args)
+
+        song_file = await music_cache.get_filename(song)
+
+        if song.local is True:
+            pass  # For now, might not need this
+        else:
+            self._dpy_player = self._voice_client.create_ffmpeg_player(
+                song_file, options=ffmpeg_args,
+                after=functools.partial(self.bot.dispatch,
+                                        "red_audio_song_end"))
+
+        self._dpy_player.start()
+
+    async def change_song(self, song):
         if song != self._queue.current_song:
             # This should never happen
             log.warning("Requested song doesn't match the current song from"
                         " queue! Please report this error as it should never"
                         " happen.")
-            self._queue._temp_songs.insert(0, song)
+            self._queue.add_to_tempqueue(song, 0)
+            self._queue.current_song = song
 
-        if self.current_song != self._queue.current_song:
-            # TODO: Kill current dpy player
-            # TODO: Guarantee downloaded new current
-            # TODO: Guarantee downloaded next N songs
-            self.current_song = self._queue.current_song
+        # TODO: Kill current dpy player
+        # TODO: Guarantee downloaded new current
+        # TODO: Guarantee downloaded next N songs
+        log.debug("Waiting on song to download.")
+        while self._queue.current_song.downloaded is False:
+            await asyncio.sleep(0.1)
+
+        self.current_song = self._queue.current_song
+        await self.create_dpy_player(self.current_song)
+
+    async def on_song_change(self, song):
+        try:
+            self._song_changer.cancel()
+        except (AttributeError, asyncio.CancelledError):
+            pass
+        self._song_changer = discord.compat.create_task(
+            self.change_song(song), loop=self.bot.loop)
+        log.debug("created song changer future")
 
     """
     async def play_loop(self):
@@ -798,7 +860,7 @@ class Audio(ChecksMixin, AudioCommandErrorHandlersMixin):
     def __unload(self):
         # Dispatching this so everything can do it's own unload stuff,
         #   might not need it.
-        self.bot.dispatch("on_red_audio_unload")
+        self.bot.dispatch("red_audio_unload")
 
     def player(self, ctx):
         """
