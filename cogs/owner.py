749e2fa6cb9f07ae701acef83f291f17f7c042dd
palmtree5 <remlap5.ap@gmail.com>
2017-04-13 16:37:41 -0500

    [Mod] move ignore/unignore of servers and channels to Owner

--- cogs/owner.py
+++ cogs/owner.py
@@ -46,6 +46,7 @@ class Owner:
         self.setowner_lock = False
         self.disabled_commands = dataIO.load_json("data/red/disabled_commands.json")
         self.global_ignores = dataIO.load_json("data/red/global_ignores.json")
+        self.ignore_list = dataIO.load_json("data/mod/ignorelist.json")
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
 
     def __unload(self):
@@ -252,6 +253,92 @@ class Owner:
                     finally:
                         break
             await self.bot.say(box(page, lang="py"))
+
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_channels=True)
+    async def ignore(self, ctx):
+        """Adds servers/channels to ignorelist"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+            await self.bot.say(self.count_ignored())
+
+    @ignore.command(name="channel", pass_context=True)
+    async def ignore_channel(self, ctx, channel: discord.Channel=None):
+        """Ignores channel
+
+        Defaults to current one"""
+        current_ch = ctx.message.channel
+        if not channel:
+            if current_ch.id not in self.ignore_list["CHANNELS"]:
+                self.ignore_list["CHANNELS"].append(current_ch.id)
+                dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+                await self.bot.say("Channel added to ignore list.")
+            else:
+                await self.bot.say("Channel already in ignore list.")
+        else:
+            if channel.id not in self.ignore_list["CHANNELS"]:
+                self.ignore_list["CHANNELS"].append(channel.id)
+                dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+                await self.bot.say("Channel added to ignore list.")
+            else:
+                await self.bot.say("Channel already in ignore list.")
+
+    @ignore.command(name="server", pass_context=True)
+    async def ignore_server(self, ctx):
+        """Ignores current server"""
+        server = ctx.message.server
+        if server.id not in self.ignore_list["SERVERS"]:
+            self.ignore_list["SERVERS"].append(server.id)
+            dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+            await self.bot.say("This server has been added to the ignore list.")
+        else:
+            await self.bot.say("This server is already being ignored.")
+
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_channels=True)
+    async def unignore(self, ctx):
+        """Removes servers/channels from ignorelist"""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+            await self.bot.say(self.count_ignored())
+
+    @unignore.command(name="channel", pass_context=True)
+    async def unignore_channel(self, ctx, channel: discord.Channel=None):
+        """Removes channel from ignore list
+
+        Defaults to current one"""
+        current_ch = ctx.message.channel
+        if not channel:
+            if current_ch.id in self.ignore_list["CHANNELS"]:
+                self.ignore_list["CHANNELS"].remove(current_ch.id)
+                dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+                await self.bot.say("This channel has been removed from the ignore list.")
+            else:
+                await self.bot.say("This channel is not in the ignore list.")
+        else:
+            if channel.id in self.ignore_list["CHANNELS"]:
+                self.ignore_list["CHANNELS"].remove(channel.id)
+                dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+                await self.bot.say("Channel removed from ignore list.")
+            else:
+                await self.bot.say("That channel is not in the ignore list.")
+
+    @unignore.command(name="server", pass_context=True)
+    async def unignore_server(self, ctx):
+        """Removes current server from ignore list"""
+        server = ctx.message.server
+        if server.id in self.ignore_list["SERVERS"]:
+            self.ignore_list["SERVERS"].remove(server.id)
+            dataIO.save_json("data/mod/ignorelist.json", self.ignore_list)
+            await self.bot.say("This server has been removed from the ignore list.")
+        else:
+            await self.bot.say("This server is not in the ignore list.")
+
+    def count_ignored(self):
+        msg = "```Currently ignoring:\n"
+        msg += str(len(self.ignore_list["CHANNELS"])) + " channels\n"
+        msg += str(len(self.ignore_list["SERVERS"])) + " servers\n```\n"
+        return msg
 
     @commands.group(name="set", pass_context=True)
     async def _set(self, ctx):
