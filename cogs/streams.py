09bfee8d4549103866b72dab67379974235f4af2
palmtree5 <remlap5.ap@gmail.com>
2017-04-13 14:50:09 -0500

    [Streams] add list of streamalerts for specified channel

--- cogs/streams.py
+++ cogs/streams.py
@@ -80,13 +80,33 @@ class Streams:
             await self.bot.say("Error.")
 
     @commands.group(pass_context=True, no_pm=True)
-    @checks.mod_or_permissions(manage_server=True)
     async def streamalert(self, ctx):
         """Adds/removes stream alerts from the current channel"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
+    @streamalert.command(name="list", pass_context=True)
+    async def streamalert_list(self, ctx, channel: discord.Channel=None):
+        """Lists all streamalerts for the specified channel
+           (defaults to the current channel)"""
+        if not channel:
+            channel = ctx.message.channel
+        msg = "Twitch Streams: \n"
+        for stream in self.twitch_streams:
+            if channel.id in stream["CHANNELS"]:
+                msg += stream["NAME"] + " is " + ("online" if stream["ALREADY_ONLINE"] else "offline") + "\n"
+        msg += "\nBeam Streams: \n"
+        for stream in self.beam_streams:
+            if channel.id in stream["CHANNELS"]:
+                msg += stream["NAME"] + " is " + ("online" if stream["ALREADY_ONLINE"] else "offline") + "\n"
+        msg += "\nHitbox Streams: \n"
+        for stream in self.hitbox_streams:
+            if channel.id in stream["CHANNELS"]:
+                msg += stream["NAME"] + " is " + ("online" if stream["ALREADY_ONLINE"] else "offline") + "\n"
+        await self.bot.say(msg)
+
     @streamalert.command(name="twitch", pass_context=True)
+    @checks.mod_or_permissions(manage_server=True)
     async def twitch_alert(self, ctx, stream: str):
         """Adds/removes twitch alerts from the current channel"""
         stream = escape_mass_mentions(stream)
@@ -138,6 +158,7 @@ class Streams:
         dataIO.save_json("data/streams/twitch.json", self.twitch_streams)
 
     @streamalert.command(name="hitbox", pass_context=True)
+    @checks.mod_or_permissions(manage_server=True)
     async def hitbox_alert(self, ctx, stream: str):
         """Adds/removes hitbox alerts from the current channel"""
         stream = escape_mass_mentions(stream)
@@ -184,6 +205,7 @@ class Streams:
         dataIO.save_json("data/streams/hitbox.json", self.hitbox_streams)
 
     @streamalert.command(name="beam", pass_context=True)
+    @checks.mod_or_permissions(manage_server=True)
     async def beam_alert(self, ctx, stream: str):
         """Adds/removes beam alerts from the current channel"""
         stream = escape_mass_mentions(stream)
@@ -230,6 +252,7 @@ class Streams:
         dataIO.save_json("data/streams/beam.json", self.beam_streams)
 
     @streamalert.command(name="stop", pass_context=True)
+    @checks.mod_or_permissions(manage_server=True)
     async def stop_alert(self, ctx):
         """Stops all streams alerts in the current channel"""
         channel = ctx.message.channel
