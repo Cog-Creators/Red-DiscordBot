b1d1e8e5a0eef1a2f03c05f541a739939dbd6e7c
Rhino <SexualRhinoceros@users.noreply.github.com>
2017-04-20 19:55:29 -0700

    added async support for io thru aiofiles
    also added backwards compatibility so if this is updated and deps aren't nothing will break. I could add in a forceful update of deps but I dont want to control the local environment of the end user

--- cogs/utils/dataIO.py
+++ cogs/utils/dataIO.py
@@ -9,6 +9,15 @@ class InvalidFileIO(Exception):
 class DataIO():
     def __init__(self):
         self.logger = logging.getLogger("red")
+        try:
+            __import__(aiofiles)
+            import aiofiles
+            self.async = True
+        except ImportError:
+            self.logger.exception("Attempted to use aiofiles but 
+                                  "dependancy wasn\'t installed. "
+                                  "Please update all dependancies!")
+            self.async = False
 
     def save_json(self, filename, data):
         """Atomically saves json file"""
@@ -51,7 +60,58 @@ class DataIO():
             json.dump(data, f, indent=4,sort_keys=True,
                 separators=(',',' : '))
         return data
-
+    async def async_is_valid_json(self, filename):
+        """Verifies if json file exists / is readable"""
+        try:
+            self._async_read_json(filename)
+            return True
+        except FileNotFoundError:
+            return False
+        except json.decoder.JSONDecodeError:
+            return False
+        
+    async def async_save_json(self, filename, data):
+        """Atomically saves json file"""
+        if not self.async:
+            self.logger.exception("Attempted to save using aiofiles "
+                                  "but dependancy wasn't installed. "
+                                  "Please update all dependancies!")
+            return False
+        rnd = randint(1000, 9999)
+        path, ext = os.path.splitext(filename)
+        tmp_file = "{}-{}.tmp".format(path, rnd)
+        self._async_save_json(tmp_file, data)
+        try:
+            self._async_read_json(tmp_file)
+        except json.decoder.JSONDecodeError:
+            self.logger.exception("Attempted to write file {} but JSON "
+                                  "integrity check on tmp file has failed. "
+                                  "The original file is unaltered."
+                                  "".format(filename))
+            return False
+        os.replace(tmp_file, filename)
+        return True
+    
+    async def async_load_json(self, filename):
+        """Loads json file"""
+        if not self.async:
+            self.logger.exception("Attempted to load using aiofiles "
+                                  "but dependancy wasn't installed. "
+                                  "Please update all dependancies!")
+            return False
+        return self._async_read_json(filename)
+    
+    async def _async_read_json(self, filename):
+        async with aiofiles.open(filename, encoding='utf-8', mode="r") as f:
+            data = json.load(f)
+        return data
+    
+    async def _async_save_json(self, filename, data):
+        async with aiofiles.open(filename, encoding='utf-8', mode="w") as f:
+            json.dump(data, f, indent=4,sort_keys=True,
+                separators=(',',' : '))
+        return data
+    
     def _legacy_fileio(self, filename, IO, data=None):
         """Old fileIO provided for backwards compatibility"""
         if IO == "save" and data != None:
