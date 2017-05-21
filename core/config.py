534a9fe8223ce407e6e1a9fc56667a61b8f01703
Will Tekulve <tekulve.will@gmail.com>
2017-05-21 00:21:55 -0700

    Add type annotations to get_conf

--- core/config.py
+++ core/config.py
@@ -49,7 +49,7 @@ class BaseConfig:
             "MEMBER": {}, "USER": {}, "MISC": {}}
 
     @classmethod
-    def get_conf(cls, cog_name, unique_identifier):
+    def get_conf(cls, cog_name: str, unique_identifier: int):
         """
         Gets a config object that cog's can use to safely store data. The
             backend to this is totally modular and can easily switch between
