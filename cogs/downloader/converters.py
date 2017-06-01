08297d6a3a2340e49bd309e9d0e4da86b8c13825
Will Tekulve <tekulve.will@gmail.com>
2017-05-31 22:37:25 -0400

    Change errors as indicated by Danny

--- cogs/downloader/converters.py
+++ cogs/downloader/converters.py
@@ -1,21 +1,22 @@
 from discord.ext import commands
 from .repo_manager import RepoManager
+from .repo_manager import Repo as RealRepo
 
 
 class RepoName(commands.Converter):
-    async def convert(self, ctx: commands.Context, arg: str):
+    async def convert(self, ctx: commands.Context, arg: str) -> str:
         return RepoManager.validate_and_normalize_repo_name(arg)
 
 
 class Repo(commands.Converter):
-    async def convert(self, ctx: commands.Context, argument: str):
+    async def convert(self, ctx: commands.Context, argument: str) -> RealRepo:
         downloader_cog = ctx.bot.get_cog("Downloader")
         if downloader_cog is None:
-            raise RuntimeError("No Downloader cog found.")
+            raise commands.CommandError("No Downloader cog found.")
 
         # noinspection PyProtectedMember
         repo_manager = downloader_cog._repo_manager
         poss_repo = repo_manager.get_repo(argument)
         if poss_repo is None:
-            raise ValueError("Repo by the name {} does not exist.".format(argument))
+            raise commands.BadArgument("Repo by the name {} does not exist.".format(argument))
         return poss_repo
