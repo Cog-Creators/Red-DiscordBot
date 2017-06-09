2bdf85302abd75c51cf68544da59da355caa1dcf
Twentysix <Twentysix26@users.noreply.github.com>
2017-06-09 19:20:42 +0200

    Spacing

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import sys
 
 if discord.version_info.major < 1:
     print("You are not running the rewritten version of discord.py.\n\n"
-          "In order to use Redv3 you MUST be running d.py version"
+          "In order to use Red v3 you MUST be running d.py version"
           " >= 1.0.0.")
     sys.exit(1)
 
