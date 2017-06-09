68bc6e33251e61022fe6ae43f18027ddf7fdf527
Will <tekulvw@users.noreply.github.com>
2017-06-09 13:22:36 -0400

    [Core] Check for correct discord.py version at boot (#799)
    Fixes #798

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ import sys
 #         Made by Twentysix, improved by many
 #
 
+if discord.version_info.major < 1:
+    print("You are not running the rewritten version of discord.py.\n\n"
+          "In order to use Red v3 you MUST be running d.py version"
+          " >= 1.0.0.")
+    sys.exit(1)
+
 
 def init_loggers(cli_flags):
     dpy_logger = logging.getLogger("discord")
