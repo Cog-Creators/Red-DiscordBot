e4fc4554e46199c5cfcc4207f79d53e2f6811e18
Will Tekulve <tekulve.will@gmail.com>
2017-06-23 17:18:48 -0400

    Announcement

--- cogs/admin/admin.py
+++ cogs/admin/admin.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 from core import Config, checks
 
+from .announcer import Announcer
 from .converters import MemberDefaultAuthor
 
 
@@ -11,12 +12,17 @@ GENERIC_FORBIDDEN = (
     " Your command failed to successfully complete."
 )
 
-
-HEIRARCHY_ISSUE = (
+HIERARCHY_ISSUE = (
     "I tried to add {role.name} to {member.display_name} but that role"
     " is higher than my highest role in the Discord heirarchy so I was"
     " unable to successfully add it. Please give me a higher role and "
     "try again."
+)
+
+RUNNING_ANNOUNCEMENT = (
+    "I am already announcing something. If you would like to make a"
+    " different announcement please use `{prefix}announce cancel`"
+    " first."
 )
 
 
@@ -24,9 +30,32 @@ class Admin:
     def __init__(self):
         self.conf = Config.get_conf(self, 8237492837454039)
 
-    async def complain(self, ctx: commands.Context, message: str,
+        self.conf.register_guild(
+            announce_channel=None
+        )
+
+        self.__current_announcer = None
+
+    def __unload(self):
+        try:
+            self.__current_announcer.cancel()
+        except AttributeError:
+            pass
+
+    @staticmethod
+    async def complain(ctx: commands.Context, message: str,
                        **kwargs):
         await ctx.send(message.format(**kwargs))
+
+    def is_announcing(self) -> bool:
+        """
+        Is the bot currently announcing something?
+        :return:
+        """
+        if self.__current_announcer is None:
+            return False
+
+        return self.__current_announcer.active or False
 
     def pass_heirarchy_check(self, ctx: commands.Context,
                              role: discord.Role) -> bool:
@@ -53,7 +82,7 @@ class Admin:
             user.add_roles(rolename)
         except discord.Forbidden:
             if not self.pass_heirarchy_check(ctx, rolename):
-                await self.complain(ctx, HEIRARCHY_ISSUE, role=rolename,
+                await self.complain(ctx, HIERARCHY_ISSUE, role=rolename,
                                     member=user)
             else:
                 await self.complain(ctx, GENERIC_FORBIDDEN)
@@ -74,8 +103,8 @@ class Admin:
         try:
             user.remove_roles(rolename)
         except discord.Forbidden:
-            if not self.pass_heirarchy_check(ctx, rolename)
-                await self.complain(ctx, HEIRARCHY_ISSUE, role=rolename,
+            if not self.pass_heirarchy_check(ctx, rolename):
+                await self.complain(ctx, HIERARCHY_ISSUE, role=rolename,
                                     member=user)
             else:
                 await self.complain(ctx, GENERIC_FORBIDDEN)
@@ -85,3 +114,33 @@ class Admin:
                                role=rolename, member=user
                            ))
 
+    @commands.group()
+    @checks.is_owner()
+    async def announce(self, ctx: commands.Context, message: str):
+        """
+        Announces a message to all servers the bot is in.
+        """
+        if not self.is_announcing():
+            announcer = Announcer(ctx, message, config=self.conf)
+            announcer.start()
+
+            self.__current_announcer = announcer
+
+            await ctx.send("The announcement has begun.")
+        else:
+            prefix = ctx.prefix
+            await self.complain(ctx, RUNNING_ANNOUNCEMENT,
+                                prefix=prefix)
+
+    @announce.command(name="cancel")
+    @checks.is_owner()
+    async def announce_cancel(self, ctx):
+        """
+        Cancels a running announce.
+        """
+        try:
+            self.__current_announcer.cancel()
+        except AttributeError:
+            pass
+
+        await ctx.send("The current announcement has been cancelled.")
---
+++ cogs/admin/announcer.py
@@ -0,0 +1,63 @@
+import asyncio
+from typing import MutableMapping
+
+import discord
+from discord.ext import commands
+
+
+class Announcer:
+    def __init__(self, ctx: commands.Context,
+                 message: str,
+                 config=None):
+        """
+        :param ctx:
+        :param message:
+        :param config: Used to determine channel overrides
+        """
+        self.ctx = ctx
+        self.message = message
+        self.config = config
+
+        self.active = None
+
+    def start(self):
+        """
+        Starts an announcement.
+        :return:
+        """
+        if self.active is None:
+            asyncio.ensure_future(self.announcer(), loop=ctx.bot.loop)
+
+    def cancel(self):
+        """
+        Cancels a running announcement.
+        :return:
+        """
+        self.active = False
+
+    async def announcer(self):
+        guild_list = self.ctx.bot.guilds
+        bot_owner = (await self.ctx.bot.application_info()).owner
+        for g in guild_list:
+            if not self.active:
+                return
+
+            channel_id = self.config.guild(g).announce_channel()
+            channel = None
+
+            if channel_id is not None:
+                channel = self.ctx.bot.get_channel(channel_id=int(channel_id))
+
+            if channel is None:
+                channel = g
+
+            try:
+                await channel.send(self.message)
+            except discord.Forbidden:
+                await bot_owner.send("I could not announce to guild: {}".format(
+                                         g.id
+                                     ))
+            await asyncio.sleep(0.5)
+
+        self.active = False
+
