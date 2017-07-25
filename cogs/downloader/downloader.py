99e0e8b0e1b196921bc36efbaa6d3f88ae8d14a2
Will Tekulve <tekulve.will@gmail.com>
2017-07-25 11:23:28 -0400

    Another fix

--- cogs/downloader/downloader.py
+++ cogs/downloader/downloader.py
@@ -30,7 +30,7 @@ class Downloader:
     def __init__(self, bot: Red):
         self.bot = bot
 
-        self.conf = Config.get_conf(self, unique_identifier=998240343,
+        self.conf = Config.get_conf(self, identifier=998240343,
                                     force_registration=True)
 
         self.conf.register_global(
