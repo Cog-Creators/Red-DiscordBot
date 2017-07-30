e61a71f8d5927ea919ffe0da5358d7534e36c002
Will Tekulve <tekulve.will@gmail.com>
2017-07-30 10:47:27 -0400

    Add UI commands for install path

--- core/cog_manager.py
+++ core/cog_manager.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 from discord.ext import commands
 
+from core import checks
 from core.config import Config
 from core.utils.chat_formatting import box
 
@@ -155,6 +156,7 @@ class CogManager:
 
 class CogManagerUI:
     @commands.command()
+    @checks.is_owner()
     async def paths(self, ctx: commands.Context):
         """
         Lists current cog paths in order of priority.
@@ -168,6 +170,7 @@ class CogManagerUI:
         await ctx.send(box(msg))
 
     @commands.command()
+    @checks.is_owner()
     async def addpath(self, ctx: commands.Context, path: Path):
         """
         Add a path to the list of available cog paths.
@@ -181,6 +184,7 @@ class CogManagerUI:
         await ctx.send("Path successfully added.")
 
     @commands.command()
+    @checks.is_owner()
     async def removepath(self, ctx: commands.Context, path_number: int):
         """
         Removes a path from the available cog paths given the path_number
@@ -197,6 +201,7 @@ class CogManagerUI:
         await ctx.send("Path successfully removed.")
 
     @commands.command()
+    @checks.is_owner()
     async def reorderpath(self, ctx: commands.Context, from_: int, to: int):
         """
         Reorders paths internally to allow discovery of different cogs.
@@ -221,3 +226,23 @@ class CogManagerUI:
         await ctx.bot.cog_mgr.set_paths(all_paths)
         await ctx.send("Paths reordered.")
 
+    @commands.command()
+    @checks.is_owner()
+    async def installpath(self, ctx: commands.Context, path: Path=None):
+        """
+        Returns the current install path or sets it if one is provided.
+            The provided path must be absolute or relative to the bot's
+            directory and it must already exist.
+
+        No installed cogs will be transferred in the process.
+        """
+        if path:
+            try:
+                await ctx.bot.cog_mgr.set_install_path(path)
+            except ValueError:
+                await ctx.send("That path does not exist.")
+                return
+
+        install_path = ctx.bot.cog_mgr.install_path
+        await ctx.send("The bot will install new cogs to the `{}`"
+                       " directory.".format(install_path))
