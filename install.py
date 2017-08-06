d3293a6a196a58b3a37c12509acdda1d1087d736
palmtree5 <palmtree5@users.noreply.github.com>
2017-08-06 15:32:45 -0800

    More installer work

--- install.py
+++ install.py
@@ -1,19 +1,21 @@
 from __future__ import print_function  # Used to allow python2 to run this
 import os
 import sys
-from subprocess import check_output, CalledProcessError, STDOUT
+from subprocess import call, check_output, CalledProcessError, STDOUT
 from enum import Enum
-import argparse
 import platform
+import getpass
+import fire
+from time import sleep
 try:
     import pip
 except ImportError:
     pip = None
 
-try:
-    import requests
-except ImportError:
-    requests = None
+if sys.version_info.major == 2:
+    from urllib import urlretrieve
+elif sys.version_info.major == 3:
+    from urllib.request import urlretrieve
 
 
 IS_WINDOWS = os.name == "nt"
@@ -34,18 +36,34 @@ if not IS_WINDOWS and not IS_MAC:  # On linux, so import distro to help determin
         distro = None
 
 
-def parse_cli_args():
-    parser = argparse.ArgumentParser(description="Red-DiscordBot's installer")
-    parser.add_argument("--install-reqs", "-i",
-                        help="Installs the needed prereqs for Red",
-                        action="store_true")
-    parser.add_argument("--setup-red", "-s",
-                        help="Downloads and walks through setting up Red",
-                        action="store_true")
-    parser.add_argument("--update-red", "-u",
-                        help="Updates Red",
-                        action="store_true")
-    return parser.parse_args()
+UNSUPPORTED_LINUX_TEXT = (
+    """
+    Your distro (or version) is not supported by this installer. It may have\n
+    a page that walks through the installation manually which is located at\n
+    http://twentysix26.github.io/Red-Docs/red_install_linux/\n
+    If it is not listed there, you are probably on your own.\n
+    However, to run Red, one needs to install:\n\n
+    - Git\n
+    - Python (3.5 or greater)\n
+    - ffmpeg or avconv\n
+    - libsodium\n
+    - pip\n
+    - libssl\n
+    - libffi\n
+    - unzip\n
+    - libopus\n\n
+    Please be aware that if your package manager doesn't provide a package\n
+    for some of these, you may need to compile them on\ your own which means\n
+    you would need to install the necessary tools to allow you to compile\n
+    source code; therefore if you are not comfortable doing this, you should\n
+    select a different distro to use for running an instance of Red. Please\n
+    do not ask for help with this in the support channel in Red's official\n
+    server as we may not be able to assist you with this process. If you\n
+    choose to carry on using an unsupported distro, you may wish to use\n
+    Google to assist you in answering any questions you may have. Your\n
+    distro's website may also contain documentation that may help you\n
+    """
+)
 
 
 def do_mac_install():
@@ -111,26 +129,114 @@ def ubuntu_install():
     else:
         print(aptresult.decode())
         print("\n\nRequirements installed successfully.\n")
-        if sys.version_info < (3, 5) and args.setup_red:
-            print("Please note that the script will give an error\n"
-                  "message regarding the version of python used to\n"
-                  "run this installer and will then exit. You can do\n\n"
-                  "python3 install.py --setup-red\n\n"
-                  "to continue with setting up Red"
-                  )
+
+
+def raspbian_install():
+    pass
+
+
+def debian_install():
+    try:
+        aar_install = check_output(["sudo", "apt", "install", "software-properties-common"], stderr=STDOUT)
+    except CalledProcessError as e:
+        print("Error while installing software-properties-common! Output:\n\n")
+        print(e.output.decode())
+        exit(InstallerExitCodes.REQFAIL)
+    else:
+        print(aar_install.decode())
+    sources_list_add = "deb http://httpredir.debian.org/debian stretch-backports main contrib non-free"
+
+    try:
+        add_backports = check_output(["sudo", "add-apt-repository", sources_list_add], stderr=STDOUT)
+    except CalledProcessError as e:
+        print("Error while adding stretch-backports to sources! Output:\n\n")
+        print(e.output.decode())
+        exit(InstallerExitCodes.REQFAIL)
+    else:
+        print(add_backports.decode())
+
+    try:
+        apt_update = check_output(["sudo", "apt", "update"], stderr=STDOUT)
+    except CalledProcessError as e:
+        print("Error doing apt update! Output:\n\n")
+        print(e.output.decode())
+        exit(InstallerExitCodes.REQFAIL)
+    else:
+        print(apt_update.decode())
+
+    package_list = ["python3", ""]
+
+
+def amazon_linux_install():
+    pass
 
 
 def do_linux_install():
-    distro_name = distro.name()
+    distro_id = distro.id()
     distro_version = [int(part) for part in distro.version().split(".")]
+    if distro_id == "ubuntu" and distro_version[0] == 16 and distro_version[1] == 4:
+        pass
+    elif distro_id == "debian" and distro_version[0] == 9:
+        pass
+    elif distro_id == "raspbian":
+        pass
+    elif distro_id == "amazon":
+        pass
+    else:
+        print(UNSUPPORTED_LINUX_TEXT)
+        sleep(120)  # Give enough time to read the above text before exiting
+        exit(InstallerExitCodes.UNSUPPORTEDLINUX)
+
+
+def download_pip():
+    urlretrieve("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
+
+
+def setup_red():
+    raise NotImplementedError
+
+
+def update_red():
+    raise NotImplementedError
+
+
+def install_requirements():
+    if not IS_WINDOWS and not IS_MAC and distro is None:
+        if distro is None:
+            if pip is None:  # Installer requirements on Linux need pip
+                print("Pip is not installed!")
+                print("Attempting to install pip...")
+
+            print("You are missing some requirements for using the launcher!")
+            print("Attempting to fix this now...")
+            status = pip.main(["install", "-U", "-r", "launcher-requirements.txt"])
+            if status == 0:
+                print("Launcher requirements installed successfully")
+                print("Please relaunch the launcher once it exits")
+                exit(InstallerExitCodes.SUCCESS)
+            else:
+                print("Error with installing requirements, error code {}".format(status))
+                exit(InstallerExitCodes.PIPFAILED)
+    if IS_WINDOWS:
+        print(
+            "In order to properly install requirements on Windows, "
+            "you need to use the powershell script (launch-install-win.ps1)"
+        )
+        # exit(0)
+    elif IS_MAC:
+        do_mac_install()
+    else:
+        do_linux_install()
 
 
 def installer_welcome_message():
     print(
         (
-            "===========================\n"
-            "Red Discord Bot - Installer\n"
-            "===========================\n"
+            """
+            ===========================\n
+            Red Discord Bot - Installer\n
+            ===========================\n
+            """
         )
     )
 
@@ -144,51 +250,16 @@ class InstallerExitCodes(Enum):
     HOMEBREWFAIL = 5
     BREWINSTALLFAIL = 6
     REQFAIL = 7
+    UNSUPPORTEDLINUX = 8
 
-
-args = parse_cli_args()
 
 if __name__ == "__main__":
     installer_welcome_message()
-    if args.install_reqs:
-        if not IS_WINDOWS and not IS_MAC and distro is None:
-            if requests is None or distro is None:
-                if pip is None:  # For installing requirements, we only need pip on Linux
-                    print("It appears pip is not installed! I need pip for installing requirements!")
-                    exit(InstallerExitCodes.NOPIP)
-                print("You are missing some requirements for using the launcher!")
-                print("Attempting to fix this now...")
-                status = pip.main(["install", "-U", "-r", "launcher-requirements.txt"])
-                if status == 0:
-                    print("Launcher requirements installed successfully")
-                    print("Please relaunch the launcher once it exits")
-                    exit(InstallerExitCodes.SUCCESS)
-                else:
-                    print("Error with installing requirements, error code {}".format(status))
-                    exit(InstallerExitCodes.PIPFAILED)
-        if IS_WINDOWS:
-            print(
-                "In order to properly install requirements on Windows, "
-                "you need to use the powershell script (launch-install-win.ps1)"
-            )
-            exit(0)
-        elif IS_MAC:
-            do_mac_install()
-        else:
-            do_linux_install()
-    if args.setup_red and args.update_red:
-        print("Conflicting arguments --setup-red and --update-red! I can't do both at the same time!")
-        exit(InstallerExitCodes.ARGCONFLICT)
 
-    if args.setup_red:
-        if not PYTHON_OK:
-            print("Improper Python version detected!")
-            if IS_WINDOWS:
-                print("I require Python 3.5.x to work properly.")
-                print("Please use the powershell script to ensure "
-                      "the correct version is installed")
-            else:
-                print("I require at least Python 3.5.x to work properly")
-            exit(InstallerExitCodes.IMPROPERPYVER)
-    elif args.update_red:
-        pass
+    fire.Fire(
+        {
+            "install": install_requirements,
+            "setup": setup_red,
+            "update": update_red
+        }
+    )
