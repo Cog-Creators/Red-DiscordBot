2e469d9074b39e30fff9dda498934cf7786ed6cb
Markolie <markvanalphen2@gmail.com>
2017-08-14 06:26:03 +0200

    Maybe? fix the OSError

--- cogs/audio.py
+++ cogs/audio.py
@@ -262,7 +262,8 @@ class Downloader(threading.Thread):
             self.hit_max_length.set()
         except OSError as e:
             log.warning("An operating system error occurred while downloading URL '{}':\n'{}'".format(self.url, str(e)))
-            error = str(e)
+            if(e.__cause__ is youtube_dl.utils.ExtractorError or e.__cause__ is youtube_dl.utils.GeoRestrictedError):
+                error = str(e.__cause__)
         self.done.set()
 
     def download(self):
