a44a0cedf5368ac7c17e74e488da4598dfe6b2d3
Will Tekulve <tekulve.will@gmail.com>
2017-08-20 14:10:51 -0400

    Fix versioning

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,7 @@
 from core.config import Config
 from subprocess import run, PIPE
 from collections import namedtuple
+from main import determine_main_folder
 
 __all__ = ["Config", "__version__"]
 version_info = namedtuple("VersionInfo", "major minor patch")
@@ -9,10 +10,12 @@ BASE_VERSION = version_info(3, 0, 0)
 
 
 def get_latest_version():
+    main_folder = determine_main_folder()
     try:
         p = run(
             "git describe --abbrev=0 --tags".split(),
-            stdout=PIPE
+            stdout=PIPE,
+            cwd=str(main_folder)
         )
     except FileNotFoundError:
         # No git
