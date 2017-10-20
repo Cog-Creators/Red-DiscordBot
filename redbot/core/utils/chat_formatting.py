6f103174aac58f1566fdb7ed2cd78d522b5dc491
Redjumpman <Redjumpman@users.noreply.github.com>
2017-10-20 14:29:55 -0500

    V3/start screen (#1038)
    * Added new start screen
    
    * Added Bordered util
    
    Bordered function required for boxes in start screen
    
    * Update events.py
    
    Added Color to the Title
    Added Shard count
    
    * Update events.py
    
    Added Discord.py version
    Added package checking for docs and tests
    Added package checking for voice
    
    * Update chat_formatting.py
    
    Cleaned up the border function a bit.

--- redbot/core/utils/chat_formatting.py
+++ redbot/core/utils/chat_formatting.py
@@ -1,3 +1,5 @@
+import itertools
+
 def error(text):
     return "\N{NO ENTRY SIGN} {}".format(text)
 
@@ -29,6 +31,23 @@ def inline(text):
 
 def italics(text):
     return "*{}*".format(text)
+
+
+def bordered(text1: list, text2: list):
+    width1, width2 = max((len(s1) + 9, len(s2) + 9) for s1 in text1 for s2 in text2)
+    res = ['┌{}┐{}┌{}┐'.format("─"*width1, " "*4, "─"*width2)]
+    flag = True
+    for x, y in itertools.zip_longest(text1, text2):
+        if y:
+            m = "│{}│{}│{}│".format((x + " " * width1)[:width1], " "*4, (y + " " * width2)[:width2])
+        elif x and flag and not y:
+            m = "│{}│{}└{}┘".format((x + " " * width1)[:width1], " "*4, "─" *  width2)
+            flag = False
+        else:
+            m = "│{}│".format((x + " " * width1)[:width1])
+        res.append(m)
+    res.append("└" + "─" * width1 + "┘")
+    return "\n".join(res)
 
 
 def pagify(text, delims=["\n"], *, escape_mass_mentions=True, shorten_by=8,
