09b3642559cefa5df02a7b6ba0218464cfde77b2
Will <tekulvw@users.noreply.github.com>
2017-10-27 20:06:47 -0400

    [V3 Cog/Data Manager] Bundled Cog Data (#1063)
    * Refactor find_spec out of core_commands
    
    * Fix version error when not installed
    
    * initial
    
    * Fix find_cogs call
    
    * Enable copying
    
    * Add helper method for cog creators
    
    * Add warning
    
    * My dpy skillz need work

--- redbot/core/__init__.py
+++ redbot/core/__init__.py
@@ -5,4 +5,7 @@ from .context import RedContext
 
 __all__ = ["Config", "RedContext", "__version__"]
 
-__version__ = version = pkg_resources.require("Red-DiscordBot")[0].version
+try:
+    __version__ = pkg_resources.require("Red-DiscordBot")[0].version
+except pkg_resources.DistributionNotFound:
+    __version__ = "3.0.0"
