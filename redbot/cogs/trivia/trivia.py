1db106ede3902b6475046f860d7fd0784adea405
Tobotimus <tobyharradine@live.com>
2017-11-04 12:16:51 +1100

    Leaderboard implemented

--- redbot/cogs/trivia/trivia.py
+++ redbot/cogs/trivia/trivia.py
@@ -1,11 +1,12 @@
 """Module for Trivia cog."""
+from collections import Counter
 import yaml
 import discord
 from discord.ext import commands
 import redbot.trivia
 from redbot.core import Config, checks
 from redbot.core.data_manager import cog_data_path
-from redbot.core.utils.chat_formatting import box
+from redbot.core.utils.chat_formatting import box, pagify
 from redbot.cogs.bank import check_global_setting_admin
 from .log import LOG
 from .session import TriviaSession
@@ -38,9 +39,7 @@ class Trivia:
             allow_override=True)
 
         self.conf.register_member(
-            wins=0,
-            games=0,
-            correct_answers=0)
+            wins=0, games=0, total_score=0)
 
     @commands.group()
     @commands.guild_only()
@@ -48,7 +47,7 @@ class Trivia:
     async def triviaset(self, ctx: commands.Context):
         """Manage trivia settings."""
         if ctx.invoked_subcommand is None:
-            await ctx.bot.send_cmd_help(ctx)
+            await ctx.send_help()
             settings = self.conf.guild(ctx.guild)
             settings_dict = await settings.all()
             msg = box(
@@ -97,7 +96,9 @@ class Trivia:
                        " seconds of no responses.".format(seconds))
 
     @triviaset.command(name="override")
-    async def triviaset_allowoverride(self, ctx: commands.Context, enabled: bool):
+    async def triviaset_allowoverride(self,
+                                      ctx: commands.Context,
+                                      enabled: bool):
         """Allow/disallow trivia lists to override settings."""
         settings = self.conf.guild(ctx.guild)
         await settings.allow_override.set(enabled)
@@ -168,7 +169,7 @@ class Trivia:
         questions from all of them.
         """
         if not categories:
-            await ctx.bot.send_cmd_help(ctx)
+            await ctx.send_help()
             return
         categories = [c.lower() for c in categories]
         session = self._get_trivia_session(ctx.channel)
@@ -215,13 +216,9 @@ class Trivia:
                 "There is no ongoing trivia session in this channel.")
             return
         author = ctx.author
-        auth_checks = (
-            await ctx.bot.is_owner(author),
-            await ctx.bot.is_mod(author),
-            await ctx.bot.is_admin(author),
-            author == ctx.guild.owner,
-            author == session.ctx.author
-        )
+        auth_checks = (await ctx.bot.is_owner(author), await
+                       ctx.bot.is_mod(author), await ctx.bot.is_admin(author),
+                       author == ctx.guild.owner, author == session.ctx.author)
         if any(auth_checks):
             await session.end_game()
             session.force_stop()
@@ -240,6 +237,169 @@ class Trivia:
             await ctx.author.send(msg)
             return
         await ctx.send(msg)
+
+    @trivia.group(name="leaderboard", aliases=["lboard"])
+    async def trivia_leaderboard(self, ctx: commands.Context):
+        """Leaderboard for trivia.
+
+        Defaults to the top 10 of this server, sorted by total wins. Use
+        subcommands for a more customised leaderboard.
+        """
+        if ctx.invoked_subcommand == self.trivia_leaderboard:
+            cmd = self.trivia_leaderboard_server
+            if isinstance(ctx.channel, discord.abc.PrivateChannel):
+                cmd = self.trivia_leaderboard_global
+            await ctx.invoke(cmd, "wins", 10)
+
+    @trivia_leaderboard.command(name="server")
+    @commands.guild_only()
+    async def trivia_leaderboard_server(self,
+                                        ctx: commands.Context,
+                                        sort_by: str="wins",
+                                        top: int=10):
+        """Leaderboard for this server.
+
+        <sort_by> can be any of the following fields:
+         - wins  : total wins
+         - avg   : average score
+         - total : total correct answers
+
+        <top> is the number of ranks to show on the leaderboard.
+        """
+        key = self._get_sort_key(sort_by)
+        if key is None:
+            await ctx.send("Unknown field `{}`, see `{}help trivia "
+                           "leaderboard server` for valid fields to sort by."
+                           "".format(sort_by, ctx.prefix))
+            return
+        guild = ctx.guild
+        data = await self.conf.all_members(guild)
+        data = {guild.get_member(u): d for u, d in data.items()}
+        data.pop(None, None)  # remove any members which aren't in the guild
+        await self.send_leaderboard(ctx, data, key, top)
+
+    @trivia_leaderboard.command(name="global")
+    async def trivia_leaderboard_global(self,
+                                        ctx: commands.Context,
+                                        sort_by: str="wins",
+                                        top: int=10):
+        """Global trivia leaderboard.
+
+        <sort_by> can be any of the following fields:
+         - wins  : total wins
+         - avg   : average score
+         - total : total correct answers from all sessions
+
+        <top> is the number of ranks to show on the leaderboard.
+        """
+        key = self._get_sort_key(sort_by)
+        if key is None:
+            await ctx.send("Unknown field `{}`, see `{}help trivia "
+                           "leaderboard global` for valid fields to sort by."
+                           "".format(sort_by, ctx.prefix))
+            return
+        data = await self.conf.all_members()
+        collated_data = {}
+        for guild_id, guild_data in data.items():
+            guild = ctx.bot.get_guild(guild_id)
+            if guild is None:
+                continue
+            for member_id, member_data in guild_data.items():
+                member = guild.get_member(member_id)
+                if member is None:
+                    continue
+                collated_member_data = collated_data.get(member, Counter())
+                for v_key, value in member_data.items():
+                    collated_member_data[v_key] += value
+                collated_data[member] = collated_member_data
+        await self.send_leaderboard(ctx, collated_data, key, top)
+
+    def _get_sort_key(self, key: str):
+        key = key.lower()
+        if key in ("wins", "average_score", "total_score"):
+            return key
+        elif key in ("avg", "average"):
+            return "average_score"
+        elif key in ("total", "score", "answers", "correct"):
+            return "total_score"
+
+    async def send_leaderboard(self,
+                               ctx: commands.Context,
+                               data: dict,
+                               key: str,
+                               top: int):
+        """Send the leaderboard from the given data.
+
+        Parameters
+        ----------
+        ctx : commands.Context
+            The context to send the leaderboard to.
+        data : dict
+            The data for the leaderboard. This must map `discord.Member` ->
+            `dict`.
+        key : str
+            The field to sort the data by. Can be ``wins``, ``total_score``,
+            ``games`` or ``average_score``.
+        top : int
+            The number of members to display on the leaderboard.
+
+        Returns
+        -------
+        `list` of `discord.Message`
+            The sent leaderboard messages.
+
+        """
+        if not data:
+            await ctx.send("There are no scores on record!")
+            return
+        leaderboard = self._get_leaderboard(data, key, top)
+        ret = []
+        for page in pagify(leaderboard):
+            ret.append(await ctx.send(box(page, lang="py")))
+        return ret
+
+    def _get_leaderboard(self, data: dict, key: str, top: int):
+        # Mix in average score
+        for member, stats in data.items():
+            if stats["games"] != 0:
+                stats["average_score"] = stats["total_score"] / stats["games"]
+            else:
+                stats["average_score"] = 0.0
+        # Sort by reverse order of priority
+        priority = ["average_score", "total_score", "wins", "games"]
+        try:
+            priority.remove(key)
+        except ValueError:
+            raise ValueError("{} is not a valid key".format(key))
+        # Put key last in reverse priority
+        priority.append(key)
+        items = data.items()
+        for key in priority:
+            items = sorted(items, key=lambda t: t[1][key])
+        max_name_len = max(map(lambda m: len(str(m)), data.keys()))
+        # Headers
+        headers = ("Rank", "Member{}".format(" " * (max_name_len - 6)), "Wins",
+                   "Games Played", "Total Score", "Average Score")
+        lines = [" | ".join(headers)]
+        # Header underlines
+        lines.append(" | ".join(("-" * len(h) for h in headers)))
+        for rank, tup in enumerate(items, 1):
+            member, m_data = tup
+            # Align fields to header width
+            fields = tuple(map(str, (rank,
+                                     member,
+                                     m_data["wins"],
+                                     m_data["games"],
+                                     m_data["total_score"],
+                                     round(m_data["average_score"], 2))))
+            padding = [
+                " " * (len(h) - len(f)) for h, f in zip(headers, fields)
+            ]
+            fields = tuple(f + padding[i] for i, f in enumerate(fields))
+            lines.append(" | ".join(fields).format(member=member, **m_data))
+            if rank == top:
+                break
+        return "\n".join(lines)
 
     async def on_trivia_end(self, session: TriviaSession):
         """Event for a trivia session ending.
@@ -274,7 +434,7 @@ class Trivia:
             stats = await self.conf.member(member).all()
             if score == max_score:
                 stats["wins"] += 1
-            stats["correct_answers"] += score
+            stats["total_score"] += score
             stats["games"] += 1
             await self.conf.member(member).set(stats)
 
