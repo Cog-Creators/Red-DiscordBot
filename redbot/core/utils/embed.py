f33d1bcc79ff6e7d4f095268b55c0ef059d5d1b0
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2017-12-11 14:01:08 -0900

    [V3 Utils] random_colour -> randomize_colour + make it take an embed

--- redbot/core/utils/embed.py
+++ redbot/core/utils/embed.py
@@ -3,21 +3,28 @@ import discord
 import random
 
 
-def random_colour() -> discord.Colour:
+def randomize_colour(embed: discord.Embed) -> discord.Embed:
     """
-    Get a random colour for use in an embed
-    There is an alias for this called random_color
+    Gives the provided embed a random color.
+    There is an alias for this called randomize_color
+
+    Parameters
+    ----------
+    embed : discord.Embed
+        The embed to add a color to
 
     Returns
     -------
-    discord.Colour
-        The random colour
+    discord.Embed
+        The embed with the color set to a random color
+
     """
-    return discord.Color(
+    embed.colour = discord.Color(
         value=int(
             ''.join([random.choice('0123456789ABCDEF') for x in range(6)]), 16
         )
     )
+    return embed
 
 
-random_color = random_colour
+randomize_color = randomize_colour
