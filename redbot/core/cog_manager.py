9f8a008442917d23aa9fd53162b5aaf220569ba5
Haruki N. Grande <jordan99oliver@outlook.com>
2018-02-15 00:57:17 +0000

    [V3 CogManager] Fixed typo (#1285)

--- redbot/core/cog_manager.py
+++ redbot/core/cog_manager.py
@@ -321,7 +321,7 @@ class CogManagerUI:
         Add a path to the list of available cog paths.
         """
         if not path.is_dir():
-            await ctx.send(_("That path is does not exist or does not"
+            await ctx.send(_("That path does not exist or does not"
                              " point to a valid directory."))
             return
 
