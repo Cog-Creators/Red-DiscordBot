4ce1cef9cb621abd6f2c47807d42c8d26824765b
Thomas Mercurio <tmercswims@gmail.com>
2018-02-27 11:21:29 -0800

    Correct connection string without credentials

--- redbot/core/drivers/red_mongo.py
+++ redbot/core/drivers/red_mongo.py
@@ -13,11 +13,16 @@ def _initialize(**kwargs):
     admin_user = kwargs['USERNAME']
     admin_pass = kwargs['PASSWORD']
     db_name = kwargs.get('DB_NAME', 'default_db')
-
-    url = "mongodb://{}:{}@{}:{}/{}".format(
-        admin_user, admin_pass, host, port,
-        db_name
-    )
+    
+    if admin_user is not None and admin_pass is not None:
+        url = "mongodb://{}:{}@{}:{}/{}".format(
+            admin_user, admin_pass, host, port,
+            db_name
+        )
+    else:
+        url = "mongodb://{}:{}/{}".format(
+            host, port, db_name
+        )
 
     global _conn
     _conn = motor.motor_asyncio.AsyncIOMotorClient(url)
