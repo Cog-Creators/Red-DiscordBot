c627ff06f5aca39445b18e6b0099a887301f12a7
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2018-03-22 20:14:09 -0800

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Red - Discord Bot'
-copyright = '2017, Cog Creators'
+copyright = '2018, Cog Creators'
 author = 'Cog Creators'
 
 # The version info for the project you're documenting, acts as replacement for
