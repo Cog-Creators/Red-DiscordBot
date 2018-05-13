bf1300dd712022518bb85d328cc81cd856442e45
Tobotimus <tobyharradine@live.com>
2018-05-13 23:00:14 +1000

    Fix reference in tunnel.py

--- redbot/core/utils/tunnel.py
+++ redbot/core/utils/tunnel.py
@@ -112,7 +112,7 @@ class Tunnel(metaclass=TunnelMeta):
             The message content
         embed: `discord.Embed`
             The embed to send
-        files: `List[discord.Files]`
+        files: `list` of `discord.File`
             A list of files to send.
 
         Returns
