4c60e552b0c188f28e8c2d28fb4585ca771340f6
Bobloy <alboblexloy@gmail.com>
2018-05-14 17:02:43 -0400

    Do nothing with error

--- redbot/cogs/downloader/repo_manager.py
+++ redbot/cogs/downloader/repo_manager.py
@@ -129,9 +129,6 @@ class Repo(RepoJSONMixin):
 
         return p.stdout.decode().strip()
 
-    def _module_error(self, name: str):
-        print("Error importing module {}".format(name))
-
     def _update_available_modules(self) -> Tuple[str]:
         """
         Updates the available modules attribute for this repo.
@@ -149,7 +146,7 @@ class Repo(RepoJSONMixin):
                         Installable(location=name)
                     )
         """
-        for file_finder, name, is_pkg in pkgutil.walk_packages(path=[str(self.folder_path), ], onerror=self._module_error):
+        for file_finder, name, is_pkg in pkgutil.walk_packages(path=[str(self.folder_path), ], onerror=lambda name: None):
             curr_modules.append(
                 Installable(location=self.folder_path / name)
             )
