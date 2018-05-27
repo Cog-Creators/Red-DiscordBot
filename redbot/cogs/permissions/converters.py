54975eb8126b04cee82287e3cbae663763163dce
Michael H <Sinbad@michaelhall.tech>
2018-05-27 18:17:17 -0400

    [V3] Permissions (#1548)
    * This starts setting up checks.py to handle managed permission overrides
    
    * A decent starting point, more work to come
    
    * missing else fix
    
    * more work on this
    
    * reduce redundant code
    
    * More work on this...
    
    * more progress
    
    * add a debug flag to some things in .resolvers to help with exploring why checks behave in a certain way
    
    * modify this to be a list for ease of showing full resolution order
    
    * more
    
    * don't bypass is_owner, ever
    
    * remove old logic about ownercommands
    
    * better handling of chec validity
    
    * anonymous functions return None for __module__, remove some code as a result
    
    * mutable default bind fix
    
    * Add a caching layer (to be invalidated as needed)
    
    Ensure checks in the chain inserted before the core logic only return None or False
    (whitelists then blacklists are checked first in core logic, from most to least specific scope, overriding this with an allow does not make sense)
    
    * more progress, slow work as I have time
    
    * Modifies the predicates so that their inner functions are accesible from cogs without
    being a check
    
    * Update checks.py
    
    Safety for existing permissions.py cogs
    
    * This is where I take a change of course on setting this up,
    because this would have been the most long winded interactive command ever as
    it was starting to progress.
    
    This is going to support individual entry updates, settings from yaml, gettings, and clearing existing settings
    as well as printing a settings template out and referring people to what is going to be very well written docs
    
    * block permissions cog from being unblocked by the permissions cog as a safety feature (really, co-owner exists at this point)
    
    * WIP
    
    * Okay, this has the intent of the changes, just to actually test these as working as intended + add corresponding guild functions
    
    * oh nice, missed a couple files, sec...
    
    * WIP, also, something's broken in resolvers or check_overrides >>
    
    * This is working now (still needs docs and more...)
    
    * unmerge changes from other PR
    
    * is_owner still needs to exist in here due to management of non checked commands
    
    * Update this to new style standards
    
    * forgot to commit some local changes earlier
    
    * fix update logic
    
    * fix update logic
    
    * b14 fix, lol
    
    * fix issue with management command name
    
    * this isnt a real fix
    
    * Ok..
    
    * perms
    
    * This is working, but needs docs and more configuration opts now
    
    * more
    
    * Ux functions, need testing
    
    * style
    
    * fix using the obj str rather than the id
    
    * fix CogOrCommand converter
    
    * Return the correct things in the converter
    
    * last fix, needs docs, and possibly some extra Ux utils
    
    * start doc writing
    
    * extra user facing commands
    
    * yaml docs
    
    * yaml fix
    
    * secondary checks-fix
    
    * 3rd party check stuff
    
    * remove warning that this isn't ready yet
    
    * swap ctx.tick for real responses, require emoji perms for interactive menuing, better attr handling for nicknames
    
    * send file to author
    
    * alias to `p`
    
    * more ctx tick removal
    
    (This is a long ass changelog...)

---
+++ redbot/cogs/permissions/converters.py
@@ -0,0 +1,26 @@
+from redbot.core import commands
+from typing import Tuple
+
+
+class CogOrCommand(commands.Converter):
+
+    async def convert(self, ctx: commands.Context, arg: str) -> Tuple[str]:
+        ret = ctx.bot.get_cog(arg)
+        if ret:
+            return "cogs", ret.__class__.__name__
+        ret = ctx.bot.get_command(arg)
+        if ret:
+            return "commands", ret.qualified_name
+
+        raise commands.BadArgument()
+
+
+class RuleType(commands.Converter):
+
+    async def convert(self, ctx: commands.Context, arg: str) -> str:
+        if arg.lower() in ("allow", "whitelist", "allowed"):
+            return "allow"
+        if arg.lower() in ("deny", "blacklist", "denied"):
+            return "deny"
+
+        raise commands.BadArgument()
