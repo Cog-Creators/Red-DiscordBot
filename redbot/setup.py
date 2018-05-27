f8a9b9cd3eae4c6b8a80ce2eb2f1692686d65986
palmtree5 <3577255+palmtree5@users.noreply.github.com>
2018-05-27 12:31:41 -0800

    Fix issue causing style check to fail

--- redbot/setup.py
+++ redbot/setup.py
@@ -320,6 +320,7 @@ async def create_backup(selected, instance_data):
 async def remove_instance(selected, instance_data):
     if instance_data["STORAGE_TYPE"] == "MongoDB":
         from redbot.core.drivers.red_mongo import Mongo
+        
         m = Mongo("Core", **instance_data["STORAGE_DETAILS"])
         db = m.db
         collections = await db.collection_names(include_system_collections=False)
