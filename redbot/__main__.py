a89a22036e484184a1cb76998f4afd3d279d5566
michael <michael@michaelhall.tech>
2018-05-27 21:54:10 -0400

    rpc close removed

--- redbot/__main__.py
+++ redbot/__main__.py
@@ -164,7 +164,6 @@ def main():
         sentry_log.critical("Fatal Exception", exc_info=e)
         loop.run_until_complete(red.logout())
     finally:
-        red.rpc.close()
         if cleanup_tasks:
             pending = asyncio.Task.all_tasks(loop=red.loop)
             gathered = asyncio.gather(*pending, loop=red.loop, return_exceptions=True)
