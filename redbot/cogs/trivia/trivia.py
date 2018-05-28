f6633927d6fde674b209d383c6bdec56290b62fb
Redjumpman <Redjumpman@users.noreply.github.com>
2018-05-28 03:45:10 -0400

    Update trivia.py
    Fixed some missing punctuation.

--- redbot/cogs/trivia/trivia.py
+++ redbot/cogs/trivia/trivia.py
@@ -382,7 +382,7 @@ class Trivia:
         try:
             priority.remove(key)
         except ValueError:
-            raise ValueError("{} is not a valid key".format(key))
+            raise ValueError("{} is not a valid key.".format(key))
         # Put key last in reverse priority
         priority.append(key)
         items = data.items()
@@ -480,7 +480,7 @@ class Trivia:
         try:
             path = next(p for p in self._all_lists() if p.stem == category)
         except StopIteration:
-            raise FileNotFoundError("Could not find the `{}` category" "".format(category))
+            raise FileNotFoundError("Could not find the `{}` category.".format(category))
 
         with path.open(encoding="utf-8") as file:
             try:
