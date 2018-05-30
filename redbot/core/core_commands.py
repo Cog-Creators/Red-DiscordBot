9e5a9afe47691b151d15586dd1710183593f5cf2
michael <michael@michaelhall.tech>
2018-05-30 14:50:12 -0400

    issue #1741 fix

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -515,30 +515,31 @@ class Core:
     async def _set(self, ctx):
         """Changes Red's settings"""
         if ctx.invoked_subcommand is None:
-            admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
-            admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id)
-            mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
-            mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id)
-            prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
-            if not prefixes:
-                prefixes = await ctx.bot.db.prefix()
-            locale = await ctx.bot.db.locale()
+            if ctx.guild:
+                admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
+                admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id)
+                mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
+                mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id)
+                prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
+                if not prefixes:
+                    prefixes = await ctx.bot.db.prefix()
+                locale = await ctx.bot.db.locale()
 
-            settings = (
-                "{} Settings:\n\n"
-                "Prefixes: {}\n"
-                "Admin role: {}\n"
-                "Mod role: {}\n"
-                "Locale: {}"
-                "".format(
-                    ctx.bot.user.name,
-                    " ".join(prefixes),
-                    admin_role.name if admin_role else "Not set",
-                    mod_role.name if mod_role else "Not set",
-                    locale,
+                settings = (
+                    "{} Settings:\n\n"
+                    "Prefixes: {}\n"
+                    "Admin role: {}\n"
+                    "Mod role: {}\n"
+                    "Locale: {}"
+                    "".format(
+                        ctx.bot.user.name,
+                        " ".join(prefixes),
+                        admin_role.name if admin_role else "Not set",
+                        mod_role.name if mod_role else "Not set",
+                        locale,
+                    )
                 )
-            )
-            await ctx.send(box(settings))
+                await ctx.send(box(settings))
             await ctx.send_help()
 
     @_set.command()
