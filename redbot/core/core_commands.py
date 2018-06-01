54a95a26838b916c3c24fa3ff9379331a3bb6606
Michael H <michael@michaelhall.tech>
2018-06-01 16:04:25 -0400

    requested changes

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -517,29 +517,25 @@ class Core:
         if ctx.invoked_subcommand is None:
             if ctx.guild:
                 admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
-                admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id)
+                admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id) or "Not set"
                 mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
-                mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id)
+                mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id) or "Not set"
                 prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
-                if not prefixes:
-                    prefixes = await ctx.bot.db.prefix()
-                locale = await ctx.bot.db.locale()
+                guild_settings = f"Admin role: {admin_role}\nMod role: {mod_role}\n"
+            else:
+                guild_settings = ""
+            if not prefixes:
+                prefixes = await ctx.bot.db.prefix()
+            locale = await ctx.bot.db.locale()
 
-                settings = (
-                    "{} Settings:\n\n"
-                    "Prefixes: {}\n"
-                    "Admin role: {}\n"
-                    "Mod role: {}\n"
-                    "Locale: {}"
-                    "".format(
-                        ctx.bot.user.name,
-                        " ".join(prefixes),
-                        admin_role.name if admin_role else "Not set",
-                        mod_role.name if mod_role else "Not set",
-                        locale,
-                    )
-                )
-                await ctx.send(box(settings))
+            prefix_string = " ".join(prefixes)
+            settings = (
+                f"{ctx.bot.user.name} Settings:\n\n"
+                f"Prefixes: {prefix_string}\n"
+                f"{guild_settings}"
+                f"Locale: {locale}"
+            )
+            await ctx.send(box(settings))
             await ctx.send_help()
 
     @_set.command()
