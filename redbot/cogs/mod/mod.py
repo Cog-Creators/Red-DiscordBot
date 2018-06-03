bcde0528d79d1a6ed8503842e5c9fcc3d897cb41
Michael H <michael@michaelhall.tech>
2018-06-03 18:57:33 -0400

    spelling

--- redbot/cogs/mod/mod.py
+++ redbot/cogs/mod/mod.py
@@ -317,7 +317,7 @@ class Mod:
             )
             return
         elif ctx.guild.me.top_role <= user.top_role or user == ctx.guild.owner:
-            await ctx.send(_("I cannot do that due to discord heirarchy rules"))
+            await ctx.send(_("I cannot do that due to discord hierarchy rules"))
             return
         audit_reason = get_audit_reason(author, reason)
         try:
@@ -373,7 +373,7 @@ class Mod:
             )
             return
         elif ctx.guild.me.top_role <= user.top_role or user == ctx.guild.owner:
-            await ctx.send(_("I cannot do that due to discord heirarchy rules"))
+            await ctx.send(_("I cannot do that due to discord hierarchy rules"))
             return
 
         if days:
