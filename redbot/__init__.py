4d54e063e73abc958887873ed8a0927e5a9c47f7
El Laggron <laggron42@gmail.com>
2018-06-05 21:32:37 +0200

    [V3] Root user warning

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -21,3 +21,12 @@ if sys.version_info < (3, 6, 0):
         " Please update your environment to Python 3.6 as soon as possible to avoid"
         " any interruptions after the beta 16 release."
     )
+    
+if os.geteuid() == 0:
+    print(Back.RED + "[SECURITY WARNING]")
+    print(
+        Back.RED + "You are running Red as a root user.\n"
+        "It is recommanded to exit now and run Red as a normal user. "
+        "This has serious security repercussion. The bot will have access to ALL files "
+        "and can lead to fatal damages on your computer."
+    )
