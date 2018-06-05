eda02706eb5b511f21ae9ac8c4e28f5e7d711c37
Michael H <michael@michaelhall.tech>
2018-06-05 16:28:47 -0400

    async gen

--- redbot/cogs/permissions/permissions.py
+++ redbot/cogs/permissions/permissions.py
@@ -196,7 +196,7 @@ class Permissions:
             try:
                 testcontext = await self.bot.get_context(message, cls=commands.Context)
                 can = await com.can_run(testcontext) and all(
-                    await p.can_run(testcontext) for p in com.parents
+                    [await p.can_run(testcontext) for p in com.parents]
                 )
             except commands.CheckFailure:
                 can = False
