39bf7bb5388fd6f2dc22cc953d809654abc725c5
Tobotimus <tobyharradine@live.com>
2018-06-16 14:56:57 +1000

    [V3] Remove requirements.txt and declare in install_requires

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
-from distutils.core import setup
-from distutils import ccompiler
-from distutils.errors import CCompilerError, DistutilsPlatformError
-from pathlib import Path
+import distutils.ccompiler as ccompiler
 import re
 import tempfile
-
-import os
-import sys
-
-from setuptools import find_packages
-
-IS_TRAVIS = "TRAVIS" in os.environ
-IS_DEPLOYING = "DEPLOYING" in os.environ
-IS_RTD = "READTHEDOCS" in os.environ
+from distutils.errors import CCompilerError, DistutilsPlatformError
+from pathlib import Path
+from setuptools import setup, find_packages
 
 dep_links = [
-    "https://github.com/Rapptz/discord.py/tarball/7eb918b19e3e60b56eb9039eb267f8f3477c5e17#egg=discord.py-1.0"
+    (
+        "https://github.com/Rapptz/discord.py/tarball/"
+        "7eb918b19e3e60b56eb9039eb267f8f3477c5e17#egg=discord.py-1.0"
+    )
 ]
-if IS_TRAVIS:
-    dep_links = []
+
+requirements = [
+    "aiohttp-json-rpc==0.8.7",
+    "aiohttp==2.2.5",
+    "appdirs==1.4.3",
+    "async-timeout==2.0.1",
+    "chardet==3.0.4",
+    "colorama==0.3.9",
+    "distro==1.3.0; sys_platform == 'linux'",
+    "fuzzywuzzy==0.16.0",
+    "idna==2.7",
+    "multidict==4.3.1",
+    "python-levenshtein==0.12.0",
+    "pyyaml==3.12",
+    "raven==6.5.0",
+    "red-trivia==1.1.1",
+    "websockets==3.4",
+    "yarl==0.18.0",
+]
 
 
 def get_package_list():
@@ -38,26 +49,6 @@ def check_compiler_available():
             except (CCompilerError, DistutilsPlatformError):
                 return False
     return True
-
-
-def get_requirements():
-    with open("requirements.txt") as f:
-        requirements = f.read().splitlines()
-    try:
-        requirements.remove(
-            "git+https://github.com/Rapptz/discord.py.git@rewrite#egg=discord.py[voice]"
-        )
-    except ValueError:
-        pass
-
-    if not check_compiler_available():  # Can't compile python-Levensthein, so drop extra
-        requirements.remove("fuzzywuzzy[speedup]<=0.16.0")
-        requirements.append("fuzzywuzzy<=0.16.0")
-    if IS_DEPLOYING or not (IS_TRAVIS or IS_RTD):
-        requirements.append("discord.py>=1.0.0a0")
-    if sys.platform.startswith("linux"):
-        requirements.append("distro")
-    return requirements
 
 
 def get_version():
@@ -97,44 +88,49 @@ def find_locale_folders():
     return ret
 
 
-setup(
-    name="Red-DiscordBot",
-    version=get_version(),
-    packages=get_package_list(),
-    package_data=find_locale_folders(),
-    include_package_data=True,
-    url="https://github.com/Cog-Creators/Red-DiscordBot",
-    license="GPLv3",
-    author="Cog-Creators",
-    author_email="",
-    description="A highly customizable Discord bot",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: AsyncIO",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Communications :: Chat",
-        "Topic :: Documentation :: Sphinx",
-    ],
-    entry_points={
-        "console_scripts": [
-            "redbot=redbot.__main__:main",
-            "redbot-setup=redbot.setup:main",
-            "redbot-launcher=redbot.launcher:main",
-        ]
-    },
-    python_requires=">=3.6,<3.7",
-    setup_requires=get_requirements(),
-    install_requires=get_requirements(),
-    dependency_links=dep_links,
-    extras_require={
-        "test": ["pytest>3", "pytest-asyncio"],
-        "mongo": ["motor"],
-        "docs": ["sphinx>=1.7", "sphinxcontrib-asyncio", "sphinx_rtd_theme"],
-        "voice": ["red-lavalink>=0.0.4"],
-        "style": ["black==18.5b1"],
-    },
-)
+if __name__ == "__main__":
+    if not check_compiler_available():
+        requirements.remove(
+            next(r for r in requirements if r.lower().startswith("python-levenshtein"))
+        )
+
+    setup(
+        name="Red-DiscordBot",
+        version=get_version(),
+        packages=get_package_list(),
+        package_data=find_locale_folders(),
+        include_package_data=True,
+        url="https://github.com/Cog-Creators/Red-DiscordBot",
+        license="GPLv3",
+        author="Cog-Creators",
+        author_email="",
+        description="A highly customizable Discord bot",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Framework :: AsyncIO",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Communications :: Chat",
+            "Topic :: Documentation :: Sphinx",
+        ],
+        entry_points={
+            "console_scripts": [
+                "redbot=redbot.__main__:main",
+                "redbot-setup=redbot.setup:main",
+                "redbot-launcher=redbot.launcher:main",
+            ]
+        },
+        python_requires=">=3.6,<3.7",
+        install_requires=requirements,
+        dependency_links=dep_links,
+        extras_require={
+            "test": ["pytest>3", "pytest-asyncio"],
+            "mongo": ["motor"],
+            "docs": ["sphinx>=1.7", "sphinxcontrib-asyncio", "sphinx_rtd_theme"],
+            "voice": ["red-lavalink>=0.0.4"],
+            "style": ["black==18.5b1"],
+        },
+    )
