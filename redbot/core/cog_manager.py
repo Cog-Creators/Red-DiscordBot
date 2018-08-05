c19cbb984b5b8eec068b8f4c6880036d9ddd1b32
Toby Harradine <tobyharradine@gmail.com>
2018-08-05 17:23:22 +1000

    [CogManager] Paths should default to a list

--- redbot/core/cog_manager.py
+++ redbot/core/cog_manager.py
@@ -41,7 +41,7 @@ class CogManager:
         self.conf = Config.get_conf(self, 2938473984732, True)
         tmp_cog_install_path = cog_data_path(self) / "cogs"
         tmp_cog_install_path.mkdir(parents=True, exist_ok=True)
-        self.conf.register_global(paths=(), install_path=str(tmp_cog_install_path))
+        self.conf.register_global(paths=[], install_path=str(tmp_cog_install_path))
         self._paths = [Path(p) for p in paths]
 
     async def paths(self) -> Tuple[Path, ...]:
