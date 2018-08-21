133a70e6ef6d11c324557c460eb4428c48b6c92e
Michael H <michael@michaelhall.tech>
2018-08-20 22:19:32 -0400

    put back in try/except block

--- redbot/cogs/cleanup/cleanup.py
+++ redbot/cogs/cleanup/cleanup.py
@@ -244,11 +244,10 @@ class Cleanup:
             await ctx.send(_("This command can only be used on bots with bot accounts."))
             return
 
-        after = await channel.get_message(message_id)
-
-        if not after:
-            await ctx.send(_("Message not found."))
-            return
+        try:
+            after = await channel.get_message(message_id)
+        except discord.NotFound:
+            return await ctx.send(_("Message not found."))
 
         to_delete = await self.get_messages_for_deletion(
             channel=channel, number=None, after=after, delete_pinned=delete_pinned
