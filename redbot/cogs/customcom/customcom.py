27b0d606c8ea3e5f556fd770133c465b700b07de
Toby Harradine <Tobotimus@users.noreply.github.com>
2018-09-10 00:35:53 +1000

    Fix CCs with no args (#2114)
    Signed-off-by: Toby Harradine <tobyharradine@gmail.com>

--- redbot/cogs/customcom/customcom.py
+++ redbot/cogs/customcom/customcom.py
@@ -349,11 +349,12 @@ class CustomCommands:
             param = self.transform_parameter(result, ctx.message)
             raw_response = raw_response.replace("{" + result + "}", param)
         results = re.findall(r"\{((\d+)[^\.}]*(\.[^:}]+)?[^}]*)\}", raw_response)
-        low = min(int(result[1]) for result in results)
-        for result in results:
-            index = int(result[1]) - low
-            arg = self.transform_arg(result[0], result[2], cc_args[index])
-            raw_response = raw_response.replace("{" + result[0] + "}", arg)
+        if results:
+            low = min(int(result[1]) for result in results)
+            for result in results:
+                index = int(result[1]) - low
+                arg = self.transform_arg(result[0], result[2], cc_args[index])
+                raw_response = raw_response.replace("{" + result[0] + "}", arg)
         await ctx.send(raw_response)
 
     def prepare_args(self, raw_response) -> Mapping[str, Parameter]:
