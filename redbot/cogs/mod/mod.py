2f8e7bfefe1bdd91ea93e682293c2d965e8355cf
Toby Harradine <Tobotimus@users.noreply.github.com>
2018-09-24 18:10:52 +1000

    Use ctx.invoke instead of Command.callback
    Would prefer being consistent with the use of this method, and also prevents warnings about unresolved attributes.

--- redbot/cogs/mod/mod.py
+++ redbot/cogs/mod/mod.py
@@ -452,8 +452,7 @@ class Mod:
         user = guild.get_member(user_id)
         if user is not None:
             # Instead of replicating all that handling... gets attr from decorator
-            # pylint: disable=E1101
-            return await self.ban.callback(self, ctx, user, None, reason=reason)
+            return await ctx.invoke(self.ban, user, None, reason=reason)
         user = discord.Object(id=user_id)  # User not in the guild, but
 
         audit_reason = get_audit_reason(author, reason)
