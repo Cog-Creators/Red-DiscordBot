7ab52d4f225a5b8d45cbb9d3c83d89d7271eb722
Toby <tobyharradine@gmail.com>
2018-09-25 23:08:58 +1000

    Reformat
    Signed-off-by: Toby <tobyharradine@gmail.com>

--- redbot/pytest/permissions.py
+++ redbot/pytest/permissions.py
@@ -3,6 +3,7 @@ import pytest
 from redbot.cogs.permissions import Permissions
 from redbot.core import Config
 
+
 @pytest.fixture()
 def permissions(config, monkeypatch, red):
     with monkeypatch.context() as m:
