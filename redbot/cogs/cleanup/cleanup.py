bfae5c603f228542a2b8c8e07d933ff9c0c88312
bobloy <alboblexloy@gmail.com>
2018-10-02 09:24:58 -0400

    Cleanup Before command

--- redbot/cogs/cleanup/cleanup.py
+++ redbot/cogs/cleanup/cleanup.py
@@ -248,6 +248,42 @@ class Cleanup:
 
     @cleanup.command()
     @commands.guild_only()
+    async def before(
+        self, ctx: commands.Context, message_id: int, number: int, delete_pinned: bool = False
+    ):
+        """Deletes X messages before specified message.
+
+        To get a message id, enable developer mode in Discord's
+        settings, 'appearance' tab. Then right click a message
+        and copy its id.
+
+        This command only works on bots running as bot accounts.
+        """
+
+        channel = ctx.channel
+        if not channel.permissions_for(ctx.guild.me).manage_messages:
+            await ctx.send("I need the Manage Messages permission to do this.")
+            return
+        author = ctx.author
+
+        try:
+            before = await channel.get_message(message_id)
+        except discord.NotFound:
+            return await ctx.send(_("Message not found."))
+
+        to_delete = await self.get_messages_for_deletion(
+            channel=channel, number=number, before=before, delete_pinned=delete_pinned
+        )
+
+        reason = "{}({}) deleted {} messages in channel {}.".format(
+            author.name, author.id, len(to_delete), channel.name
+        )
+        log.info(reason)
+
+        await mass_purge(to_delete, channel)
+
+    @cleanup.command()
+    @commands.guild_only()
     async def messages(self, ctx: commands.Context, number: int, delete_pinned: bool = False):
         """Deletes last X messages.
 
