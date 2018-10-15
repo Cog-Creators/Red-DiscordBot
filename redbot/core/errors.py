d2d26835c3a5a38a8ebccfdbe1ce194629191b1d
Toby Harradine <Tobotimus@users.noreply.github.com>
2018-10-16 09:30:53 +1100

    [Economy] Detect max balance and prevent OverflowError (#2211)
    Resolves #2091.
    
    This doesn't fix every OverflowError with MongoDB; but at least the seemingly easiest one to achieve with core cogs.
    
    Signed-off-by: Toby Harradine <tobyharradine@gmail.com>

--- redbot/core/errors.py
+++ redbot/core/errors.py
@@ -1,4 +1,11 @@
 import importlib.machinery
+from typing import Optional
+
+import discord
+
+from .i18n import Translator
+
+_ = Translator(__name__, __file__)
 
 
 class RedError(Exception):
@@ -14,3 +21,24 @@ class PackageAlreadyLoaded(RedError):
 
     def __str__(self) -> str:
         return f"There is already a package named {self.spec.name.split('.')[-1]} loaded"
+
+
+class BankError(RedError):
+    """Base error class for bank-related errors."""
+
+
+class BalanceTooHigh(BankError, OverflowError):
+    """Raised when trying to set a user's balance to higher than the maximum."""
+
+    def __init__(
+        self, user: discord.abc.User, max_balance: int, currency_name: str, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.user = user
+        self.max_balance = max_balance
+        self.currency_name = currency_name
+
+    def __str__(self) -> str:
+        return _("{user}'s balance cannot rise above {max:,} {currency}.").format(
+            user=self.user, max=self.max_balance, currency=self.currency_name
+        )
