61501f6e42ab5b7eb93b298c6d9aebb2a13a654f
Toby Harradine <tobyharradine@gmail.com>
2018-10-16 18:51:59 +1100

    Move __checks assignment
    Signed-off-by: Toby Harradine <tobyharradine@gmail.com>

--- redbot/core/commands/errors.py
+++ redbot/core/commands/errors.py
@@ -1,9 +1,14 @@
 """Errors module for the commands package."""
 import inspect
+from typing import TYPE_CHECKING, Any
+
 import discord
 from discord.ext import commands
 
-__all__ = ["ConversionFailure", "BotMissingPermissions"]
+if TYPE_CHECKING:
+    from .cog import CogMeta
+
+__all__ = ["ConversionFailure", "BotMissingPermissions", "CogError", "OverrideNotAllowed"]
 
 
 class ConversionFailure(commands.BadArgument):
@@ -22,3 +27,22 @@ class BotMissingPermissions(commands.CheckFailure):
     def __init__(self, missing: discord.Permissions, *args):
         self.missing: discord.Permissions = missing
         super().__init__(*args)
+
+
+class CogError(Exception):
+    """Base exception class for errors relating to cogs."""
+
+
+class OverrideNotAllowed(CogError):
+    """Raised when a cog class tries to override base cog methods.
+
+    Methods and attributes of `commands.Cog` have special meaning and
+    should not be overridden.
+    """
+
+    def __init__(self, cog_cls: "CogMeta", name: str) -> None:
+        self.cog_cls = cog_cls
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"{self.name} is a reserved cog class attribute"
