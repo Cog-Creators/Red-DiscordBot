9b9426e301688f7019f635ecf80d314f41fa2796
Michael Hall <michael@michaelhall.tech>
2019-01-17 14:47:22 -0500

    quick extra comment of importance

--- redbot/core/commands/requires.py
+++ redbot/core/commands/requires.py
@@ -93,6 +93,10 @@ DM_PERMS.update(
 class PrivilegeLevel(enum.IntEnum):
     """Enumeration for special privileges."""
 
+    # Maintainer Note: do NOT re-order these.
+    # Each privelege level also implies access to the ones before it.
+    # Inserting new privelege levels at a later point is fine if that is considered.
+
     NONE = enum.auto()
     """No special privilege level."""
 
