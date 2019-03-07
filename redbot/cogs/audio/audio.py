1c22b212c256f7c7d79d2f6552fbb0dabe670444
aikaterna <20862007+aikaterna@users.noreply.github.com>
2019-03-06 19:12:15 -0800

    [Audio] Add songs when search-queuing (#2513)
    Searching for a song and pressing the reaction to queue a song would not add the song to the queue if `[p]audioset maxlength` was off. This was an omission from #2465.

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -2212,6 +2212,8 @@ class Audio(commands.Cog):
                 player.add(ctx.author, search_choice)
             else:
                 return await self._embed_msg(ctx, _("Track exceeds maximum length."))
+        else:
+            player.add(ctx.author, search_choice)
         if not player.current:
             await player.play()
         await ctx.send(embed=embed)
