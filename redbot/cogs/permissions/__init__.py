d64dbef379c74027dfeec8d19c80168731667f36
NeuroAssassin <wrbrown70@yahoo.com>
2019-03-12 08:58:24 -0400

    Update permissions to adhere to new event name change

--- redbot/cogs/permissions/__init__.py
+++ redbot/cogs/permissions/__init__.py
@@ -8,6 +8,6 @@ async def setup(bot):
     # the permissions commands themselves have rules added.
     # Automatic listeners being added in add_cog happen in arbitrary
     # order, so we want to circumvent that.
-    bot.add_listener(cog.cog_added, "on_cog_add")
+    bot.add_listener(cog.cog_added, "on_red_cog_add")
     bot.add_listener(cog.command_added, "on_command_add")
     bot.add_cog(cog)
