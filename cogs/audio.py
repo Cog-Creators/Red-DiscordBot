92c4f148b5e38f3d4532b22631948266a5ad8d62
Angel Hiraldo <ahiraldo41@gmail.com>
2019-03-31 22:38:04 -0400

    added despacito 4 the roblox version
    sorry couldnt find the fortnite version

--- cogs/audio.py
+++ cogs/audio.py
@@ -2071,7 +2071,7 @@ class Audio:
     @commands.command(pass_context=True,aliases=["alexaplaydespacito"], no_pm=True)
     async def despacito(self, ctx):
         """This is so sad Alexa play despacito \n\nWARNING: you might get earraped in this command"""
-        ids = ("FXovf5dsRTw", "W3GrSMYbkBE", "Gl6ekgobG2k", "X3XnZxMB0sA", "zYsLq_tcktk", "Q6KJjE30hE4", "FkkPhbwgDI0")
+        ids = ("FXovf5dsRTw", "W3GrSMYbkBE", "Gl6ekgobG2k", "X3XnZxMB0sA", "zYsLq_tcktk", "Q6KJjE30hE4", "FkkPhbwgDI0", "jqtJLyQRqIc")
         url = "https://www.youtube.com/watch?v={}".format(choice(ids))
         await ctx.invoke(self.play, url_or_search_terms=url)
 
