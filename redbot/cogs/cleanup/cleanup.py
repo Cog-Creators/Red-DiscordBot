4bc6b5e0ff34cc458f92b7a05f3981e54ff9de48
Neuro Assassin <wrbrown70@yahoo.com>
2019-04-26 09:05:32 -0400

    Create converter for message IDs; remove f-strings; use converters in a few extra commands

--- redbot/cogs/cleanup/cleanup.py
+++ redbot/cogs/cleanup/cleanup.py
@@ -10,6 +10,7 @@ from redbot.core.i18n import Translator, cog_i18n
 from redbot.core.utils.mod import slow_deletion, mass_purge
 from redbot.cogs.mod.log import log
 from redbot.core.utils.predicates import MessagePredicate
+from .converters import RawMessageIds
 
 _ = Translator("Cleanup", __file__)
 
@@ -211,7 +212,7 @@ class Cleanup(commands.Cog):
     @cleanup.command()
     @commands.guild_only()
     @commands.bot_has_permissions(manage_messages=True)
-    async def after(self, ctx: commands.Context, message_id: int, delete_pinned: bool = False):
+    async def after(self, ctx: commands.Context, message_id: RawMessageIds, delete_pinned: bool = False):
         """Delete all messages after a specified message.
 
         To get a message id, enable developer mode in Discord's
@@ -242,7 +243,7 @@ class Cleanup(commands.Cog):
     @commands.guild_only()
     @commands.bot_has_permissions(manage_messages=True)
     async def before(
-        self, ctx: commands.Context, message_id: int, number: int, delete_pinned: bool = False
+        self, ctx: commands.Context, message_id: RawMessageIds, number: int, delete_pinned: bool = False
     ):
         """Deletes X messages before specified message.
 
@@ -275,7 +276,7 @@ class Cleanup(commands.Cog):
     @commands.guild_only()
     @commands.bot_has_permissions(manage_messages=True)
     async def between(
-        self, ctx: commands.Context, one: int, two: int, delete_pinned: bool = False
+        self, ctx: commands.Context, one: RawMessageIds, two: RawMessageIds, delete_pinned: bool = False
     ):
         """Delete the messages between Messsage One and Message Two, providing the messages IDs.
 
@@ -289,11 +290,11 @@ class Cleanup(commands.Cog):
         try:
             mone = await channel.fetch_message(one)
         except discord.errors.Notfound:
-            return await ctx.send(f"Could not find a message with the ID of {one}.")
+            return await ctx.send(_("Could not find a message with the ID of {id}.".format(id=one)))
         try:
             mtwo = await channel.fetch_message(two)
         except discord.errors.Notfound:
-            return await ctx.send(f"Could not find a message with the ID of {two}.")
+            return await ctx.send(_("Could not find a message with the ID of {id}.".format(id=two)))
         to_delete = await self.get_messages_for_deletion(
             channel=channel, before=mtwo, after=mone, delete_pinned=delete_pinned
         )
