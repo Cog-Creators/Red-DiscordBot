588b3608e4c559316af71bc6e90c38854398fb9c
Michael H <michael@michaelhall.tech>
2019-05-15 20:14:17 -0400

    Add DM forbidden handling

--- redbot/core/commands/help.py
+++ redbot/core/commands/help.py
@@ -494,17 +494,35 @@ class RedHelpFormatter:
         Sends pages based on settings.
         """
 
-        if not await ctx.bot.db.help.use_menus():
+        if not (
+            ctx.channel.permissions_for(ctx.me).add_reactions and await ctx.bot.db.help.use_menus()
+        ):
 
             max_pages_in_guild = await ctx.bot.db.help.max_pages_in_guild()
             destination = ctx.author if len(pages) > max_pages_in_guild else ctx
 
             if embed:
                 for page in pages:
-                    await destination.send(embed=page)
+                    try:
+                        await destination.send(embed=page)
+                    except discord.Forbidden:
+                        await ctx.send(
+                            T_(
+                                "I couldn't send the help message to you in DM. "
+                                "Either you blocked me or you disabled DMs in this server."
+                            )
+                        )
             else:
                 for page in pages:
-                    await destination.send(page)
+                    try:
+                        await destination.send(page)
+                    except discord.Forbidden:
+                        await ctx.send(
+                            T_(
+                                "I couldn't send the help message to you in DM. "
+                                "Either you blocked me or you disabled DMs in this server."
+                            )
+                        )
         else:
             if len(pages) > 1:
                 await menus.menu(ctx, pages, menus.DEFAULT_CONTROLS)
