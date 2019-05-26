29a400b81f881acdfb184975b057520d613fb923
DevilXD <DevilXD@users.noreply.github.com>
2019-05-26 17:51:43 +0200

    Fixed me being a dumbass

--- redbot/cogs/mod/slowmode.py
+++ redbot/cogs/mod/slowmode.py
@@ -1,6 +1,8 @@
-import discord
-from redbot.core import commands, i18n, checks
+import re
 from .abc import MixinMeta
+from datetime import timedelta
+from redbot.core import commands, i18n, checks
+from redbot.core.utils.chat_formatting import humanize_timedelta
 
 _ = i18n.Translator("Mod", __file__)
 
@@ -13,18 +15,32 @@ class Slowmode(MixinMeta):
     @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(manage_channels=True)
-    @checks.mod_or_permissions(manage_channels=True)
-    async def slowmode(self, ctx, interval: int = 0):
+    @checks.admin_or_permissions(manage_channels=True)
+    async def slowmode(self, ctx, interval: str = None):
         """Changes channel's slowmode setting.
 
-        Interval can be anything from 0 to 21600 seconds.
+        Interval can be anything from 0 seconds to 6 hours.
+        You can specify the unit using `s`, `m` or `h` as suffix.
         Use without parameters or set to 0 to disable.
         """
-        if 0 <= interval <= 21600:
-            await ctx.send(_("Interval must be between 0 and 21600 seconds!"))
-            return
+        if interval is None:
+            interval = 0
+        else:
+            match = re.match(r'(?i)(\d{1,5})([a-zA-Z])?', interval)
+            if match:
+                interval = int(match.group(1))
+                if match.group(2) is not None:
+                    interval = {
+                        "s": 1,
+                        "m": 60,
+                        "h": 60 * 60,
+                    }.get(match.group(2).lower(), -1) * interval
+            if not match or not 0 <= interval <= 21600:
+                await ctx.send(_("Interval must be between 0 seconds and 6 hours!"))
+                return
         await ctx.channel.edit(slowmode_delay=interval)
         if interval > 0:
-            await ctx.send(_(f"Slowmode interval is now {interval} seconds."))
+            interval = humanize_timedelta(seconds=interval)
+            await ctx.send(_(f"Slowmode interval is now {interval}."))
         else:
             await ctx.send(_("Slowmode has been disabled."))
