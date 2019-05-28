63977c8d16470a0e8d0f10f6069377f5379ca549
DevilXD <DevilXD@users.noreply.github.com>
2019-05-28 20:41:00 +0200

    Made use of the new timedelta converter

--- redbot/cogs/mod/slowmode.py
+++ redbot/cogs/mod/slowmode.py
@@ -16,29 +16,26 @@ class Slowmode(MixinMeta):
     @commands.guild_only()
     @commands.bot_has_permissions(manage_channels=True)
     @checks.admin_or_permissions(manage_channels=True)
-    async def slowmode(self, ctx, interval: str = None):
+    async def slowmode(
+        self,
+        ctx,
+        *,
+        interval: commands.TimedeltaConverter(
+            minimum=timedelta(seconds=0), maximum=timedelta(hours=6)
+        ) = timedelta(seconds=0),
+    ):
         """Changes channel's slowmode setting.
 
         Interval can be anything from 0 seconds to 6 hours.
-        You can specify the unit using `s`, `m` or `h` as suffix.
-        Use without parameters or set to 0 to disable.
+        Use without parameters to disable.
         """
-        if interval:
-            match = re.match(r"(?i)(\d{1,5})([a-zA-Z])?$", interval)
-            if match:
-                interval = int(match.group(1))
-                if match.group(2) is not None:
-                    interval = {"s": 1, "m": 60, "h": 60 * 60}.get(
-                        match.group(2).lower(), -1
-                    ) * interval
-            if not match or not 0 <= interval <= 21600:
-                await ctx.send(_("Interval must be between 0 seconds and 6 hours!"))
-                return
-        else:
-            interval = 0
-        await ctx.channel.edit(slowmode_delay=interval)
-        if interval > 0:
-            interval = humanize_timedelta(seconds=interval)
-            await ctx.send(_("Slowmode interval is now {}.".format(interval)))
+        seconds = interval.total_seconds()
+        await ctx.channel.edit(slowmode_delay=seconds)
+        if seconds > 0:
+            await ctx.send(
+                _("Slowmode interval is now {interval}.").format(
+                    interval=humanize_timedelta(timedelta=interval)
+                )
+            )
         else:
             await ctx.send(_("Slowmode has been disabled."))
