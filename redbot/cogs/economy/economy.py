a457cb13792984bf3d56ac57b65fbf9584ae2134
Tetra <tetra@digianetworks.com>
2019-05-30 15:01:32 -0400

    incorrectly formatted

--- redbot/cogs/economy/economy.py
+++ redbot/cogs/economy/economy.py
@@ -174,9 +174,9 @@ class Economy(commands.Cog):
         except (ValueError, errors.BalanceTooHigh) as e:
             return await ctx.send(str(e))
 
-        await ctx.send(embed=discord.Embed(color=(await ctx.embed_colour()),title="Payment Sent!",description="{user} transferred {num} {currency} to {other_user}"))
-            _("{user} transferred {num} {currency} to {other_user}").format(
+        await ctx.send(embed=discord.Embed(color=(await ctx.embed_colour()),title="Payment Sent!",description="{user} transferred {num} {currency} to {other_user}".format(
                 user=from_.display_name, num=amount, currency=currency, other_user=to.display_name
+                )
             )
         )
 
