aaa0d4988e767a9d4cbeea585692b0bdc92e9903
Tetrahedron <tetrahedron789@gmail.com>
2019-05-30 23:54:32 -0400

    Formatted ping response to look a bit better

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -277,7 +277,7 @@ class Core(commands.Cog, CoreLogic):
         """Pong."""
         latencies = self.bot.latencies
         for shard, pingt in latencies:
-            await ctx.send(embed=discord.Embed(color=(await ctx.embed_colour()),description=":ping_pong: **Pong!** `{}ms`".format(round(pingt * 1000))))
+            await ctx.send(embed=discord.Embed(color=(await ctx.embed_colour()),title=":ping_pong: Pong!",description="\n **Latency to Discord:**\n```py\n{} ms```".format(round(pingt * 1000))))
 
     @commands.command()
     async def info(self, ctx: commands.Context):
