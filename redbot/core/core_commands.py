6d7ae43bc9bba76fe371701d154abe310499858b
Vexima <51716387+Vexima@users.noreply.github.com>
2019-07-06 16:18:30 +0100

    linked Twentysix

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -285,7 +285,7 @@ class Core(commands.Cog, CoreLogic):
             "<@418078199982063626> would like to especially thank the very "
             "kind people on the [support server]({}). Without their help, "
             "this bot would not be possible today. I would also like to thank "
-            "[Twentysix]{} who made this wonderful bot a reality. Finally, I "
+            "[Twentysix]({}) who made this wonderful bot a reality. Finally, I "
             "want to thank all the great people who develop the cogs that"
             "this bot uses."
         ).format(support_server_url, author_repo)
