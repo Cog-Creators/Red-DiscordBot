619f045c3b746a4500f80fcfb9ba2c40add44b8f
Michael H <michael@michaelhall.tech>
2019-07-16 07:09:28 -0400

    make the script more generally useful

--- tools/check_changelog_entries.sh
+++ tools/check_changelog_entries.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 
-# This is hard coded as `origin` for the remote to check against for travis
-# This should not be run elsewhere
+# This can be run for other remotes than origin with the env vat `RED_REMOTE`
+REMOTE="${RED_REMOTE:-origin}"
 
-if [[ ${TRAVIS:-false} = "true" ]]; then
+if [[ $(git diff "$REMOTE"/V3/develop) ]]; then
 
-  if [[ $(git diff origin/V3/develop) ]]; then
-
-    if [[ $(git diff origin/V3/develop -- changelog.d/) ]]; then
-      echo "Found changelog fragments..."
-      exit 0
-    else
-      echo "Error: No new changelog fragments!"
-      exit 1
-    fi
- 
-  else
-    echo "No changes to need changelog for."
+  if [[ $(git diff "$REMOTE"/V3/develop -- changelog.d/) ]]; then
+    echo "Found changelog fragments..."
     exit 0
+  else
+    echo "Error: No new changelog fragments!"
+    exit 1
   fi
 
 else
-  echo "We aren't on Travis, skipping changelog check."
+  echo "No changes to need changelog for."
   exit 0
 fi
