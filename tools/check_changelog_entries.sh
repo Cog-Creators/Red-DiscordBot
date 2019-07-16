efd07b4d8bf11a0b167a01f7321ef9ab11ffc60a
Michael H <michael@michaelhall.tech>
2019-07-16 06:13:35 -0400

    make fragment check only happen on Travis

--- tools/check_changelog_entries.sh
+++ tools/check_changelog_entries.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
 # This is hard coded as `origin` for the remote to check against for travis
+# This should not be run elsewhere
 
-if [[ $(git diff origin/V3/develop) ]]; then
-  if [[ $(git diff origin/V3/develop -- changelog.d/) ]]; then
-    echo "Found changelog fragments..."
-    exit 0
+if [[ ${TRAVIS:-false} = "true" ]]; then
+
+  if [[ $(git diff origin/V3/develop) ]]; then
+
+    if [[ $(git diff origin/V3/develop -- changelog.d/) ]]; then
+      echo "Found changelog fragments..."
+      exit 0
+    else
+      echo "Error: No new changelog fragments!"
+      exit 1
+    fi
+ 
   else
-    echo "Error: No new changelog fragments!"
-    exit 1
+    echo "No changes to need changelog for."
+    exit 0
   fi
+
 else
-  echo "No changes to need changelog for."
+  echo "We aren't on Travis, skipping changelog check."
   exit 0
 fi
