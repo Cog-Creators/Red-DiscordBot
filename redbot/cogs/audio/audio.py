647d2cf55263005b0394f3e6cdb520974b23a996
Draper <guyreis96@gmail.com>
2019-08-01 11:33:54 +0100

    Fix UX changes and should fix autoplay
    Signed-off-by: Draper <guyreis96@gmail.com>

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -849,9 +849,10 @@ class Audio(commands.Cog):
         msg = "----" + _("Server Settings") + "----        \n"
         msg += _("Auto-disconnect:  [{dc}]\n").format(dc=dc)
         msg += _("Auto-play:        [{autoplay}]\n").format(autoplay=autoplay)
-        msg += _("Disconnect timer: [{num_seconds}]\n").format(
-            num_seconds=dynamic_time(emptydc_timer)
-        )
+        if emptydc_enabled:
+            msg += _("Disconnect timer: [{num_seconds}]\n").format(
+                num_seconds=dynamic_time(emptydc_timer)
+            )
         if dj_enabled:
             msg += _("DJ Role:          [{role.name}]\n").format(role=dj_role_obj)
         if jukebox:
@@ -3472,7 +3473,7 @@ class Audio(commands.Cog):
                 colour=await ctx.embed_colour(),
                 title=_("Playlist Enqueued"),
                 description=_(
-                    "{name} - (id) [{scope}]\nAdded {num} tracks to the queue.{maxlength_msg}"
+                    "{name} - ({id}) [{scope}]\nAdded {num} tracks to the queue.{maxlength_msg}"
                 ).format(
                     num=track_len,
                     maxlength_msg=maxlength_msg,
@@ -5008,7 +5009,6 @@ class Audio(commands.Cog):
         """Stop playback and clear the queue."""
         dj_enabled = await self.config.guild(ctx.guild).dj_enabled()
         vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
-        autoplay = await self.config.guild(ctx.guild).auto_play()
         if not self._player_check(ctx):
             return await self._embed_msg(ctx, _("Nothing playing."))
         player = lavalink.get_player(ctx.guild.id)
@@ -5029,16 +5029,16 @@ class Audio(commands.Cog):
             if not await self._can_instaskip(ctx, ctx.author):
                 return await self._embed_msg(ctx, _("You need the DJ role to stop the music."))
         if player.is_playing or (not player.is_playing and player.paused):
-            await player.stop()
             eq = player.fetch("eq")
             if eq:
                 await self.config.custom("EQUALIZER", ctx.guild.id).eq_bands.set(eq.bands)
+            player.queue = []
+            player.store("playing_song", None)
             player.store("prev_requester", None)
             player.store("prev_song", None)
-            player.store("playing_song", None)
             player.store("requester", None)
-            if not autoplay:
-                await self._embed_msg(ctx, _("Stopping..."))
+            await player.stop()
+            await self._embed_msg(ctx, _("Stopping..."))
 
     @commands.command()
     @commands.guild_only()
