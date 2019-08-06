de2819b60f4dbb763aed34c7e5007e3f91b356a7
Guy <guyreis96@gmail.com>
2019-08-06 18:48:04 +0100

    Fix a Potential crash
    Signed-off-by: Guy <guyreis96@gmail.com>

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -2708,7 +2708,7 @@ class Audio(commands.Cog):
                 ]
             if match_count > 10:
                 raise TooManyMatches(
-                    f"{match_count} playlist match {arg} "
+                    f"{match_count} playlist match {original_input} "
                     f"Please try to be more specific or use the playlist id."
                 )
 
