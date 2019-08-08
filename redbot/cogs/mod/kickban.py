a0ed4ec4708946ceebc4fec970e22543c23bd8a9
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-08 02:27:36 +0100

    Added another check
    Don't send embed to user if  author != user

--- redbot/cogs/mod/kickban.py
+++ redbot/cogs/mod/kickban.py
@@ -218,16 +218,18 @@ class KickBanMixin(MixinMeta):
         self,
         ctx: commands.Context,
         user: discord.Member,
-        days: Optional[int] = 0,
+        days: Optional[int] = None,
         *,
         reason: str = None,
     ):
         """Ban a user from this server and optionally delete days of messages.
 
         If days is not a number, it's treated as the first word of the reason.
-        Minimum 0 days, maximum 7. Defaults to 0."""
+        Minimum 0 days, maximum 7. If not specified, defaultdays setting will be used instead."""
         author = ctx.author
         guild = ctx.guild
+        if days is None:
+            days = await self.settings.guild(guild).default_days()
         if reason == None:
             reason = "No reason was given."
         else:
@@ -236,15 +238,15 @@ class KickBanMixin(MixinMeta):
         toggle = await self.settings.guild(guild).toggle_dm()
         try:
             if toggle == True:
-                if guild.me.top_role >= user.top_role and user != guild.owner:
+                if guild.me.top_role >= user.top_role and user != guild.owner and author != user:
                     with contextlib.suppress(discord.HTTPException):
                         em = discord.Embed(
                             title=_("**You have been banned from {guild}.**").format(guild=guild)
                         )
                         em.add_field(name=_("**Reason**"), value=reason, inline=False)
                         await user.send(embed=em)
-        except discord.HTTPException as e:
-            print(e)
+        except discord.HTTPException:
+            return
 
         result = await self.ban_user(
             user=user, ctx=ctx, days=days, reason=reason, create_modlog_case=True
