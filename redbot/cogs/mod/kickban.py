16c2d368c0c2cceb1b97043359b1c5778152e29c
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-12 20:25:48 +0100

    Update kickban.py

--- redbot/cogs/mod/kickban.py
+++ redbot/cogs/mod/kickban.py
@@ -180,23 +180,20 @@ class KickBanMixin(MixinMeta):
                 reason = reason
             self == self.bot.get_cog("Mod")
             toggle = await self.settings.guild(guild).toggle_dm()
-            try:
-                if toggle == True:
-                    if (
-                        guild.me.top_role >= user.top_role
-                        and user != guild.owner
-                        and author != user
-                    ):
-                        with contextlib.suppress(discord.HTTPException):
-                            em = discord.Embed(
-                                title=_("**You have been kicked from {guild}.**").format(
-                                    guild=guild
-                                )
+            if toggle == True:
+                if (
+                    guild.me.top_role >= user.top_role
+                    and user != guild.owner
+                    and author != user
+                ):
+                    with contextlib.suppress(discord.HTTPException):
+                        em = discord.Embed(
+                            title=_("**You have been kicked from {guild}.**").format(
+                                guild=guild
                             )
-                            em.add_field(name=_("**Reason**"), value=reason, inline=False)
-                            await user.send(embed=em)
-            except discord.HTTPException:
-                return
+                        )
+                        em.add_field(name=_("**Reason**"), value=reason, inline=False)
+                        await user.send(embed=em)
 
             await guild.kick(user, reason=audit_reason)
             log.info("{}({}) kicked {}({})".format(author.name, author.id, user.name, user.id))
@@ -245,17 +242,14 @@ class KickBanMixin(MixinMeta):
             reason = reason
         self == self.bot.get_cog("Mod")
         toggle = await self.settings.guild(guild).toggle_dm()
-        try:
-            if toggle == True:
-                if guild.me.top_role >= user.top_role and user != guild.owner and author != user:
-                    with contextlib.suppress(discord.HTTPException):
-                        em = discord.Embed(
-                            title=_("**You have been banned from {guild}.**").format(guild=guild)
-                        )
-                        em.add_field(name=_("**Reason**"), value=reason, inline=False)
-                        await user.send(embed=em)
-        except discord.HTTPException:
-            return
+        if toggle == True:
+            if guild.me.top_role >= user.top_role and user != guild.owner and author != user:
+                with contextlib.suppress(discord.HTTPException):
+                    em = discord.Embed(
+                        title=_("**You have been banned from {guild}.**").format(guild=guild)
+                    )
+                    em.add_field(name=_("**Reason**"), value=reason, inline=False)
+                    await user.send(embed=em)
 
         result = await self.ban_user(
             user=user, ctx=ctx, days=days, reason=reason, create_modlog_case=True
