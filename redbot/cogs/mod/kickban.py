001b8b1a2dd3af30693357dfa20e7d4ba98b00f9
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-12 22:56:14 +0100

    Update redbot/cogs/mod/kickban.py
    Co-Authored-By: jack1142 <6032823+jack1142@users.noreply.github.com>

--- redbot/cogs/mod/kickban.py
+++ redbot/cogs/mod/kickban.py
@@ -231,7 +231,7 @@ class KickBanMixin(MixinMeta):
             reason = "No reason was given."
         self == self.bot.get_cog("Mod")
         toggle = await self.settings.guild(guild).toggle_dm()
-        if toggle == True:
+        if toggle:
             if guild.me.top_role >= user.top_role and user != guild.owner and author != user:
                 with contextlib.suppress(discord.HTTPException):
                     em = discord.Embed(
