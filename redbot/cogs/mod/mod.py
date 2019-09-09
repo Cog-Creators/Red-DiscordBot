0e4859facdc46ede9ecc4451b9c177781643351c
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-09-09 15:37:02 +0100

    FUCK

--- redbot/cogs/mod/mod.py
+++ redbot/cogs/mod/mod.py
@@ -53,6 +53,7 @@ class Mod(
         "delete_delay": -1,
         "reinvite_on_unban": False,
         "current_tempbans": [],
+        "toggle_dm": False,
     }
 
     default_channel_settings = {"ignored": False}
