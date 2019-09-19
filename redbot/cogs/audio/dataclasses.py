b2454539a02412c2c131100faee8202d23515c5f
guyre <27962761+drapersniper@users.noreply.github.com>
2019-09-19 18:50:20 +0100

    Rework `index` parsing for youtube urls
    Signed-off-by: guyre <27962761+drapersniper@users.noreply.github.com>

--- redbot/cogs/audio/dataclasses.py
+++ redbot/cogs/audio/dataclasses.py
@@ -381,14 +381,14 @@ class Query:
                             match = re.search(_re_youtube_index, track)
                             if match:
                                 returning["track_index"] = int(match.group(1)) - 1
-                        if any(k in track for k in ["playlist?", "&list="]):
-                            if all(x in track in track for x in ["&start_radio=", "watch?"]):
-                                returning["track_index"] = 0
-                                returning["playlist"] = True
-                                returning["single"] = False
-                            else:
-                                returning["playlist"] = True if not _has_index else False
-                                returning["single"] = True if _has_index else False
+
+                        if all(k in track for k in ["&list=", "watch?"]):
+                            returning["track_index"] = 0
+                            returning["playlist"] = True
+                            returning["single"] = False
+                        elif all(x in track for x in ["playlist?"]):
+                            returning["playlist"] = True if not _has_index else False
+                            returning["single"] = True if _has_index else False
                         else:
                             returning["single"] = True
                     elif url_domain == "spotify.com":
