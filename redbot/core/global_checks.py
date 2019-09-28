d6269df8cdada7a332167c5bc534452f7a2ea6ab
Michael H <michael@michaelhall.tech>
2019-09-28 11:47:33 -0400

    More conclusive fix

--- redbot/core/global_checks.py
+++ redbot/core/global_checks.py
@@ -4,10 +4,18 @@ from . import commands
 
 def init_global_checks(bot):
     @bot.check_once
+    def actually_up(ctx):
+        """ 
+        Uptime is set during the initial startup process.
+        If this hasn't been set, we should assume the bot isn't ready yet. 
+        """
+        return ctx.bot.uptime is not None
+
+    @bot.check_once
     async def whiteblacklist_checks(ctx):
         return await ctx.bot.allowed_by_whitelist_blacklist(ctx.author)
 
     @bot.check_once
-    async def bots(ctx):
+    def bots(ctx):
         """Check the user is not another bot."""
         return not ctx.author.bot
