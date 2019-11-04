7245cc02198d10d3dc0fa3ce97e71b9d189d232f
Michael H <michael@michaelhall.tech>
2019-11-03 22:52:58 -0500

    Fix a pre-existing NoneType Error

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -171,9 +171,13 @@ class Audio(commands.Cog):
 
     async def cog_before_invoke(self, ctx: commands.Context):
         await self._ready_event.wait()
+        # check for unsupported arch
+        # Check on this needs refactoring at a later date
+        # so that we have a better way to handle the tasks
         if self.llsetup in [ctx.command, ctx.command.root_parent]:
             pass
-        elif self._connect_task.cancelled():
+
+        elif self._connect_task and self._connect_task.cancelled():
             await ctx.send(
                 "You have attempted to run Audio's Lavalink server on an unsupported"
                 " architecture. Only settings related commands will be available."
@@ -181,6 +185,7 @@ class Audio(commands.Cog):
             raise RuntimeError(
                 "Not running audio command due to invalid machine architecture for Lavalink."
             )
+
         dj_enabled = await self.config.guild(ctx.guild).dj_enabled()
         if dj_enabled:
             dj_role_obj = ctx.guild.get_role(await self.config.guild(ctx.guild).dj_role())
