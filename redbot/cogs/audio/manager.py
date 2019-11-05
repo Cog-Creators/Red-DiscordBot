989e16b20b814971e01a8657dedf4d9b45c23ed1
Michael H <michael@michaelhall.tech>
2019-11-05 05:28:34 -0500

    Lavalink bump to 3.2.1_846

--- redbot/cogs/audio/manager.py
+++ redbot/cogs/audio/manager.py
@@ -17,7 +17,7 @@ from redbot.core import data_manager
 from .errors import LavalinkDownloadFailed
 
 JAR_VERSION = "3.2.1"
-JAR_BUILD = 823
+JAR_BUILD = 846
 LAVALINK_DOWNLOAD_URL = (
     f"https://github.com/Cog-Creators/Lavalink-Jars/releases/download/{JAR_VERSION}_{JAR_BUILD}/"
     f"Lavalink.jar"
