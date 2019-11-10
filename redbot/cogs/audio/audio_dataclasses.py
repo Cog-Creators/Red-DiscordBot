a2ad03dd67352497d925116163f3cb46503bd271
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2019-11-10 17:05:43 +0000

    Black formatting
    Signed-off-by: Drapersniper <27962761+drapersniper@users.noreply.github.com>

--- redbot/cogs/audio/audio_dataclasses.py
+++ redbot/cogs/audio/audio_dataclasses.py
@@ -215,7 +215,7 @@ class LocalPath:
         for track in self.multirglob(*[f"*{ext}" for ext in self._all_music_ext]):
             if track.exists() and track.is_file() and track.parent != self.localtrack_folder:
                 tracks.append(Query.process_input(LocalPath(str(track.absolute()))))
-        return sorted(tracks, key=lambda x:x.to_string_user().swapcase())
+        return sorted(tracks, key=lambda x: x.to_string_user().swapcase())
 
     def subfolders_in_tree(self):
         files = list(self.multirglob(*[f"*{ext}" for ext in self._all_music_ext]))
