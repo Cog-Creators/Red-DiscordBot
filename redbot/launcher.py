fe2f92d01856ba1465bc1e502875a4e3e676a832
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-12-07 23:43:31 +0100

    Update launcher.py

--- redbot/launcher.py
+++ redbot/launcher.py
@@ -151,7 +151,8 @@ def cli_flag_getter():
         if choice == "y":
             print("Enter the user id for the owner")
             owner_id = user_choice()
-            flags.append("--owner {}".format(owner_id))
+            flags.append("--owner")
+            flags.append(owner_id)
         print("Would you like to specify any prefixes? (y/n)")
         choice = user_choice()
         if choice == "y":
@@ -161,7 +162,8 @@ def cli_flag_getter():
             )
             prefixes = user_choice().split()
             for p in prefixes:
-                flags.append("-p {}".format(p))
+                flags.append("-p")
+                flags.append(p)
         print("Would you like mentioning the bot to be a prefix? (y/n)")
         choice = user_choice()
         if choice == "y":
