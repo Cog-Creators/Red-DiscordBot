df72847ea279461a1be5c897b8552152e12c29fd
Draper <27962761+Drapersniper@users.noreply.github.com>
2019-12-18 11:35:45 +0000

    *sigh* Black

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -3947,9 +3947,7 @@ class Audio(commands.Cog):
             ]
             playlist_data[
                 "playlist"
-            ] = (
-                playlist_songs_backwards_compatible
-            )  # TODO: Keep new playlists backwards compatible, Remove me in a few releases
+            ] = playlist_songs_backwards_compatible  # TODO: Keep new playlists backwards compatible, Remove me in a few releases
             playlist_data[
                 "link"
             ] = (
