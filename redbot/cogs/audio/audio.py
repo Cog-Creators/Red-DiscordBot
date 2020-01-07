0068175e62e95b9afca61cd95a48e9198a30d8bf
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-01-07 11:42:55 +0000

    add asyncio sleep on playlist queue command
    Signed-off-by: Drapersniper <27962761+drapersniper@users.noreply.github.com>

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -4876,44 +4876,49 @@ class Audio(commands.Cog):
         ​ ​ ​ ​ [p]playlist queue MyGlobalPlaylist --scope Global
         ​ ​ ​ ​ [p]playlist queue MyPersonalPlaylist --scope User
         """
-        if scope_data is None:
-            scope_data = [PlaylistScope.GUILD.value, ctx.author, ctx.guild, False]
-        scope, author, guild, specified_user = scope_data
-        scope_name = humanize_scope(
-            scope, ctx=guild if scope == PlaylistScope.GUILD.value else author
-        )
-        temp_playlist = FakePlaylist(author.id, scope)
-        if not await self.can_manage_playlist(scope, temp_playlist, ctx, author, guild):
-            ctx.command.reset_cooldown(ctx)
-            return
-        playlist_name = playlist_name.split(" ")[0].strip('"')[:32]
-        if playlist_name.isnumeric():
-            ctx.command.reset_cooldown(ctx)
-            return await self._embed_msg(
-                ctx,
-                title=_("Invalid Playlist Name"),
-                description=_(
-                    "Playlist names must be a single word "
-                    "(up to 32 characters) and not numbers only."
-                ),
+        async with ctx.typing():
+            if scope_data is None:
+                scope_data = [PlaylistScope.GUILD.value, ctx.author, ctx.guild, False]
+            scope, author, guild, specified_user = scope_data
+            scope_name = humanize_scope(
+                scope, ctx=guild if scope == PlaylistScope.GUILD.value else author
             )
-        if not self._player_check(ctx):
-            ctx.command.reset_cooldown(ctx)
-            return await self._embed_msg(ctx, title=_("Nothing playing."))
+            temp_playlist = FakePlaylist(author.id, scope)
+            if not await self.can_manage_playlist(scope, temp_playlist, ctx, author, guild):
+                ctx.command.reset_cooldown(ctx)
+                return
+            playlist_name = playlist_name.split(" ")[0].strip('"')[:32]
+            if playlist_name.isnumeric():
+                ctx.command.reset_cooldown(ctx)
+                return await self._embed_msg(
+                    ctx,
+                    title=_("Invalid Playlist Name"),
+                    description=_(
+                        "Playlist names must be a single word "
+                        "(up to 32 characters) and not numbers only."
+                    ),
+                )
+            if not self._player_check(ctx):
+                ctx.command.reset_cooldown(ctx)
+                return await self._embed_msg(ctx, title=_("Nothing playing."))
 
-        player = lavalink.get_player(ctx.guild.id)
-        if not player.queue:
-            ctx.command.reset_cooldown(ctx)
-            return await self._embed_msg(ctx, title=_("There's nothing in the queue."))
-        tracklist = []
-        np_song = track_creator(player, "np")
-        tracklist.append(np_song)
-        for track in player.queue:
-            queue_idx = player.queue.index(track)
-            track_obj = track_creator(player, queue_idx)
-            tracklist.append(track_obj)
+            player = lavalink.get_player(ctx.guild.id)
+            if not player.queue:
+                ctx.command.reset_cooldown(ctx)
+                return await self._embed_msg(ctx, title=_("There's nothing in the queue."))
+            tracklist = []
+            np_song = track_creator(player, "np")
+            tracklist.append(np_song)
+            for i, track in enumerate(player.queue, start=1):
+                if i % 500 == 0:
+                    await asyncio.sleep(0.02)
+                queue_idx = player.queue.index(track)
+                track_obj = track_creator(player, queue_idx)
+                tracklist.append(track_obj)
 
-        playlist = await create_playlist(ctx, scope, playlist_name, None, tracklist, author, guild)
+            playlist = await create_playlist(
+                ctx, scope, playlist_name, None, tracklist, author, guild
+            )
         await self._embed_msg(
             ctx,
             title=_("Playlist Created"),
