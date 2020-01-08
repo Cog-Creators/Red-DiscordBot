3287c76a051577ee75495a06a464ff6088fdc5ee
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-01-08 22:00:01 +0000

    formatting plus some other fixes
    Signed-off-by: Drapersniper <27962761+drapersniper@users.noreply.github.com>

--- redbot/cogs/audio/audio.py
+++ redbot/cogs/audio/audio.py
@@ -6232,83 +6232,84 @@ class Audio(commands.Cog):
         if not self._player_check(ctx):
             return await self._embed_msg(ctx, title=_("There's nothing in the queue."))
         player = lavalink.get_player(ctx.guild.id)
-        if not player.queue or not player.current:
+
+        if player.current and not player.queue:
+            arrow = await draw_time(ctx)
+            pos = lavalink.utils.format_time(player.position)
+            if player.current.is_stream:
+                dur = "LIVE"
+            else:
+                dur = lavalink.utils.format_time(player.current.length)
+            song = get_track_description(player.current)
+            song += _("\n Requested by: **{track.requester}**")
+            song += "\n\n{arrow}`{pos}`/`{dur}`"
+            song = song.format(track=player.current, arrow=arrow, pos=pos, dur=dur)
+            embed = discord.Embed(title=_("Now Playing"), description=song)
+            if await self.config.guild(ctx.guild).thumbnail() and player.current:
+                if player.current.thumbnail:
+                    embed.set_thumbnail(url=player.current.thumbnail)
+
+            shuffle = await self.config.guild(ctx.guild).shuffle()
+            repeat = await self.config.guild(ctx.guild).repeat()
+            autoplay = await self.config.guild(ctx.guild).auto_play()
+            text = ""
+            text += (
+                _("Auto-Play")
+                + ": "
+                + ("\N{WHITE HEAVY CHECK MARK}" if autoplay else "\N{CROSS MARK}")
+            )
+            text += (
+                (" | " if text else "")
+                + _("Shuffle")
+                + ": "
+                + ("\N{WHITE HEAVY CHECK MARK}" if shuffle else "\N{CROSS MARK}")
+            )
+            text += (
+                (" | " if text else "")
+                + _("Repeat")
+                + ": "
+                + ("\N{WHITE HEAVY CHECK MARK}" if repeat else "\N{CROSS MARK}")
+            )
+            embed.set_footer(text=text)
+            message = await self._embed_msg(ctx, embed=embed)
+            dj_enabled = self._dj_status_cache.setdefault(
+                ctx.guild.id, await self.config.guild(ctx.guild).dj_enabled()
+            )
+            vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
+            if dj_enabled or vote_enabled:
+                if not await self._can_instaskip(ctx, ctx.author) and not await self._is_alone(
+                    ctx
+                ):
+                    return
+
+            expected = ("⏹", "⏯")
+            emoji = {"stop": "⏹", "pause": "⏯"}
             if player.current:
-                arrow = await draw_time(ctx)
-                pos = lavalink.utils.format_time(player.position)
-                if player.current.is_stream:
-                    dur = "LIVE"
-                else:
-                    dur = lavalink.utils.format_time(player.current.length)
-                song = get_track_description(player.current)
-                song += _("\n Requested by: **{track.requester}**")
-                song += "\n\n{arrow}`{pos}`/`{dur}`"
-                song = song.format(track=player.current, arrow=arrow, pos=pos, dur=dur)
-                embed = discord.Embed(title=_("Now Playing"), description=song)
-                if await self.config.guild(ctx.guild).thumbnail() and player.current:
-                    if player.current.thumbnail:
-                        embed.set_thumbnail(url=player.current.thumbnail)
+                task = start_adding_reactions(message, expected[:4], ctx.bot.loop)
+            else:
+                task = None
 
-                shuffle = await self.config.guild(ctx.guild).shuffle()
-                repeat = await self.config.guild(ctx.guild).repeat()
-                autoplay = await self.config.guild(ctx.guild).auto_play()
-                text = ""
-                text += (
-                    _("Auto-Play")
-                    + ": "
-                    + ("\N{WHITE HEAVY CHECK MARK}" if autoplay else "\N{CROSS MARK}")
+            try:
+                (r, u) = await self.bot.wait_for(
+                    "reaction_add",
+                    check=ReactionPredicate.with_emojis(expected, message, ctx.author),
+                    timeout=30.0,
                 )
-                text += (
-                    (" | " if text else "")
-                    + _("Shuffle")
-                    + ": "
-                    + ("\N{WHITE HEAVY CHECK MARK}" if shuffle else "\N{CROSS MARK}")
-                )
-                text += (
-                    (" | " if text else "")
-                    + _("Repeat")
-                    + ": "
-                    + ("\N{WHITE HEAVY CHECK MARK}" if repeat else "\N{CROSS MARK}")
-                )
-                embed.set_footer(text=text)
-                message = await self._embed_msg(ctx, embed=embed)
-                dj_enabled = self._dj_status_cache.setdefault(
-                    ctx.guild.id, await self.config.guild(ctx.guild).dj_enabled()
-                )
-                vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
-                if dj_enabled or vote_enabled:
-                    if not await self._can_instaskip(ctx, ctx.author) and not await self._is_alone(
-                        ctx
-                    ):
-                        return
-
-                expected = ("⏹", "⏯")
-                emoji = {"stop": "⏹", "pause": "⏯"}
-                if player.current:
-                    task = start_adding_reactions(message, expected[:4], ctx.bot.loop)
-                else:
-                    task = None
-
-                try:
-                    (r, u) = await self.bot.wait_for(
-                        "reaction_add",
-                        check=ReactionPredicate.with_emojis(expected, message, ctx.author),
-                        timeout=30.0,
-                    )
-                except asyncio.TimeoutError:
-                    return await self._clear_react(message, emoji)
-                else:
-                    if task is not None:
-                        task.cancel()
-                reacts = {v: k for k, v in emoji.items()}
-                react = reacts[r.emoji]
-                if react == "stop":
-                    await self._clear_react(message, emoji)
-                    return await ctx.invoke(self.stop)
-                elif react == "pause":
-                    await self._clear_react(message, emoji)
-                    return await ctx.invoke(self.pause)
-                return
+            except asyncio.TimeoutError:
+                return await self._clear_react(message, emoji)
+            else:
+                if task is not None:
+                    task.cancel()
+            reacts = {v: k for k, v in emoji.items()}
+            react = reacts[r.emoji]
+            if react == "stop":
+                await self._clear_react(message, emoji)
+                return await ctx.invoke(self.stop)
+            elif react == "pause":
+                await self._clear_react(message, emoji)
+                return await ctx.invoke(self.pause)
+            return
+        elif not player.current and not player.queue:
             return await self._embed_msg(ctx, title=_("There's nothing in the queue."))
 
         async with ctx.typing():
@@ -6341,7 +6342,7 @@ class Audio(commands.Cog):
         try:
             arrow = await draw_time(ctx)
         except AttributeError:
-            return await self._embed_msg(ctx, title=_("There's nothing in the queue2."))
+            return await self._embed_msg(ctx, title=_("There's nothing in the queue."))
         pos = lavalink.utils.format_time(player.position)
 
         if player.current.is_stream:
