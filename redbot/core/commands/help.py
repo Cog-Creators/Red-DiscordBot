5320a8cd5e3be538baba2f608c3d8a0b33a43746
Michael H <michael@michaelhall.tech>
2020-01-08 20:16:38 -0500

    until splitting the fields, ensure a field

--- redbot/core/commands/help.py
+++ redbot/core/commands/help.py
@@ -276,10 +276,10 @@ class RedHelpFormatter:
         ret = []
         current_count = 0
 
-        for f in fields:
+        for i, f in enumerate(fields):
             f_len = len(f.value) + len(f.name)
 
-            if not current_count or f_len + current_count > max_chars:
+            if not current_count or f_len + current_count > max_chars or i == 0:
                 current_count += f_len
                 curr_group.append(f)
             elif curr_group:
