6ae72b44588c670b3417d63b8344d7881939c115
Michael H <michael@michaelhall.tech>
2020-01-09 09:18:48 -0500

    make this work, from a user perspective

--- redbot/core/commands/help.py
+++ redbot/core/commands/help.py
@@ -279,7 +279,8 @@ class RedHelpFormatter:
         for i, f in enumerate(fields):
             f_len = len(f.value) + len(f.name)
 
-            if not current_count or f_len + current_count > max_chars or i == 0:
+            # Commands start at the 1st index of fields, i < 2 is a hacky workaround for now
+            if not current_count or f_len + current_count > max_chars or i < 2:
                 current_count += f_len
                 curr_group.append(f)
             elif curr_group:
