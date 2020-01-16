efb6e0b327469f5be99614ebe0d28694f8ee9729
Draper <27962761+Drapersniper@users.noreply.github.com>
2020-01-16 14:30:22 +0000

    *sigh*

--- redbot/cogs/audio/audio_dataclasses.py
+++ redbot/cogs/audio/audio_dataclasses.py
@@ -239,7 +239,7 @@ class LocalPath:
 
     def tracks_in_tree(self):
         tracks = []
-        for track in self.multirglob(*[f"*{ext}" for ext in self._all_music_ext]):
+        for track in self.multirglob(*[f"{ext}" for ext in self._all_music_ext]):
             if track.exists() and track.is_file() and track.parent != self.localtrack_folder:
                 tracks.append(Query.process_input(LocalPath(str(track.absolute()))))
         return sorted(tracks, key=lambda x: x.to_string_user().lower())
