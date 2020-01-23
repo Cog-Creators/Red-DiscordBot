64db1ee4013ffbd082dcb3108c94449f353be412
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-01-23 16:21:08 +0000

    Update redbot/cogs/mod/settings.py
    Co-Authored-By: jack1142 <6032823+jack1142@users.noreply.github.com>

--- redbot/cogs/mod/settings.py
+++ redbot/cogs/mod/settings.py
@@ -206,7 +206,7 @@ class ModSettings(MixinMeta):
     @modset.command()
     @commands.guild_only()
     async def defaultdays(self, ctx: commands.Context, days: int = 0):
-        """Set the default days worth of messages to be deleted when a user is banned.
+        """Set the default number of days worth of messages to be deleted when a user is banned.
 
         The number of days must be between 0 and 7.
         """
