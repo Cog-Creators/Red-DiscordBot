8eccea58a430989e2ed4d1605bf6b614ac78d62c
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-25 03:53:12 +0100

    Update downloader.py

--- redbot/cogs/downloader/downloader.py
+++ redbot/cogs/downloader/downloader.py
@@ -73,7 +73,16 @@ class Downloader(commands.Cog):
             self._init_task.cancel()
 
     def create_init_task(self):
+        def _done_callback(task: asyncio.Task) -> None:
+            exc = task.exception()
+            if exc is not None:
+                log.error(
+                    "An unexpected error occurred during Downloader's initialization.",
+                    exc_info=exc,
+                )
+
         self._init_task = asyncio.create_task(self.initialize())
+        self._init_task.add_done_callback(_done_callback)
 
     async def initialize(self) -> None:
         await self._repo_manager.initialize()
