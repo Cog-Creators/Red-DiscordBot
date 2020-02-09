4d445e6cf08a849815c8e083276eaf6af0e11877
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-02-09 15:18:30 +0100

    set hidden to True for shared libs

--- redbot/cogs/downloader/installable.py
+++ redbot/cogs/downloader/installable.py
@@ -136,6 +136,8 @@ class Installable(RepoJSONMixin):
         super()._read_info_file()
 
         update_mixin(self, INSTALLABLE_SCHEMA)
+        if self.type == InstallableType.SHARED_LIBRARY:
+            self.hidden = True
 
 
 class InstalledModule(Installable):
