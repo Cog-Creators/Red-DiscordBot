f1d9abe2a29a6825f6b4b8c3e0ed42a03531830f
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-02-09 22:51:42 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -21,6 +21,10 @@ exit /B %ERRORLEVEL%
 black --check !PYFILES!
 exit /B %ERRORLEVEL%
 
+:stylediff
+black --check --diff !PYFILES!
+exit /B %ERRORLEVEL%
+
 :newenv
 py -3.8 -m venv --clear .venv
 .\.venv\Scripts\python -m pip install -U pip setuptools
