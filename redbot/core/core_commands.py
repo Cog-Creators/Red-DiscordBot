df1ddbf5f604b86c6440b2e849e586bba9af9a77
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-03-20 18:59:09 +0100

    Remove `[p]listlocales` and link to Crowdin in `[p]set locale`

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -1245,30 +1245,26 @@ class Core(commands.Cog, CoreLogic):
 
     @_set.command()
     @checks.is_owner()
-    async def locale(self, ctx: commands.Context, locale_name: str):
+    async def locale(self, ctx: commands.Context, language_code: str):
         """
-        Changes bot locale.
+        Changes bot's locale.
 
-        Use [p]listlocales to get a list of available locales.
+        `<language_code>` can be any language code with country code included,
+        e.g. `en-US`, `de-DE`, `fr-FR`, `pl-PL`, etc.
+
+        Go to Red's Crowdin page to see locales that are available with translations:
+        https://translate.discord.red
 
         To reset to English, use "en-US".
         """
         try:
-            locale = Locale.parse(locale_name, sep="-")
+            locale = Locale.parse(language_code, sep="-")
         except (ValueError, UnknownLocaleError):
-            await ctx.send(
-                _(
-                    "Invalid locale. Use `{prefix}listlocales` to get "
-                    "a list of available locales."
-                ).format(prefix=ctx.clean_prefix)
-            )
+            await ctx.send(_("Invalid language code. Use format: `en-US`"))
             return
         if locale.territory is None:
             await ctx.send(
-                _(
-                    "Invalid locale format. Use `{prefix}listlocales` to get "
-                    "a list of available locales."
-                ).format(prefix=ctx.clean_prefix)
+                _("Invalid format - language code has to include country code, e.g. `en-US`")
             )
             return
         standardized_locale_name = f"{locale.language}-{locale.territory}"
@@ -1480,27 +1476,6 @@ class Core(commands.Cog, CoreLogic):
 
         await ctx.bot._config.help.tagline.set(tagline)
         await ctx.send(_("The tagline has been set."))
-
-    @commands.command()
-    @checks.is_owner()
-    async def listlocales(self, ctx: commands.Context):
-        """
-        Lists all available locales
-
-        Use `[p]set locale` to set a locale
-        """
-        async with ctx.channel.typing():
-            red_dist = pkg_resources.get_distribution("red-discordbot")
-            red_path = Path(red_dist.location) / "redbot"
-            locale_list = [loc.stem for loc in list(red_path.glob("**/*.po"))]
-            locale_list.append("en-US")
-            locale_list = sorted(set(locale_list))
-            if not locale_list:
-                await ctx.send(_("No languages found."))
-                return
-            pages = pagify("\n".join(locale_list), shorten_by=26)
-
-        await ctx.send_interactive(pages, box_lang="Available Locales:")
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.user)
