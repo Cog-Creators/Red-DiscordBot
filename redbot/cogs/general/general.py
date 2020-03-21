4ee4ec67e07a8f62777e080ac6e610b6128aad71
PredaaA <46051820+PredaaA@users.noreply.github.com>
2020-03-21 22:22:19 +0100

    is

--- redbot/cogs/general/general.py
+++ redbot/cogs/general/general.py
@@ -306,7 +306,7 @@ class General(commands.Cog):
                 "\N{LARGE RED CIRCLE}": lambda x: x.status is discord.Status.do_not_disturb,
                 "\N{MEDIUM WHITE CIRCLE}": lambda x: x.status is discord.Status.offline,
                 "\N{LARGE PURPLE CIRCLE}": lambda x: (
-                    x.activity is not None and x.activity.type == discord.ActivityType.streaming
+                    x.activity is not None and x.activity.type is discord.ActivityType.streaming
                 ),
                 "\N{MOBILE PHONE}": lambda x: x.is_on_mobile(),
             }
