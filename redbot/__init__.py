5e90eda2cc8e766e0da43d7bd3f904ca62a56953
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-03-28 18:56:01 +0100

    Update __init__.py

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -215,9 +215,11 @@ if "--debug" not in _sys.argv:
     _warnings.filterwarnings("ignore", category=DeprecationWarning, module="aiohttp", lineno=21)
     # DeprecationWarning: rename klass to create_protocol
     #   warnings.warn("rename klass to create_protocol", DeprecationWarning)
-    #
-    # discord.py is using deprecated kwarg name when making websockets connection
-    # https://github.com/Rapptz/discord.py/issues/2574
     _warnings.filterwarnings(
         "ignore", category=DeprecationWarning, module="websockets", lineno=407
+    )
+    # DeprecationWarning: The loop argument is deprecated since Python 3.8, and scheduled for removal in Python 3.10.
+    #   transport, protocol = await self._create_connection()
+    _warnings.filterwarnings(
+        "ignore", category=DeprecationWarning, module="websockets", lineno=535
     )
