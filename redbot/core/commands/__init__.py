85e50991d0897d3279405e0af21c764cd18d55ef
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-04-05 19:18:02 +0200

    add `commands.before/after_invoke`

--- redbot/core/commands/__init__.py
+++ redbot/core/commands/__init__.py
@@ -76,6 +76,8 @@ from ._dpy_reimplements import (
     when_mentioned_or as when_mentioned_or,
     when_mentioned as when_mentioned,
     bot_has_any_role as bot_has_any_role,
+    before_invoke as before_invoke,
+    after_invoke as after_invoke,
 )
 
 ### DEP-WARN: Check this *every* discord.py update
