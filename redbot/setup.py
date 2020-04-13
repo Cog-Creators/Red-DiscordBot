294b05bf00cb253f205fe855760c12c4cf587eae
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-04-13 23:14:40 +0100

    lets not fuck it up again
    Signed-off-by: Drapersniper <27962761+drapersniper@users.noreply.github.com>

--- redbot/setup.py
+++ redbot/setup.py
@@ -105,13 +105,14 @@ def get_data_dir(instance_name: str):
 
 
 def get_storage_type():
-    storage_dict = {1: "JSON", 2: "PostgreSQL"}
+    storage_dict = {1: "JSON", 2: "PostgreSQL", 3: "RedisJSON"}
     storage = None
     while storage is None:
         print()
         print("Please choose your storage backend (if you're unsure, just choose 1).")
         print("1. JSON (file storage, requires no database).")
         print("2. PostgreSQL (Requires a database server)")
+        print("3. RedisJSON (Requires a redis server with the RedisJSON plugin)")
         storage = input("> ")
         try:
             storage = int(storage)
@@ -167,7 +168,7 @@ def basic_setup():
 
     storage = get_storage_type()
 
-    storage_dict = {1: BackendType.JSON, 2: BackendType.POSTGRES}
+    storage_dict = {1: BackendType.JSON, 2: BackendType.POSTGRES, 3: BackendType.REDIS}
     storage_type: BackendType = storage_dict.get(storage, BackendType.JSON)
     default_dirs["STORAGE_TYPE"] = storage_type.value
     driver_cls = drivers.get_driver_class(storage_type)
@@ -201,6 +202,8 @@ def get_target_backend(backend) -> BackendType:
         return BackendType.JSON
     elif backend == "postgres":
         return BackendType.POSTGRES
+    elif backend == "redis":
+        return BackendType.REDIS
 
 
 async def do_migration(
@@ -387,7 +390,7 @@ def delete(
 
 @cli.command()
 @click.argument("instance", type=click.Choice(instance_list), metavar="<INSTANCE_NAME>")
-@click.argument("backend", type=click.Choice(["json", "postgres"]))
+@click.argument("backend", type=click.Choice(["json", "postgres", "redis"]))
 def convert(instance, backend):
     """Convert data backend of an instance."""
     current_backend = get_current_backend(instance)
@@ -400,7 +403,8 @@ def convert(instance, backend):
     if current_backend == BackendType.MONGOV1:
         raise RuntimeError("Please see the 3.2 release notes for upgrading a bot using mongo.")
     else:
-        new_storage_details = asyncio.run(do_migration(current_backend, target))
+        loop = asyncio.get_event_loop()
+        new_storage_details = loop.run_until_complete(do_migration(current_backend, target))
 
     if new_storage_details is not None:
         default_dirs["STORAGE_TYPE"] = target.value
