807f65f5ae6eed91da492c6a4d664ca89bcf970a
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-05-11 18:02:45 +0200

    properly define deps, remove aioredis_lock import

--- redbot/core/drivers/redis/redis.py
+++ redbot/core/drivers/redis/redis.py
@@ -12,7 +12,6 @@ from secrets import compare_digest
 try:
     # pylint: disable=import-error
     import aioredis
-    import aioredis_lock
     from .client_interface import Client
 except ModuleNotFoundError:
     aioredis = None
