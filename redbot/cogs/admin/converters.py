2e2c34cbefc7648554acc29b466802cbb66ad5f9
droon <chasehult@gmail.com>
2020-05-22 19:06:24 -0700

    Change pool to set

--- redbot/cogs/admin/converters.py
+++ redbot/cogs/admin/converters.py
@@ -7,22 +7,22 @@ _ = Translator("AdminConverters", __file__)
 
 class SelfRole(commands.Converter):
     async def convert(self, ctx: commands.Context, arg: str) -> discord.Role:
-        admin = ctx.command.cog.bot.get_cog("Admin")
+        admin = ctx.command.cog
         if admin is None:
             raise commands.BadArgument(_("The Admin cog is not loaded."))
 
         selfroles = await admin.config.guild(ctx.guild).selfroles()
         role_converter = commands.RoleConverter()
 
-        pool = []
+        pool = set()
         for role_id in selfroles:
             role = ctx.guild.get_role(role_id)
             if role is None:
                 continue
             if role.name.lower() == arg.lower():
-                pool.append(role)
+                pool.add(role)
 
-        if not pool :
+        if not pool:
             role = await role_converter.convert(ctx, arg)
             if role.id not in selfroles:
                 raise commands.BadArgument(_("The provided role is not a valid selfrole."))
@@ -34,6 +34,6 @@ class SelfRole(commands.Converter):
                 )
             )
         else:
-            role = pool[0]
+            role = pool.pop()
 
         return role
