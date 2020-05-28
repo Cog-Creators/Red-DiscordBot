a9acb80132e5dd1620758b3bd08a783b591b100f
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-05-28 23:58:52 +0200

    Use `bot.send_help_for` in `[p]alias help` instead of "hacky way" (#3864)
    * Use `bot.send_help_for` instead of this "hack" in `[p]alias help`
    
    * This isn't needed here

--- redbot/cogs/alias/alias.py
+++ redbot/cogs/alias/alias.py
@@ -258,14 +258,7 @@ class Alias(commands.Cog):
         """Try to execute help for the base command of the alias."""
         alias = await self._aliases.get_alias(ctx.guild, alias_name=alias_name)
         if alias:
-            if self.is_command(alias.command):
-                base_cmd = alias.command
-            else:
-                base_cmd = alias.command.rsplit(" ", 1)[0]
-
-            new_msg = copy(ctx.message)
-            new_msg.content = f"{ctx.prefix}help {base_cmd}"
-            await self.bot.process_commands(new_msg)
+            await self.bot.send_help_for(ctx, alias.command)
         else:
             await ctx.send(_("No such alias exists."))
 
