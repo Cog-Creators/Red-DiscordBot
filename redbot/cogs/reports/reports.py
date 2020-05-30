c9cfa92b044e935f8a923394ff786e8a743b68f9
Vexed01 <51716387+Vexed01@users.noreply.github.com>
2020-05-30 19:33:32 +0100

    add class docstring to reports

--- redbot/cogs/reports/reports.py
+++ redbot/cogs/reports/reports.py
@@ -23,7 +23,13 @@ log = logging.getLogger("red.reports")
 
 @cog_i18n(_)
 class Reports(commands.Cog):
+    """Create user reports that server staff can respond to.
 
+    Users can open reports using `[p]report`. These are then
+    sent to a channel in the server, where staff members can
+    respond.
+    """
+    
     default_guild_settings = {"output_channel": None, "active": False, "next_ticket": 1}
 
     default_report = {"report": {}}
