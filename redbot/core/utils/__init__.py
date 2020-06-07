0d032e13c881f915e8ba2ffae17a6d83d53e9cd2
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-06-07 15:27:46 +0100

    ffs

--- redbot/core/utils/__init__.py
+++ redbot/core/utils/__init__.py
@@ -476,7 +476,7 @@ class AsyncIter(AsyncIterator[_T], Awaitable[List[_T]]):  # pylint: disable=dupl
         Examples
         --------
         >>> from redbot.core.utils import AsyncIter
-        >>> async for value in AsyncIter(range(3)).map(bool)
+        >>> async for value in AsyncIter(range(3)).map(bool):
         ...     print(value)
         False
         True
