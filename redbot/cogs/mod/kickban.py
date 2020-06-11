60a5553f88ffd16d7221ac662d503a68ca2e9066
Dav <57032623+Dav-Git@users.noreply.github.com>
2020-06-11 15:49:51 +0200

    insert facepalm here

--- redbot/cogs/mod/kickban.py
+++ redbot/cogs/mod/kickban.py
@@ -412,6 +412,7 @@ class KickBanMixin(MixinMeta):
             await ctx.send(
                 _("I cannot let you do that. Self-harm is bad {}").format("\N{PENSIVE FACE}")
             )
+            return
         elif not await is_allowed_by_hierarchy(self.bot, self.config, guild, author, user):
             await ctx.send(
                 _(
@@ -420,8 +421,10 @@ class KickBanMixin(MixinMeta):
                     "hierarchy."
                 )
             )
+            return
         elif guild.me.top_role <= user.top_role or user == guild.owner:
             await ctx.send(_("I cannot do that due to discord hierarchy rules"))
+            return
         invite = await self.get_invite_for_reinvite(ctx, int(days_delta.total_seconds() + 86400))
         if invite is None:
             invite = ""
