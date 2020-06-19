fa1abe1f9444b53bb01b7b57d95d3d27e2cefa3f
Vexed <sebdazeley@gmail.com>
2020-06-19 15:04:20 +0100

    this is just vexed tries and jack fixes
    Co-authored-by: jack1142 <6032823+jack1142@users.noreply.github.com>

--- redbot/setup.py
+++ redbot/setup.py
@@ -136,7 +136,7 @@ def get_name() -> str:
         if re.fullmatch(r"[A-Za-z0-9_\.\-]*", name) is None:
             print(
                 "ERROR: Instance names can only include characters A-z, numbers, "
-                "underscores (_) and periods(.)."
+                "underscores (_) and periods (.)."
             )
             name = ""
         elif "-" in name and not confirm(
