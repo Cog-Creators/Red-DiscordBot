f84f1d7e345be32cc707c7fd3c3f3449402ee1f3
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-06-21 20:09:49 +0100

    add new owner logic

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -216,6 +216,7 @@ class RedBase(
         self._main_dir = bot_dir
         self._cog_mgr = CogManager()
         self._use_team_features = cli_flags.use_team_features
+        self._true_owner_ids = kwargs.pop("owner_ids", set())
         # to prevent multiple calls to app info in `is_owner()`
         self._app_owners_fetched = False
         super().__init__(*args, help_command=None, **kwargs)
@@ -928,7 +929,7 @@ class RedBase(
         if self._owner_id_overwrite is None:
             self._owner_id_overwrite = await self._config.owner()
         if self._owner_id_overwrite is not None:
-            self.owner_ids.add(self._owner_id_overwrite)
+            self._true_owner_ids.add(self._owner_id_overwrite)
 
         i18n_locale = await self._config.locale()
         i18n.set_locale(i18n_locale)
@@ -1137,11 +1138,11 @@ class RedBase(
             if app.team:
                 if self._use_team_features:
                     ids = {m.id for m in app.team.members}
-                    self.owner_ids.update(ids)
+                    self._true_owner_ids.update(ids)
                     ret = user.id in ids
             elif self._owner_id_overwrite is None:
                 owner_id = app.owner.id
-                self.owner_ids.add(owner_id)
+                self._true_owner_ids.add(owner_id)
                 ret = user.id == owner_id
             self._app_owners_fetched = True
 
@@ -1623,7 +1624,7 @@ class RedBase(
         await self.wait_until_red_ready()
         destinations = []
         opt_outs = await self._config.owner_opt_out_list()
-        for user_id in self.owner_ids:
+        for user_id in self._true_owner_ids:
             if user_id not in opt_outs:
                 user = self.get_user(user_id)
                 if user and not user.bot:  # user.bot is possible with flags and teams
