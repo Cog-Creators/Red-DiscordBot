579774103c82b920ceeab3da1f5b141462fe69da
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-07-08 15:21:27 +0100

    one last tweak

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -2016,7 +2016,7 @@ class Core(commands.Cog, CoreLogic):
         names = [getattr(u_or_r, "name", u_or_r) for u_or_r in users_or_roles]
         uids = {getattr(u_or_r, "id", u_or_r) for u_or_r in users_or_roles}
         if not (ctx.guild.owner == ctx.author or await self.bot.is_owner(ctx.author)):
-            current_whitelist = set(await self.bot._whiteblacklist_cache.get_whitelist(ctx.guild))
+            current_whitelist = await self.bot._whiteblacklist_cache.get_whitelist(ctx.guild)
             theoretical_whitelist = current_whitelist.union(uids)
             ids = {i for i in (ctx.author.id, *(getattr(ctx.author, "_roles", [])))}
             if ids.isdisjoint(theoretical_whitelist):
@@ -2027,7 +2027,7 @@ class Core(commands.Cog, CoreLogic):
                         "please ensure to add yourself to the whitelist first."
                     )
                 )
-        await self.bot._whiteblacklist_cache.add_to_whitelist(ctx.guild, list(uids))
+        await self.bot._whiteblacklist_cache.add_to_whitelist(ctx.guild, uids)
 
         await ctx.send(_("{names} added to whitelist.").format(names=humanize_list(names)))
 
@@ -2063,7 +2063,7 @@ class Core(commands.Cog, CoreLogic):
         names = [getattr(u_or_r, "name", u_or_r) for u_or_r in users_or_roles]
         uids = {getattr(u_or_r, "id", u_or_r) for u_or_r in users_or_roles}
         if not (ctx.guild.owner == ctx.author or await self.bot.is_owner(ctx.author)):
-            current_whitelist = set(await self.bot._whiteblacklist_cache.get_whitelist(ctx.guild))
+            current_whitelist = await self.bot._whiteblacklist_cache.get_whitelist(ctx.guild)
             theoretical_whitelist = current_whitelist - uids
             ids = {i for i in (ctx.author.id, *(getattr(ctx.author, "_roles", [])))}
             if theoretical_whitelist and ids.isdisjoint(theoretical_whitelist):
@@ -2073,7 +2073,7 @@ class Core(commands.Cog, CoreLogic):
                         "remove your ability to run commands."
                     )
                 )
-        await self.bot._whiteblacklist_cache.remove_from_whitelist(ctx.guild, list(uids))
+        await self.bot._whiteblacklist_cache.remove_from_whitelist(ctx.guild, uids)
 
         await ctx.send(
             _("{names} removed from the local whitelist.").format(names=humanize_list(names))
