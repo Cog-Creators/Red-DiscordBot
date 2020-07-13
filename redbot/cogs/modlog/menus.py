1e4b2193524fc3d4df980fd2a54e06a4b0661547
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-07-13 14:08:08 +0100

    add Case counter for embeded message

--- redbot/cogs/modlog/menus.py
+++ redbot/cogs/modlog/menus.py
@@ -6,6 +6,7 @@ import discord
 
 from redbot.core.i18n import Translator
 from redbot.core.modlog import Case
+from redbot.core.utils.chat_formatting import humanize_number
 
 from redbot.core.utils.menus import SimpleHybridMenu
 from redbot.vendored.discord.ext import menus
@@ -18,4 +19,12 @@ class CasesForSource(menus.ListPageSource):
         super().__init__(cases, per_page=1)
 
     async def format_page(self, menu: SimpleHybridMenu, case: Case) -> Union[discord.Embed, str]:
-        return await case.message_content(embed=await menu.ctx.embed_requested())
+        current_entry = menu.current_page + 1
+        total_entries = self._max_pages
+        message = await case.message_content(embed=await menu.ctx.embed_requested())
+        if total_entries > 1 and isinstance(message, discord.Embed):
+            text = _("Case: {page_num}/{total_pages}\n").format(
+                page_num=humanize_number(current_entry),
+                total_pages=humanize_number(max(1, total_entries)),
+            )
+            message.set_footer(text=text)
