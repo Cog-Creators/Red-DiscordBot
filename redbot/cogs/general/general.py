4f10854533c9649b4083130983573e54a4d53f54
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-07-13 13:59:19 +0100

    general.py

--- redbot/cogs/general/general.py
+++ redbot/cogs/general/general.py
@@ -6,9 +6,9 @@ from typing import Final
 import urllib.parse
 import aiohttp
 import discord
+
 from redbot.core import commands
 from redbot.core.i18n import Translator, cog_i18n
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from redbot.core.utils.chat_formatting import (
     bold,
     escape,
@@ -16,6 +16,8 @@ from redbot.core.utils.chat_formatting import (
     humanize_number,
     humanize_timedelta,
 )
+from redbot.core.utils.menus import SimpleHybridMenu
+from .menus import UrbanSource
 
 _ = T_ = Translator("General", __file__)
 
@@ -477,7 +479,7 @@ class General(commands.Cog):
         await ctx.send(embed=data)
 
     @commands.command()
-    async def urban(self, ctx, *, word):
+    async def urban(self, ctx: commands.Context, *, word: str.lower):
         """Search the Urban Dictionary.
 
         This uses the unofficial Urban Dictionary API.
@@ -489,7 +491,6 @@ class General(commands.Cog):
             params = {"term": str(word).lower()}
 
             headers = {"content-type": "application/json"}
-
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, headers=headers, params=params) as response:
                     data = await response.json()
@@ -503,66 +504,9 @@ class General(commands.Cog):
         if data.get("error") != 404:
             if not data.get("list"):
                 return await ctx.send(_("No Urban Dictionary entries were found."))
-            if await ctx.embed_requested():
-                # a list of embeds
-                embeds = []
-                for ud in data["list"]:
-                    embed = discord.Embed()
-                    title = _("{word} by {author}").format(
-                        word=ud["word"].capitalize(), author=ud["author"]
-                    )
-                    if len(title) > 256:
-                        title = "{}...".format(title[:253])
-                    embed.title = title
-                    embed.url = ud["permalink"]
-
-                    description = _("{definition}\n\n**Example:** {example}").format(**ud)
-                    if len(description) > 2048:
-                        description = "{}...".format(description[:2045])
-                    embed.description = description
-
-                    embed.set_footer(
-                        text=_(
-                            "{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary."
-                        ).format(**ud)
-                    )
-                    embeds.append(embed)
-
-                if embeds is not None and len(embeds) > 0:
-                    await menu(
-                        ctx,
-                        pages=embeds,
-                        controls=DEFAULT_CONTROLS,
-                        message=None,
-                        page=0,
-                        timeout=30,
-                    )
-            else:
-                messages = []
-                for ud in data["list"]:
-                    ud.setdefault("example", "N/A")
-                    message = _(
-                        "<{permalink}>\n {word} by {author}\n\n{description}\n\n"
-                        "{thumbs_down} Down / {thumbs_up} Up, Powered by Urban Dictionary."
-                    ).format(word=ud.pop("word").capitalize(), description="{description}", **ud)
-                    max_desc_len = 2000 - len(message)
-
-                    description = _("{definition}\n\n**Example:** {example}").format(**ud)
-                    if len(description) > max_desc_len:
-                        description = "{}...".format(description[: max_desc_len - 3])
-
-                    message = message.format(description=description)
-                    messages.append(message)
-
-                if messages is not None and len(messages) > 0:
-                    await menu(
-                        ctx,
-                        pages=messages,
-                        controls=DEFAULT_CONTROLS,
-                        message=None,
-                        page=0,
-                        timeout=30,
-                    )
+            await SimpleHybridMenu(
+                source=UrbanSource(data["list"]), cog=self, delete_message_after=True,
+            ).start(ctx=ctx, wait=False)
         else:
             await ctx.send(
                 _("No Urban Dictionary entries were found, or there was an error in the process.")
