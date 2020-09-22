aa2df3ab836ed34524ea5cd0617b639085fc34b2
MrBillson <71342555+MrBillson@users.noreply.github.com>
2020-09-21 20:10:24 -0700

    Update bank.py
    Added Tuple import from typing

--- redbot/core/bank.py
+++ redbot/core/bank.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import asyncio
 import logging
 from datetime import datetime, timezone
-from typing import Union, List, Optional, TYPE_CHECKING, Literal
+from typing import Union, List, Optional, TYPE_CHECKING, Literal, Tuple
 from functools import wraps
 
 import discord
