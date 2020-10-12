3ba35e2c0b9ae21a0af566c18292ea20baaad2ca
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-10-12 19:05:19 +0100

    resolve last review

--- redbot/cogs/audio/core/commands/player.py
+++ redbot/cogs/audio/core/commands/player.py
@@ -873,7 +873,7 @@ class PlayerCommands(MixinMeta, metaclass=CompositeMetaClass):
                         ctx,
                         title=_("Unable to Get Track"),
                         description=_(
-                            "I'm unable to get a track from Lavalink at the moment,"
+                            "I'm unable to get a track from Lavalink at the moment, "
                             "try again in a few minutes."
                         ),
                     )
