b345e56cf4c183c6fb32678d47eaaa93fafb854a
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-10-15 12:01:17 +0100

    handles #4491

--- redbot/cogs/audio/core/commands/audioset.py
+++ redbot/cogs/audio/core/commands/audioset.py
@@ -1378,7 +1378,7 @@ class AudioSetCommands(MixinMeta, metaclass=CompositeMetaClass):
     async def command_audioset_audiodb_toggle(self, ctx: commands.Context):
         """Toggle the server settings.
 
-        Default is ON
+        Default is OFF
         """
         state = await self.config.global_db_enabled()
         await self.config.global_db_enabled.set(not state)
