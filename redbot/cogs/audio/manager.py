9ca025ff153b15ec1a99da8ef9688db232017288
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-10-16 09:59:39 +0100

    aaaaaaaaa

--- redbot/cogs/audio/manager.py
+++ redbot/cogs/audio/manager.py
@@ -10,17 +10,16 @@ import shutil
 import sys
 import tempfile
 import time
-
 from typing import ClassVar, Final, List, Optional, Pattern, Tuple
 
 import aiohttp
+from tqdm import tqdm
 
 from redbot.core import data_manager
-from tqdm import tqdm
+from redbot.core.i18n import Translator
 
 from .errors import LavalinkDownloadFailed
 from .utils import task_callback
-from ...core.i18n import Translator
 
 _ = Translator("Audio", pathlib.Path(__file__))
 log = logging.getLogger("red.audio.manager")
