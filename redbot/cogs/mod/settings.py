b7f2a5fff3b25a8cf1d49287420ef07a31814ca2
aikaterna <20862007+aikaterna@users.noreply.github.com>
2020-10-25 11:54:51 -0700

    Don't need the extra parens here

--- redbot/cogs/mod/settings.py
+++ redbot/cogs/mod/settings.py
@@ -395,6 +395,6 @@ class ModSettings(MixinMeta):
         await self.config.guild(guild).default_tempban_duration.set(duration.total_seconds())
         await ctx.send(
             _("The default duration for tempbanning a user is now {duration}.").format(
-                duration=(humanize_timedelta(timedelta=duration))
+                duration=humanize_timedelta(timedelta=duration)
             )
         )
