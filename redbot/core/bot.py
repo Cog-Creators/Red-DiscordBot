6b6c38e786d748fbc03fba1c11056510e03ef266
npc203 <npc203@users.noreply.github.com>
2020-11-24 22:23:34 +0530

    remove_command returns a dpy command object

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -1374,7 +1374,7 @@ class RedBase(
                 if permissions_not_loaded:
                     subcommand.requires.ready_event.set()
 
-    def remove_command(self, name: str) -> None:
+    def remove_command(self, name: str) -> dpy_commands:
         command = super().remove_command(name)
         if not command:
             return
@@ -1382,6 +1382,7 @@ class RedBase(
         if isinstance(command, commands.Group):
             for subcommand in command.walk_commands():
                 subcommand.requires.reset()
+        return command
 
     def clear_permission_rules(self, guild_id: Optional[int], **kwargs) -> None:
         """Clear all permission overrides in a scope.
