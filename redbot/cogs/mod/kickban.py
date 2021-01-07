9a9caafe5ce689918698fb176215773b1b6eaf77
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-01-07 16:08:07 +0100

    smh

--- redbot/cogs/mod/kickban.py
+++ redbot/cogs/mod/kickban.py
@@ -264,6 +264,7 @@ class KickBanMixin(MixinMeta):
     @checks.admin_or_permissions(kick_members=True)
     async def kick(self, ctx: commands.Context, user: discord.Member, *, reason: str = None):
         """Kick a user.
+        
         Examples:
             `[p]kick 428675506947227648 wanted to be kicked.`
             This will kick Twentysix from the server.
