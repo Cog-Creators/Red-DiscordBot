415c15509dda5fb997fa3a993afa15497b81ce85
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-01-22 00:09:06 +0100

    Raise `StreamNotFound` for 404 errors in YouTube RSS feed

--- redbot/cogs/streams/streamtypes.py
+++ redbot/cogs/streams/streamtypes.py
@@ -106,6 +106,8 @@ class YoutubeStream(Stream):
 
         async with aiohttp.ClientSession() as session:
             async with session.get(YOUTUBE_CHANNEL_RSS.format(channel_id=self.id)) as r:
+                if r.status == 404:
+                    raise StreamNotFound()
                 rssdata = await r.text()
 
         if self.not_livestreams:
