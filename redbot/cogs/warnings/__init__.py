8279001537d599eae0affbac0c9291757135c131
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-01-23 21:22:43 +0100

    Wrong `__init__.py` :|

--- redbot/cogs/warnings/__init__.py
+++ redbot/cogs/warnings/__init__.py
@@ -1,9 +1,5 @@
-from redbot.core.bot import Red
-
 from .warnings import Warnings
 
 
-async def setup(bot: Red) -> None:
-    cog = Warnings(bot)
-    await cog.initialize()
-    bot.add_cog(cog)
+def setup(bot):
+    bot.add_cog(Warnings(bot))
