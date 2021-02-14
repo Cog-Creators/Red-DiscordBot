eb20281713ba6e114fb294519e08a1794e284736
maxbooiii <maxbooiii@outlook.com>
2021-02-14 14:21:46 +0100

    style

--- redbot/cogs/cleanup/cleanup.py
+++ redbot/cogs/cleanup/cleanup.py
@@ -608,7 +608,9 @@ class Cleanup(commands.Cog):
     @commands.guild_only()
     @checks.mod_or_permissions(manage_messages=True)
     @commands.bot_has_permissions(manage_messages=True)
-    async def cleanup_duplicates(self, ctx: commands.Context, number: positive_int = PositiveInt(50)):
+    async def cleanup_duplicates(
+        self, ctx: commands.Context, number: positive_int = PositiveInt(50)
+    ):
         """Deletes duplicate messages in the channel from the last X messages and keeps only one copy.
 
         Defaults to 50.
