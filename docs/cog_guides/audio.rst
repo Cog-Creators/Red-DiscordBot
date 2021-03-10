3478b0b57fe4aa6d5a20e7aff8137430c38b0845
bobloy <alboblexloy@gmail.com>
2021-03-10 10:12:53 -0500

    WIP to make lists work with ZWSP

--- docs/cog_guides/audio.rst
+++ docs/cog_guides/audio.rst
@@ -1568,14 +1568,14 @@ Playlist configuration options.
 
 Scope info:
 ​ ​ ​ ​ **Global**:
-​ ​ ​ ​ ​ ​ ​ ​ Visible to all users of this bot.
-​ ​ ​ ​ ​ ​ ​ ​ Only editable by bot owner.
+​ ​ ​ ​ ​ ​ ​ ​ - Visible to all users of this bot.
+​ ​ ​ ​ ​ ​ ​ ​ - Only editable by bot owner.
 ​ ​ ​ ​ **Guild**:
-​ ​ ​ ​ ​ ​ ​ ​ Visible to all users in this guild.
-​ ​ ​ ​ ​ ​ ​ ​ Editable by bot owner, guild owner, guild admins, guild mods, DJ role and playlist creator.
+​ ​ ​ ​ ​ ​ ​ ​ - Visible to all users in this guild.
+​ ​ ​ ​ ​ ​ ​ ​ - Editable by bot owner, guild owner, guild admins, guild mods, DJ role and playlist creator.
 ​ ​ ​ ​ **User**:
-​ ​ ​ ​ ​ ​ ​ ​ Visible to all bot users, if --author is passed.
-​ ​ ​ ​ ​ ​ ​ ​ Editable by bot owner and creator.
+​ ​ ​ ​ ​ ​ ​ ​ - Visible to all bot users, if --author is passed.
+​ ​ ​ ​ ​ ​ ​ ​ - Editable by bot owner and creator.
 
 .. _audio-command-playlist-append:
 
--- redbot/cogs/audio/core/commands/playlists.py
+++ redbot/cogs/audio/core/commands/playlists.py
@@ -45,14 +45,14 @@ class PlaylistCommands(MixinMeta, metaclass=CompositeMetaClass):
 
         Scope info:
         ​ ​ ​ ​ **Global**:
-        ​ ​ ​ ​ ​ ​ ​ ​ Visible to all users of this bot.
-        ​ ​ ​ ​ ​ ​ ​ ​ Only editable by bot owner.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Visible to all users of this bot.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Only editable by bot owner.
         ​ ​ ​ ​ **Guild**:
-        ​ ​ ​ ​ ​ ​ ​ ​ Visible to all users in this guild.
-        ​ ​ ​ ​ ​ ​ ​ ​ Editable by bot owner, guild owner, guild admins, guild mods, DJ role and playlist creator.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Visible to all users in this guild.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Editable by bot owner, guild owner, guild admins, guild mods, DJ role and playlist creator.
         ​ ​ ​ ​ **User**:
-        ​ ​ ​ ​ ​ ​ ​ ​ Visible to all bot users, if --author is passed.
-        ​ ​ ​ ​ ​ ​ ​ ​ Editable by bot owner and creator.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Visible to all bot users, if --author is passed.
+        ​ ​ ​ ​ ​ ​ ​ ​ - Editable by bot owner and creator.
         """
 
     @command_playlist.command(
