a975547c7760f9dea021aa50cd70473de1ed5a25
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-04-27 15:25:32 +0100

    Implement the new Volume and EQ class across Audio usages

--- redbot/cogs/audio/core/commands/equalizer.py
+++ redbot/cogs/audio/core/commands/equalizer.py
@@ -337,6 +337,7 @@ class EqualizerCommands(MixinMeta, metaclass=CompositeMetaClass):
             "16k",
         ]
 
+        bands_num = player.equalizer.band_count
         if band_value > 1:
             band_value = 1
         elif band_value <= -0.25:
