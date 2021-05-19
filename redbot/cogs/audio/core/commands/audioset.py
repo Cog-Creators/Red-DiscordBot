f3231682b0b24bbf8ccb4d20eee13fb53fe1a95a
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-05-19 20:17:37 +0100

    Update application.yml + remove mixer support. (#5013)

--- redbot/cogs/audio/core/commands/audioset.py
+++ redbot/cogs/audio/core/commands/audioset.py
@@ -923,7 +923,7 @@ class AudioSetCommands(MixinMeta, metaclass=CompositeMetaClass):
         """Toggle the domain restriction on Audio.
 
         When toggled off, users will be able to play songs from non-commercial websites and links.
-        When toggled on, users are restricted to YouTube, SoundCloud, Mixer, Vimeo, Twitch, and
+        When toggled on, users are restricted to YouTube, SoundCloud, Vimeo, Twitch, and
         Bandcamp links.
         """
         restrict = await self.config.restrict()
