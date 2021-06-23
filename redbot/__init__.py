18937f38e09547e2bf918f50020e7e5ac237e30c
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-06-23 20:30:30 +0200

    Version bump to 3.5.0.dev1

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -214,7 +214,7 @@ def _early_init():
     _ensure_no_colorama()
 
 
-__version__ = "3.4.13.dev1"
+__version__ = "3.5.0.dev1"
 version_info = VersionInfo.from_str(__version__)
 
 # Filter fuzzywuzzy slow sequence matcher warning
