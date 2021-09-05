cb2242a2458cd1b24153c475f31fba23df8f64c0
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-09-05 19:57:48 +0200

    Add Python 3.10-dev to CI

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
+import sys
 from setuptools import setup
 
-# Metadata and options defined in setup.cfg
-setup()
+if os.getenv("TOX_RED", False) and sys.version_info >= (3, 10):
+    # We want to be able to test Python versions that we do not support yet.
+    setup(python_requires=">=3.8.1")
+else:
+    # Metadata and options defined in setup.cfg
+    setup()
