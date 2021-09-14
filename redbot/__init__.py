998e310082174bde28efddb10e9ddce8eb5c2a03
rich <rich@fakeemail.com>
2021-09-14 01:58:13 -0700

    Updated again...

--- redbot/__init__.py
+++ redbot/__init__.py
@@ -214,7 +214,7 @@ def _early_init():
     _ensure_no_colorama()
 
 
-__version__ = "0.0.0.dev"
+__version__ = "0.0.0"
 version_info = VersionInfo.from_str(__version__)
 
 # Filter fuzzywuzzy slow sequence matcher warning
