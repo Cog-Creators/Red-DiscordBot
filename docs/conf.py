39169703a78411e9a0cb5553dab5e96c9b66a469
Red-GitHubBot <88117545+Red-GitHubBot@users.noreply.github.com>
2021-09-23 05:28:59 +0200

    [3.4] Split documents per OS, fix few things, drop and add few OSes (#5328) (#5331)
    * Exclude include files from being built
    
    * Split install guides into multiple documents (one per OS/version combo)
    
    * Update references
    
    * Unify anchor names
    
    * Update pyenv requirements in CentOS 7 guide
    
    * Update install guides for RHEL derivatives to not use pyenv
    
    * Add guide for Rocky Linux 8
    
    * 8.4+ -> 8.4-8.x
    
    * Add 'the'
    (cherry picked from commit cbbb9d9b9b5a4734a70ac1dba65b2bc667331c63)
    
    Co-authored-by: jack1142 <6032823+jack1142@users.noreply.github.com>
    
    Co-authored-by: jack1142 <6032823+jack1142@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,13 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    # to ensure that include files (partial pages) aren't built, exclude them
+    "**/_includes/**",
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
