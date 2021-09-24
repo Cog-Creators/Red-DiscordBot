224089f7980da24c9bc583cb81a0a6099ea1a300
rich <rich@fakeemail.com>
2021-09-24 02:35:07 -0700

    Experimenting

--- redbot/core/events.py
+++ redbot/core/events.py
@@ -352,6 +352,7 @@ def init_events(bot, cli_flags):
         await set_contextual_locales_from_guild(bot, message.guild)
 
         await bot.process_commands(message)
+
         discord_now = message.created_at
         if (
             not bot._checked_time_accuracy
