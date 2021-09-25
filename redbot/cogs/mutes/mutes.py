61bbd1b892d7029dc81abc52c63f26f25edff5bc
veni-vivi-code <tom.mucke@web.de>
2021-09-26 00:36:40 +0200

    Fix #5161

--- redbot/cogs/mutes/mutes.py
+++ redbot/cogs/mutes/mutes.py
@@ -1602,15 +1602,18 @@ class Mutes(VoiceMutes, commands.Cog, metaclass=CompositeMetaClass):
             if not role:
                 ret["reason"] = _(MUTE_UNMUTE_ISSUES["role_missing"])
                 return ret
-
-            if guild.id in self._server_mutes:
-                if user.id in self._server_mutes[guild.id]:
-                    del self._server_mutes[guild.id][user.id]
             if not guild.me.guild_permissions.manage_roles or role >= guild.me.top_role:
                 ret["reason"] = _(MUTE_UNMUTE_ISSUES["permissions_issue_role"])
                 return ret
+
             try:
-                await user.remove_roles(role, reason=reason)
+                if role in user.roles:
+                    await user.remove_roles(role, reason=reason)
+                elif guild.id in self._server_mutes and user.id in self._server_mutes[guild.id]:
+                    del self._server_mutes[guild.id][user.id]
+                else:
+                    ret["reason"] = _(MUTE_UNMUTE_ISSUES["already_unmuted"])
+                    return ret
             except discord.errors.Forbidden:
                 ret["reason"] = _(MUTE_UNMUTE_ISSUES["permissions_issue_role"])
                 return ret
