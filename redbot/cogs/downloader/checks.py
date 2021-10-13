47e488521d9f91cb75b519ad2efba2738733111f
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-10-13 23:48:17 +0200

    Address review comment

--- redbot/cogs/downloader/checks.py
+++ redbot/cogs/downloader/checks.py
@@ -7,19 +7,7 @@ from redbot.core.utils.predicates import MessagePredicate
 
 __all__ = ["do_install_agreement"]
 
-T_ = Translator("DownloaderChecks", __file__)
-
-_ = lambda s: s
-REPO_INSTALL_MSG = _(
-    "You're about to add a 3rd party repository. The creator of Red"
-    " and its community have no responsibility for any potential "
-    "damage that the content of 3rd party repositories might cause."
-    "\n\nBy typing '{confirmation_message}' you declare that you have read and"
-    " fully understand the above message. This message won't be "
-    "shown again until the next reboot.\n\nYou have **30** seconds"
-    " to reply to this message."
-)
-_ = T_
+_ = Translator("DownloaderChecks", __file__)
 
 
 async def do_install_agreement(ctx: commands.Context) -> bool:
@@ -27,11 +15,24 @@ async def do_install_agreement(ctx: commands.Context) -> bool:
     if downloader is None or downloader.already_agreed:
         return True
 
-    await ctx.send(T_(REPO_INSTALL_MSG).format(confirmation_message=bold("I agree")))
+    confirmation_message = "I agree"
+    await ctx.send(
+        _(
+            "You're about to add a 3rd party repository. The creator of Red"
+            " and its community have no responsibility for any potential "
+            "damage that the content of 3rd party repositories might cause."
+            "\n\nBy typing '{confirmation_message}' you declare that you have read and"
+            " fully understand the above message. This message won't be "
+            "shown again until the next reboot.\n\nYou have **30** seconds"
+            " to reply to this message."
+        ).format(confirmation_message=bold(confirmation_message))
+    )
 
     try:
         await ctx.bot.wait_for(
-            "message", check=MessagePredicate.lower_equal_to("i agree", ctx), timeout=30
+            "message",
+            check=MessagePredicate.lower_equal_to(confirmation_message.lower(), ctx),
+            timeout=30,
         )
     except asyncio.TimeoutError:
         await ctx.send(_("Your response has timed out, please try again."))
