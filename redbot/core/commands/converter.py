ad92110f0e3b79bff99267ed51ec1cd4a00deb27
Flame442 <34169552+Flame442@users.noreply.github.com>
2021-10-15 13:20:08 -0400

    Prevent unexpected matches by asserting whitespace or eof at the end of matches

--- redbot/core/commands/converter.py
+++ redbot/core/commands/converter.py
@@ -58,13 +58,13 @@ ID_REGEX = re.compile(r"([0-9]{15,20})")
 # https://github.com/mikeshardmind/SinbadCogs/blob/816f3bc2ba860243f75112904b82009a8a9e1f99/scheduler/time_utils.py#L9-L19
 TIME_RE_STRING = r"\s?".join(
     [
-        r"((?P<years>\d+?)\s?(years?|y))?",
-        r"((?P<months>\d+?)\s?(months?|mo))?",
-        r"((?P<weeks>\d+?)\s?(weeks?|w))?",
-        r"((?P<days>\d+?)\s?(days?|d))?",
-        r"((?P<hours>\d+?)\s?(hours?|hrs|hr?))?",
-        r"((?P<minutes>\d+?)\s?(minutes?|mins?|m(?!o)))?",  # prevent matching "months"
-        r"((?P<seconds>\d+?)\s?(seconds?|secs?|s))?",
+        r"((?P<years>\d+?)\s?(years?|y)($|\s))?",
+        r"((?P<months>\d+?)\s?(months?|mo)($|\s))?",
+        r"((?P<weeks>\d+?)\s?(weeks?|w)($|\s))?",
+        r"((?P<days>\d+?)\s?(days?|d)($|\s))?",
+        r"((?P<hours>\d+?)\s?(hours?|hrs|hr?)($|\s))?",
+        r"((?P<minutes>\d+?)\s?(minutes?|mins?|m(?!o))($|\s))?",  # prevent matching "months"
+        r"((?P<seconds>\d+?)\s?(seconds?|secs?|s)($|\s))?",
     ]
 )
 
