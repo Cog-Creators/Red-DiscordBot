8a7e7c0f5fda5f871176a9bfad823f2adc5b7b12
Stonedestroyer <1307729+Stonedestroyer@users.noreply.github.com>
2021-12-24 00:40:41 +0100

    Fix docs

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -1310,7 +1310,8 @@ class Red(
         Generates the invite URL for the bot.
 
         Does not check if invites are public.
-        To check if invites are public, use `Bot.is_invite_public()`.
+        To check if invites are public, use `Bot.is_invite_url_public()`.
+        To check if invites are public, use `is_invite_url_public()`.
 
         Returns
         -------
