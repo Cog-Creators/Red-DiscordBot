f2669f5c2707770f923bffe869b42e5fce01c79d
Artemis6969 <mukherjee.aryan2020@gmail.com>
2022-01-28 23:13:04 +0530

    Fix indentation and add Credits

--- redbot/cogs/audio/core/utilities/player.py
+++ redbot/cogs/audio/core/utilities/player.py
@@ -129,12 +129,13 @@ class PlayerUtilities(MixinMeta, metaclass=CompositeMetaClass):
     async def _skip_action(self, ctx: commands.Context, skip_to_track: int = None) -> None:
         player = lavalink.get_player(ctx.guild.id)
         autoplay = await self.config.guild(player.guild).auto_play()
-        if not player.current: 
+        if not player.current:
             await self.send_embed_msg(ctx, title=_("There's nothing being played."))
             return
-        elif not player.queue and not autoplay: 
-        	await ctx.invoke(self.command_stop)
+        elif not player.queue and not autoplay:
+            await ctx.invoke(self.command_stop)  # Thank you Draper and Zephyrkul
             return
+
         elif autoplay and not player.queue:
             embed = discord.Embed(
                 title=_("Track Skipped"),
