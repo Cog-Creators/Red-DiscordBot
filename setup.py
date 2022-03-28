e0060b35055605c8cb04f1fa5c96a63af186f051
L33Tech <36166244+L33Tech@users.noreply.github.com>
2022-03-27 21:17:08 -0400

    TOX_BLUE

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup
 
-if os.getenv("TOX_RED", False) and sys.version_info >= (3, 10):
+if os.getenv("TOX_BLUE", False) and sys.version_info >= (3, 10):
     # We want to be able to test Python versions that we do not support yet.
     setup(python_requires=">=3.8.1")
 else:
