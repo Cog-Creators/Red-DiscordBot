774928fbd2da063b1d3ed47958ba3afc5ce73620
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-03-31 15:49:39 +0200

    Address StoreChannel removal

--- redbot/core/commands/__init__.py
+++ redbot/core/commands/__init__.py
@@ -125,7 +125,6 @@ from discord.ext.commands import (
     ColorConverter as ColorConverter,
     VoiceChannelConverter as VoiceChannelConverter,
     StageChannelConverter as StageChannelConverter,
-    StoreChannelConverter as StoreChannelConverter,
     NSFWChannelRequired as NSFWChannelRequired,
     IDConverter as IDConverter,
     MissingRequiredArgument as MissingRequiredArgument,
