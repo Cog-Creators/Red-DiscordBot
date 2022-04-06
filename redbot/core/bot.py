02c6d0ea039b036d888f7787e2a0bee01497809f
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-04-06 23:52:23 +0200

    Add new exit codes to ExitCodes enum

--- redbot/core/bot.py
+++ redbot/core/bot.py
@@ -2109,9 +2109,16 @@ class Red(
         )
 
 
+# This needs to be an int enum to be used
+# with sys.exit
 class ExitCodes(IntEnum):
-    # This needs to be an int enum to be used
-    # with sys.exit
-    CRITICAL = 1
+    #: Clean shutdown (through signals, keyboard interrupt, [p]shutdown, etc.).
     SHUTDOWN = 0
+    #: An unrecoverable error occurred during application's runtime.
+    CRITICAL = 1
+    #: The CLI command was used incorrectly, such as when the wrong number of arguments are given.
+    INVALID_CLI_USAGE = 2
+    #: Restart was requested by the bot owner (probably through [p]restart command).
     RESTART = 26
+    #: Some kind of configuration error occurred.
+    CONFIGURATION_ERROR = 78  # Exit code borrowed from os.EX_CONFIG.
