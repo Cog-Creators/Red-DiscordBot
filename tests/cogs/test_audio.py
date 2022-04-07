f366f75813fbe49d056f00d1b4cbe69c9aba286c
jefetienne <etienneayoub@gmail.com>
2022-04-07 13:25:18 -0400

    Add single track unit test

--- tests/cogs/test_audio.py
+++ tests/cogs/test_audio.py
@@ -3,8 +3,17 @@ from redbot.pytest.audio import *
 
 
 @pytest.mark.asyncio
-async def test_command_play_bandcamp_custom_domain(audio, ctx):
+async def test_command_play_bandcamp_custom_domain_album(audio, ctx):
     url = f"https://jazzinbritain.co.uk/album/revisiting-tanglewood-63-the-early-tapes"
+
+    inv = await audio.scrape_bandcamp_url(url)
+
+    assert inv is not None
+
+
+@pytest.mark.asyncio
+async def test_command_play_bandcamp_custom_domain_track(audio, ctx):
+    url = f"https://jazzinbritain.co.uk/track/fanfare"
 
     inv = await audio.scrape_bandcamp_url(url)
 
