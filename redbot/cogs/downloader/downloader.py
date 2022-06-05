f894150ae2c91799b63dac29218818bc4ff673d9
Jakub Kuczys <6032823+jack1142@users.noreply.github.com>
2022-06-05 19:35:41 +0200

    style

--- redbot/cogs/downloader/downloader.py
+++ redbot/cogs/downloader/downloader.py
@@ -1061,7 +1061,7 @@ class Downloader(commands.Cog):
         )
         if pinned_list:
             message = "\n".join(
-                "({}) {}".format(inline(cog.commit[:7] or _('unknown')), cog.name)
+                "({}) {}".format(inline(cog.commit[:7] or _("unknown")), cog.name)
                 for cog in pinned_list
             )
         else:
