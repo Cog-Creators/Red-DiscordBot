49db916a53ba67a028eeab3b74ee388f4f8cd9ba
L33Tech <36166244+leetfin@users.noreply.github.com>
2022-10-03 12:23:26 -0400

    Import Optional type

--- redbot/core/utils/mod.py
+++ redbot/core/utils/mod.py
@@ -1,6 +1,6 @@
 import asyncio
 from datetime import timedelta
-from typing import List, Iterable, Union, TYPE_CHECKING, Dict
+from typing import List, Iterable, Union, TYPE_CHECKING, Dict, Optional
 
 import discord
 
