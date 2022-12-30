78e66880e29e945d44cf2b1c2d9b6f39423a3ca8
Vexed <gh.vexed@gmail.com>
2022-12-30 13:53:16 +0000

    format

--- redbot/cogs/trivia/trivia.py
+++ redbot/cogs/trivia/trivia.py
@@ -31,13 +31,27 @@ TRIVIA_LIST_SCHEMA = Schema(
     {
         Optional("AUTHOR"): str,
         Optional("CONFIG"): {
-            Optional("max_score"): And(int, lambda n: n >= 1, error="max_score must be a positive integer"),
-            Optional("timeout"): And(float, lambda n: n > 0.0, error="timeout must be a positive decimal number"),
-            Optional("delay"): And(float, lambda n: n > 4.0, error="delay must be a positive decimal number greater than 4"),
+            Optional("max_score"): And(
+                int, lambda n: n >= 1, error="max_score must be a positive integer"
+            ),
+            Optional("timeout"): And(
+                float, lambda n: n > 0.0, error="timeout must be a positive decimal number"
+            ),
+            Optional("delay"): And(
+                float,
+                lambda n: n > 4.0,
+                error="delay must be a positive decimal number greater than 4",
+            ),
             Optional("bot_plays"): Const(bool, error="bot_plays must be either true or false"),
-            Optional("reveal_answer"): Const(bool, error="reveal_answer must be either true or false"),
-            Optional("payout_multiplier"): And(float, lambda n: n >= 0.0, error="payout_multiplier must a positive decimal number"),
-            Optional("use_spoilers"): Const(bool, error="use_spoilers must be either true or false"),
+            Optional("reveal_answer"): Const(
+                bool, error="reveal_answer must be either true or false"
+            ),
+            Optional("payout_multiplier"): And(
+                float, lambda n: n >= 0.0, error="payout_multiplier must a positive decimal number"
+            ),
+            Optional("use_spoilers"): Const(
+                bool, error="use_spoilers must be either true or false"
+            ),
         },
         str: [str, int, bool, float],
     }
