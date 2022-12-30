849f9be83cdaf6413aa05df11a990294d945d5e8
Jakub Kuczys <me@jacken.men>
2022-12-30 21:45:01 +0100

    Quote key names

--- redbot/cogs/trivia/schema.py
+++ redbot/cogs/trivia/schema.py
@@ -8,29 +8,29 @@ TRIVIA_LIST_SCHEMA = Schema(
         Optional("AUTHOR"): str,
         Optional("CONFIG"): {
             Optional("max_score"): And(
-                int, lambda n: n >= 1, error="max_score must be a positive integer",
+                int, lambda n: n >= 1, error="'max_score' must be a positive integer",
             ),
             Optional("timeout"): And(
                 Or(int, float),
                 lambda n: n > 0.0,
-                error="timeout must be a positive number",
+                error="'timeout' must be a positive number",
             ),
             Optional("delay"): And(
                 Or(int, float),
                 lambda n: n >= 4.0,
-                error="delay must be a positive number greater than or equal to 4",
+                error="'delay' must be a positive number greater than or equal to 4",
             ),
-            Optional("bot_plays"): Const(bool, error="bot_plays must be either true or false"),
+            Optional("bot_plays"): Const(bool, error="'bot_plays' must be either true or false"),
             Optional("reveal_answer"): Const(
-                bool, error="reveal_answer must be either true or false"
+                bool, error="'reveal_answer' must be either true or false"
             ),
             Optional("payout_multiplier"): And(
                 Or(int, float),
                 lambda n: n >= 0.0,
-                error="payout_multiplier must be a non-negative number",
+                error="'payout_multiplier' must be a non-negative number",
             ),
             Optional("use_spoilers"): Const(
-                bool, error="use_spoilers must be either true or false"
+                bool, error="'use_spoilers' must be either true or false"
             ),
         },
         str: [str, int, bool, float],
