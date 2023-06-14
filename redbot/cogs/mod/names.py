552ad8331a5ea48eafb2fe6e4bdfe23539b4e021
Jakub Kuczys <me@jacken.men>
2023-06-14 04:28:54 +0200

    Update redbot/cogs/mod/names.py
    Co-authored-by: Michael Oliveira <34169552+Flame442@users.noreply.github.com>

--- redbot/cogs/mod/names.py
+++ redbot/cogs/mod/names.py
@@ -23,7 +23,7 @@ class ModInfo(MixinMeta):
 
     async def get_names(self, member: discord.Member) -> Tuple[List[str], List[str], List[str]]:
         user_data = await self.config.user(member).all()
-        usernames, display_names = user_data["past_usernames"], user_data["past_display_names"]
+        usernames, display_names = user_data["past_names"], user_data["past_display_names"]
         nicks = await self.config.member(member).past_nicks()
         usernames = list(map(escape_spoilers_and_mass_mentions, filter(None, usernames)))
         display_names = list(map(escape_spoilers_and_mass_mentions, filter(None, display_names)))
