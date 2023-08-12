f5743c0dc6b37797fdea0653af2e318e7edc26d0
Jakub Kuczys <me@jacken.men>
2023-08-12 16:11:49 +0200

    Apply suggestions from code review

--- redbot/core/i18n.py
+++ redbot/core/i18n.py
@@ -361,10 +361,10 @@ from . import commands
 _TypeT = TypeVar("_TypeT", bound=type)
 
 
-def cog_i18n(translator: Translator):
+def cog_i18n(translator: Translator) -> Callable[[_TypeT], _TypeT]:
     """Get a class decorator to link the translator to this cog."""
 
-    def decorator(cog_class: _TypeT):
+    def decorator(cog_class: _TypeT) -> _TypeT:
         cog_class.__translator__ = translator
         for name, attr in cog_class.__dict__.items():
             if isinstance(attr, (commands.Group, commands.Command)):
