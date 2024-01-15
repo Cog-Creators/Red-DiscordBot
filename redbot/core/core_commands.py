454b79d6a72d4049f0121aeffb50fd669bfa3009
Michael Oliveira <34169552+Flame442@users.noreply.github.com>
2024-01-15 17:08:37 -0500

    Remove now incorrect note in helpset maxpages

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -4446,8 +4446,6 @@ class Core(commands.commands._RuleDropper, commands.Cog, CoreLogic):
     async def helpset_maxpages(self, ctx: commands.Context, pages: int):
         """Set the maximum number of help pages sent in a server channel.
 
-        Note: This setting does not apply to menu help.
-
         If a help message contains more pages than this value, the help message will
         be sent to the command author via DM. This is to help reduce spam in server
         text channels.
