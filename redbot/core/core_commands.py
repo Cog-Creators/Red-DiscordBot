d613e87b24754563dca1dee1b4b066e3077295ad
Jakub Kuczys <me@jacken.men>
2024-03-24 00:16:31 +0100

    Update redbot/core/core_commands.py

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -2889,7 +2889,7 @@ class Core(commands.commands._RuleDropper, commands.Cog, CoreLogic):
                 _(
                     "Failed. Remember that you can edit my avatar "
                     "up to two times a hour. The URL or attachment "
-                    "must be a valid image in either JPG, PNG or GIF format."
+                    "must be a valid image in either JPG, PNG, or GIF format."
                 )
             )
         except ValueError:
