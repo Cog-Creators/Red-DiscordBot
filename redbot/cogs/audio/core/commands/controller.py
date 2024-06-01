1fec6d1e7ae67aad55a909f7bf2f0b766e8db0d9
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-06-01 18:55:15 +0100

    Update controller.py
    seeing if this will allow someone to disconnect without being in vc if no one is there

--- redbot/cogs/audio/core/commands/controller.py
+++ redbot/cogs/audio/core/commands/controller.py
@@ -31,6 +31,30 @@ class PlayerControllerCommands(MixinMeta, metaclass=CompositeMetaClass):
     async def command_disconnect(self, ctx: commands.Context):
         """Disconnect from the voice channel."""
 
+        player = lavalink.get_player(ctx.guild.id)
+
+        # Check if the voice channel is empty.
+        if ctx.guild.me.voice and len(ctx.guild.me.voice.channel.members) == 1:
+            await self.send_embed_msg(ctx, title=_("Disconnecting..."))
+            self.bot.dispatch("red_audio_audio_disconnect", ctx.guild)
+            self.update_player_lock(ctx, False)
+            eq = player.fetch("eq")
+            player.queue = []
+            player.store("playing_song", None)
+            player.store("autoplay_notified", False)
+            if eq:
+                await self.config.custom("EQUALIZER", ctx.guild.id).eq_bands.set(eq.bands)
+            await player.stop()
+            await player.disconnect()
+            await self.config.guild_from_id(guild_id=ctx.guild.id).currently_auto_playing_in.set(
+                []
+            )
+            self._ll_guild_updates.discard(ctx.guild.id)
+            await self.api_interface.persistent_queue_api.drop(ctx.guild.id)
+            return await self.send_embed_msg(
+                ctx, title=_("Disconnected because the channel was empty.")
+            )
+
         # Check if the user is in a voice channel.
         if ctx.author.voice is None:
             return await self.send_embed_msg(ctx, title=_("You are not in a voice channel!"))
@@ -41,7 +65,6 @@ class PlayerControllerCommands(MixinMeta, metaclass=CompositeMetaClass):
                 ctx.guild.id, await self.config.guild(ctx.guild).dj_enabled()
             )
             vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
-            player = lavalink.get_player(ctx.guild.id)
             can_skip = await self._can_instaskip(ctx, ctx.author)
             if (
                 (vote_enabled or (vote_enabled and dj_enabled))
