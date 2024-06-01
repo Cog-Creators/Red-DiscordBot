31ee06db6d979bd685f99073eba0c4e4057ec18f
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2024-06-01 19:00:06 +0100

    Update controller.py
    try 2 sorry

--- redbot/cogs/audio/core/commands/controller.py
+++ redbot/cogs/audio/core/commands/controller.py
@@ -55,55 +55,54 @@ class PlayerControllerCommands(MixinMeta, metaclass=CompositeMetaClass):
                 ctx, title=_("Disconnected because the channel was empty.")
             )
 
-        # Check if the user is in a voice channel.
+        # Check if the user is in a voice channel or not.
         if ctx.author.voice is None:
             return await self.send_embed_msg(ctx, title=_("You are not in a voice channel!"))
+
         if not self._player_check(ctx):
             return await self.send_embed_msg(ctx, title=_("Nothing playing."))
-        else:
-            dj_enabled = self._dj_status_cache.setdefault(
-                ctx.guild.id, await self.config.guild(ctx.guild).dj_enabled()
+        dj_enabled = self._dj_status_cache.setdefault(
+            ctx.guild.id, await self.config.guild(ctx.guild).dj_enabled()
+        )
+        vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
+        can_skip = await self._can_instaskip(ctx, ctx.author)
+        if (
+            (vote_enabled or (vote_enabled and dj_enabled))
+            and not can_skip
+            and not await self.is_requester_alone(ctx)
+        ):
+            return await self.send_embed_msg(
+                ctx,
+                title=_("Unable To Disconnect"),
+                description=_("There are other people listening - vote to skip instead."),
             )
-            vote_enabled = await self.config.guild(ctx.guild).vote_enabled()
-            can_skip = await self._can_instaskip(ctx, ctx.author)
-            if (
-                (vote_enabled or (vote_enabled and dj_enabled))
-                and not can_skip
-                and not await self.is_requester_alone(ctx)
-            ):
-                return await self.send_embed_msg(
-                    ctx,
-                    title=_("Unable To Disconnect"),
-                    description=_("There are other people listening - vote to skip instead."),
-                )
-            if dj_enabled and not vote_enabled and not can_skip:
-                return await self.send_embed_msg(
-                    ctx,
-                    title=_("Unable To Disconnect"),
-                    description=_("You need the DJ role to disconnect."),
-                )
-            if dj_enabled and not can_skip:
-                return await self.send_embed_msg(
-                    ctx,
-                    title=_("Unable to Disconnect"),
-                    description=_("You need the DJ role to disconnect."),
-                )
-            await self.send_embed_msg(ctx, title=_("Disconnecting..."))
-            self.bot.dispatch("red_audio_audio_disconnect", ctx.guild)
-            self.update_player_lock(ctx, False)
-            eq = player.fetch("eq")
-            player.queue = []
-            player.store("playing_song", None)
-            player.store("autoplay_notified", False)
-            if eq:
-                await self.config.custom("EQUALIZER", ctx.guild.id).eq_bands.set(eq.bands)
-            await player.stop()
-            await player.disconnect()
-            await self.config.guild_from_id(guild_id=ctx.guild.id).currently_auto_playing_in.set(
-                []
+        if dj_enabled and not vote_enabled and not can_skip:
+            return await self.send_embed_msg(
+                ctx,
+                title=_("Unable To Disconnect"),
+                description=_("You need the DJ role to disconnect."),
             )
-            self._ll_guild_updates.discard(ctx.guild.id)
-            await self.api_interface.persistent_queue_api.drop(ctx.guild.id)
+        if dj_enabled and not can_skip:
+            return await self.send_embed_msg(
+                ctx,
+                title=_("Unable to Disconnect"),
+                description=_("You need the DJ role to disconnect."),
+            )
+
+        await self.send_embed_msg(ctx, title=_("Disconnecting..."))
+        self.bot.dispatch("red_audio_audio_disconnect", ctx.guild)
+        self.update_player_lock(ctx, False)
+        eq = player.fetch("eq")
+        player.queue = []
+        player.store("playing_song", None)
+        player.store("autoplay_notified", False)
+        if eq:
+            await self.config.custom("EQUALIZER", ctx.guild.id).eq_bands.set(eq.bands)
+        await player.stop()
+        await player.disconnect()
+        await self.config.guild_from_id(guild_id=ctx.guild.id).currently_auto_playing_in.set([])
+        self._ll_guild_updates.discard(ctx.guild.id)
+        await self.api_interface.persistent_queue_api.drop(ctx.guild.id)
 
     @commands.command(name="now")
     @commands.guild_only()
