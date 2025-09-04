5c36b578a82fdf7c556197f2499a534552dfde7e
Karlo Prikratki <karlo@karloprikratki.com>
2025-09-04 12:47:46 +0200

    await argh

--- redbot/core/_events.py
+++ redbot/core/_events.py
@@ -388,7 +388,7 @@ def init_events(bot, cli_flags):
                     message = inline(_("Error in command '{command}'."))
             await ctx.send(message.replace("{command}", ctx.command.qualified_name))
         elif isinstance(error, commands.CommandNotFound):
-            if not bot.message_eligible_as_command(ctx.message):
+            if not await bot.message_eligible_as_command(ctx.message):
                 return
 
             help_settings = await HelpSettings.from_context(ctx)
