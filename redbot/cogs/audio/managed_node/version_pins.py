1b971b33eff5ad916fee45511eb390a5123f4705
Jakub Kuczys <me@jacken.men>
2026-03-05 20:39:11 +0100

    Bump Lavalink version to 3.7.13+red.3

--- redbot/cogs/audio/managed_node/version_pins.py
+++ redbot/cogs/audio/managed_node/version_pins.py
@@ -11,7 +11,7 @@ __all__ = (
 )
 
 
-JAR_VERSION: Final[LavalinkVersion] = LavalinkVersion(3, 7, 13, red=2)
+JAR_VERSION: Final[LavalinkVersion] = LavalinkVersion(3, 7, 13, red=3)
 YT_PLUGIN_VERSION: Final[str] = "1.18.0"
 # keep this sorted from oldest to latest
 SUPPORTED_JAVA_VERSIONS: Final[Tuple[int, ...]] = (11, 17)
