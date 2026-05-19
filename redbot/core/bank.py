b2412a6a1a5a5fff4687282c8db5d33f19e9752d
Jakub Kuczys <me@jacken.men>
2026-05-19 02:12:29 +0200

    Apply suggestion
    Co-authored-by: Draper <27962761+Drapersniper@users.noreply.github.com>

--- redbot/core/bank.py
+++ redbot/core/bank.py
@@ -604,7 +604,7 @@ async def get_leaderboard(positions: int = None, guild: discord.Guild = None) ->
 
 
 async def get_leaderboard_position(
-    member: Union[discord.User, discord.Member], return_lb_size: bool = False
+    member: Union[discord.User, discord.Member], *, return_lb_size: bool = False
 ) -> Union[Optional[int], Tuple[Optional[int], int]]:
     """
      Get the leaderboard position for the specified user
